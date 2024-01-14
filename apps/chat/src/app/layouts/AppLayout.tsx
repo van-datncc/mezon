9fd97131f04d330a13c23a2bbbfc01c11732522c
devGangz <phongnn@devcamp.edu.vn>
2024-01-15 01:33:15 +0700

    completed Login feature

--- apps/chat/src/app/layouts/AppLayout.tsx
+++ apps/chat/src/app/layouts/AppLayout.tsx
@@ -1,22 +1,36 @@
-
-// import { useChat } from '@mezon/core';
 import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '@mezon/store';
+import { getMezonSession } from 'libs/utils/src/lib/storage/storage';
+import { authActions } from '@mezon/store';
+import { useDispatch } from 'react-redux';
 
 const AppLayout = () => {
-    // const {  } = useChat();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLogin = useSelector((state: RootState) => state.auth.isLogin);
+  const account = useSelector((state: RootState) => state.account.account);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      // go to main page if user is not logged in
-      navigate('/guess/login');
-    }, [navigate]);
+  // console.log("acc", account)
 
-    return (
-      <div id="app-layout">
-        <Outlet />
-      </div>
-    )
-  }
-  
-  export default AppLayout;
+  useEffect(() => {
+    const storedSession = getMezonSession();
+    if (isLogin && storedSession !== null) {
+      navigate('/chat/servers/clan1/channels/channel1');
+    } else {
+      if (storedSession === null) {
+        dispatch(authActions.logOut());
+        navigate('guess/login');
+      }
+    }
+  }, [isLogin, navigate, dispatch]);
+
+  return (
+    <div id="app-layout">
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
