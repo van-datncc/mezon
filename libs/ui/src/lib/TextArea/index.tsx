1c174f72726703fd64a313a0254d91c4e37ebb4c
Phuc Duong <phuc.duong@zestif.com>
2024-01-15 19:04:54 +0700

    add_common_component

---
+++ libs/ui/src/lib/TextArea/index.tsx
@@ -0,0 +1,22 @@
+import React, { TextareaHTMLAttributes } from 'react';
+
+interface TextAreaProps
+    extends Omit<
+        TextareaHTMLAttributes<HTMLTextAreaElement>,
+        'dangerouslySetInnerHTML'
+    > {
+    className?: string;
+}
+
+const TextArea: React.FC<TextAreaProps> = ({ className, ...rest }) => {
+    return (
+        <div>
+            <textarea
+                className={`bg-black font-[400] py-[12px] px-[14px] rounded w-full border border-gray-300 text-white outline-none ${className}`}
+                {...rest}
+            />
+        </div>
+    );
+};
+
+export default TextArea;
