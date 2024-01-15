9654336be6ee1c93b2925fcbeaa640945b29abcc
minhlucvanncc <145336454+minhlucvanncc@users.noreply.github.com>
2024-01-15 10:25:52 +0700

    update engine

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,13 +1,13 @@
 # add ssh keys
-
 echo "Adding ssh keys"
 eval `ssh-agent -s`
+chmod 600 ./ssh/id_gh_ncc
 ssh-add ./ssh/id_gh_ncc
 
 # add git submodules
 echo "Adding git submodules"
-git submodule init 
-git submodule update
+git submodule init
+git submodule update --recursive --remote --init
 
 # finish
 echo "Done"
