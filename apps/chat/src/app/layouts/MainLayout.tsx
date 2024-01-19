9eb3427ed9f9aa55baf3c8371db68a31e8e902bd
minhlucvanncc <145336454+minhlucvanncc@users.noreply.github.com>
2024-01-19 13:15:25 +0700

    update main layout logic

--- apps/chat/src/app/layouts/MainLayout.tsx
+++ apps/chat/src/app/layouts/MainLayout.tsx
@@ -7,39 +7,29 @@ import { useSelector } from 'react-redux';
 import { Outlet, useNavigate, useParams } from 'react-router-dom';
 
 const MainLayout = () => {
-  const isLogin  = useSelector(selectIsLogin)
   const { serverId: serverIdParams } = useParams();
   const { changeCurrentClan, currentClan, fetchClans } = useChat();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isLogin) {
-      return;
-    }
-
     fetchClans()
-  }, [fetchClans, isLogin]);
+  }, [fetchClans]);
 
   useEffect(() => {
-    if(!isLogin || !serverIdParams || serverIdParams == currentClan?.id) {
+    if (!serverIdParams || serverIdParams == currentClan?.id) {
       return
     }
     changeCurrentClan(serverIdParams);
-  }, [changeCurrentClan, currentClan, serverIdParams, isLogin]);
+  }, [changeCurrentClan, currentClan, serverIdParams]);
 
   useEffect(() => {
-    if (!isLogin) {
-      navigate('/guess/login');
-      return;
-    }
-
     if (!currentClan) {
       return;
     }
 
     const url = `/chat/servers/${currentClan?.id}`;
     navigate(url);
-  }, [currentClan, navigate, isLogin]);
+  }, [currentClan, navigate]);
 
   return (
     <div id="main-layout">
@@ -51,8 +41,10 @@ const MainLayout = () => {
 }
 
 const MainLayoutWrapper = () => {
+  const isLogin = useSelector(selectIsLogin)
   const session = useSelector(selectSession);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!session) {
@@ -60,6 +52,13 @@ const MainLayoutWrapper = () => {
     }
     dispatch(authActions.refreshSession())
   }, [dispatch, session])
+
+  useEffect(() => {
+    if (!isLogin) {
+      navigate('/guess/login');
+      return;
+    }
+  }, [isLogin, navigate]);
 
   return (
     <MezonSuspense>
