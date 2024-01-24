2b923b22a1dd0c9475d227dd89710b40f3aa478e
minhlucvanncc <145336454+minhlucvanncc@users.noreply.github.com>
2024-01-24 18:20:44 +0700

    update status

--- apps/chat/src/app/pages/channel/ChannelMessageBox.tsx
+++ apps/chat/src/app/pages/channel/ChannelMessageBox.tsx
@@ -2,11 +2,8 @@ import { MessageBox, IMessagePayload } from '@mezon/components';
 import { useChat } from '@mezon/core';
 import { RootState } from '@mezon/store';
 import { IMessage } from '@mezon/utils';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { Client, Session } from '@heroiclabs/nakama-js';
-import { useParams } from 'react-router-dom';
-import { DeviceUUID } from "device-uuid";
 
 export function ChannelMessageBox() {
     const { sendMessage } = useChat();
