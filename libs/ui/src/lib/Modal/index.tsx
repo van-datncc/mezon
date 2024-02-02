a35a4e6ffb027ee03916b61d11be94e573665e12
Nguyen Tran Nhan <tran-nhan.nguyen@external.totalenergies.com>
2024-02-02 16:47:41 +0700

    v

--- libs/ui/src/lib/Modal/index.tsx
+++ libs/ui/src/lib/Modal/index.tsx
@@ -18,8 +18,8 @@ const Modal = (props: ModalProps) => {
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
                         <div className="relative w-auto my-6 mx-auto max-w-3xl ">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-[#0B0B0B] outline-none focus:outline-none">
-                                <div className="flex items-center justify-between p-3 border-b border-solid border-borderDefault rounded-t">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-bgDisable outline-none focus:outline-none">
+                                <div className="flex items-center justify-between p-3  border-solid border-borderDefault rounded-t">
                                     <h3 className="text-[22px] font-semibold">
                                         {title}
                                     </h3>
