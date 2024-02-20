87c4c459ae99a91c38eabfbe4f0fcfbb170dc523
thuthuy1901 <huyetlinh1901@gmail.com>
2024-02-20 17:32:24 +0700

    responsive login UI

--- libs/components/src/lib/components/LoginForm/GoogleButton/index.tsx
+++ libs/components/src/lib/components/LoginForm/GoogleButton/index.tsx
@@ -13,10 +13,10 @@ const GoogleButtonLogin: React.FC = () => {
 	});
 
 	return (
-		<div className="w-full px-36 lg:px-0">
+		<div className="w-full lg:px-0">
 			<button onClick={googleLogin} className="flex justify-center w-full  h-fit p-3 rounded-[4px] bg-[#d1e0ff] relative">
-				<div className="flex  w-fit h-fit gap-x-1 p-0">
-					<img src={'/assets/images/google-icon.png'} className="p-0 " alt="Google Logo" />
+				<div className="flex items-center w-fit h-fit gap-x-1 p-0">
+					<img src={'/assets/images/google-icon.png'} className="p-0 object-cover" alt="Google Logo" />
 					<p className="w-fit h-fit font-manrope text-base font-medium text-[#155eef] leading-[150%]">Continue with Google</p>
 				</div>
 			</button>
