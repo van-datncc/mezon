b414f5ec712786982e4e8416fe051346b44ed404
Nhan Nguyen <nguyentran@ncc.asia>
2024-02-21 11:08:19 +0700

    re add strict mode

--- apps/chat/src/main.tsx
+++ apps/chat/src/main.tsx
@@ -3,6 +3,11 @@ import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
 
 import './styles.scss';
+import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+<StrictMode>
+    <App />
+</StrictMode>
+);
