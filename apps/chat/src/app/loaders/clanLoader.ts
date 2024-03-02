2c50d0695446c6f6a11b4b7b3d6a10fad19da8d2
Nhan Nguyen <nguyentran@ncc.asia>
2024-03-02 08:50:46 +0700

    rename

--- apps/chat/src/app/loaders/clanLoader.ts
+++ apps/chat/src/app/loaders/clanLoader.ts
@@ -1,11 +1,11 @@
 import { clansActions, getStoreAsync } from '@mezon/store';
 import { LoaderFunction, ShouldRevalidateFunction } from 'react-router-dom';
 
-export type ServerLoaderData = {
+export type ClanLoaderData = {
 	clanId: string;
 };
 
-export const serverLoader: LoaderFunction = async ({ params }) => {
+export const clanLoader: LoaderFunction = async ({ params }) => {
 	const { clanId } = params;
 	const store = await getStoreAsync();
 	if (!clanId) {
@@ -14,7 +14,7 @@ export const serverLoader: LoaderFunction = async ({ params }) => {
 	store.dispatch(clansActions.changeCurrentClan({ clanId: clanId }));
 	return {
 		clanId,
-	} as ServerLoaderData;
+	} as ClanLoaderData;
 };
 
 export const shouldRevalidateServer: ShouldRevalidateFunction = (ctx) => {
--- apps/chat/src/app/routes/index.tsx
+++ apps/chat/src/app/routes/index.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import AppLayout from '../layouts/AppLayout';
 import GuessLayout from '../layouts/GuessLayout';
 import MainLayout from '../layouts/MainLayout';
-import ServerLayout from '../layouts/ServerLayout';
+import ServerLayout from '../layouts/ClanLayout';
 
 // Pages
 import ChannellayOut from '../pages/channel';
@@ -24,7 +24,7 @@ import { directLoader } from '../loaders/directLoader';
 import { directMessageLoader } from '../loaders/directMessageLoader';
 import { friendsLoader } from '../loaders/friendsLoader';
 import { mainLoader, shouldRevalidateMain } from '../loaders/mainLoader';
-import { serverLoader, shouldRevalidateServer } from '../loaders/serverLoader';
+import { clanLoader, shouldRevalidateServer } from '../loaders/clanLoader';
 import { appLoader, shouldRevalidateApp } from '../loaders/appLoader';
 import { ChannelIndex } from '../pages/channel/ChannelIndex';
 import { ClanIndex } from '../pages/clan/ClanIndex';
@@ -83,7 +83,7 @@ export const routes = createBrowserRouter([
 										children: [
 											{
 												path: ':clanId',
-												loader: serverLoader,
+												loader: clanLoader,
 												shouldRevalidate: shouldRevalidateServer,
 												element: <ServerLayout />,
 												children: [
