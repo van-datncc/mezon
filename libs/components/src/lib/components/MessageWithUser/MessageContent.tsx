92ee2b24347c894c3eb3bdaac706254b1b6be092
Nguyen Tran Nhan <tran-nhan.nguyen@external.totalenergies.com>
2024-03-08 17:09:02 +0700

    Fail to display message when input text after image

--- libs/components/src/lib/components/MessageWithUser/MessageContent.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageContent.tsx
@@ -12,19 +12,20 @@ type IMessageContentProps = {
 
 const MessageContent = ({ user, message, isCombine }: IMessageContentProps) => {
 	const { attachments, lines } = useMessageParser(message);
+	const renderAttachments = () => {
+		if (attachments && attachments.length > 0 && attachments[0].filetype?.indexOf('image') !== -1) {
+			// TODO: render multiple attachments
+			return <MessageImage attachmentData={attachments[0]} />;
+		}
 
-	if (attachments && attachments.length > 0 && attachments[0].filetype?.indexOf('image') !== -1) {
-		// TODO: render multiple attachments
-		return <MessageImage attachmentData={attachments[0]} />;
-	}
-
-	if (attachments && attachments.length > 0 && attachments[0].filetype?.indexOf('image') === -1) {
-		return <MessageLinkFile attachmentData={attachments[0]} />;
-	}
-
+		if (attachments && attachments.length > 0 && attachments[0].filetype?.indexOf('image') === -1) {
+			return <MessageLinkFile attachmentData={attachments[0]} />;
+		}
+	}	
 	return (
 		// eslint-disable-next-line react/jsx-no-useless-fragment
 		<>
+			{renderAttachments()}
 			{lines?.map((line: string, index: number) => {
 				return <MessageLine line={line} key={index} />;
 			})}
