0c3fd24ff4e104914517c275f1c332e638f8eed4
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-03-08 11:50:52 +0700

    remove_click_emj

--- apps/chat/src/app/pages/channel/index.tsx
+++ apps/chat/src/app/pages/channel/index.tsx
@@ -1,6 +1,6 @@
 import { MemberList } from '@mezon/components';
 import { selectCurrentChannel, selectIsShowMemberList } from '@mezon/store';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useSelector } from 'react-redux';
 import ChannelMessages from './ChanneMessages';
 import { ChannelMessageBox } from './ChannelMessageBox';
@@ -12,25 +12,24 @@ export default function ChannelLayout() {
 
 	const messagesContainerRef = useRef<HTMLDivElement>(null);
 
-	const [isCloseEmoji, setIsCloseEmoji] = useState<boolean>(false);
-	const handleCloseEmoji = () => {
-		setIsCloseEmoji(!isCloseEmoji);
-	};
-
 	return (
-		<div onClick={handleCloseEmoji} className="flex flex-col flex-1 shrink min-w-0 bg-bgSecondary h-[100%] overflow-hidden">
+		<div className="flex flex-col flex-1 shrink min-w-0 bg-bgSecondary h-[100%] overflow-hidden">
 			<div className="flex h-heightWithoutTopBar flex-row ">
 				<div className="flex flex-col flex-1 w-full h-full">
 					<div
 						className="overflow-y-auto bg-[#1E1E1E] max-w-widthMessageViewChat overflow-x-hidden max-h-heightMessageViewChat h-heightMessageViewChat"
 						ref={messagesContainerRef}
 					>
-						{currentChanel ? <ChannelMessages channelId={currentChanel?.id} channelName={currentChanel.channel_lable} type='channel' /> : <ChannelMessages.Skeleton />}
+						{currentChanel ? (
+							<ChannelMessages channelId={currentChanel?.id} channelName={currentChanel.channel_lable} type="channel" />
+						) : (
+							<ChannelMessages.Skeleton />
+						)}
 					</div>
-					<div className="flex-shrink-0 flex flex-col bg-[#1E1E1E] h-auto relative">
+					<div className="flex-shrink-0 flex flex-col bg-[#1E1E1E] h-auto relative border">
 						{currentChanel && <ChannelTyping channelId={currentChanel?.id} />}
 						{currentChanel ? (
-							<ChannelMessageBox clanId={currentChanel.clan_id} controlEmoji={isCloseEmoji} channelId={currentChanel?.id} />
+							<ChannelMessageBox clanId={currentChanel.clan_id} channelId={currentChanel?.id} />
 						) : (
 							<ChannelMessageBox.Skeleton />
 						)}
