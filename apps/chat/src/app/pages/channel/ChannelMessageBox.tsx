1e62e5de9ba02e06762c12b8862634148ad5b232
Nguyen Tran Nhan <tran-nhan.nguyen@external.totalenergies.com>
2024-03-15 10:03:54 +0700

    apply to DM

--- apps/chat/src/app/pages/channel/ChannelMessageBox.tsx
+++ apps/chat/src/app/pages/channel/ChannelMessageBox.tsx
@@ -1,5 +1,5 @@
 import { MentionData } from '@draft-js-plugins/mention';
-import { MessageBox, ReplyMessage } from '@mezon/components';
+import { MessageBox, ReplyMessage, UserMentionList } from '@mezon/components';
 import { ChatContext, useChannelMembers, useChatSending } from '@mezon/core';
 import { ChannelMembersEntity } from '@mezon/store';
 import { IMessageSendPayload } from '@mezon/utils';
@@ -33,13 +33,6 @@ export function ChannelMessageBox({ channelId, controlEmoji, clanId }: ChannelMe
 	}, [sendMessageTyping]);
 
 	const handleTypingDebounced = useThrottledCallback(handleTyping, 1000);
-	const { members } = useChannelMembers({ channelId });
-	const userMentionRaw = members[0].users;
-	const newUserMentionList: MentionData[] = userMentionRaw?.map((item: ChannelMembersEntity) => ({
-		avatar: item?.user?.avatar_url ?? '',
-		name: item?.user?.username ?? '',
-		id: item?.user?.id ?? '',
-	}));
 
 	const { setIsOpenReply } = useContext(ChatContext);
 
@@ -52,7 +45,7 @@ export function ChannelMessageBox({ channelId, controlEmoji, clanId }: ChannelMe
 			<ReplyMessage />
 			<MessageBox
 				isOpenEmojiPropOutside={controlEmoji}
-				listMentions={newUserMentionList}
+				listMentions={UserMentionList(channelId)}
 				onSend={handleSend}
 				onTyping={handleTypingDebounced}
 				currentChannelId={channelId}
