c0c651a2c989ae8c56ef409ea69683d546af03d0
Nguyen Tran Nhan <tran-nhan.nguyen@external.totalenergies.com>
2024-03-18 17:09:42 +0700

    edit message

--- libs/components/src/lib/components/MessageWithUser/MessageContent.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageContent.tsx
@@ -4,14 +4,20 @@ import MessageImage from './MessageImage';
 import MessageLinkFile from './MessageLinkFile';
 import { useMessageParser } from './useMessageParser';
 import MessageVideo from './MessageVideo';
+import { useMemo } from 'react';
 
 type IMessageContentProps = {
 	user?: IChannelMember | null;
 	message: IMessageWithUser;
 	isCombine: boolean;
+	newMessage?: string;
 };
 
-const MessageContent = ({ user, message, isCombine }: IMessageContentProps) => {
+const MessageContent = ({ user, message, isCombine, newMessage }: IMessageContentProps) => {
+	const lineNew =   useMemo(() => {
+        const values = newMessage?.split('\n');
+		return values
+	}, [newMessage]);   
 	const { attachments, lines } = useMessageParser(message);
 	const renderAttachments = () => {
 		if (attachments && attachments.length > 0 && attachments[0].filetype?.indexOf('image') !== -1) {
@@ -31,9 +37,19 @@ const MessageContent = ({ user, message, isCombine }: IMessageContentProps) => {
 		// eslint-disable-next-line react/jsx-no-useless-fragment
 		<>
 			{renderAttachments()}
-			{lines?.map((line: string, index: number) => {
-				return <MessageLine line={line} key={index} />;
-			})}
+			{newMessage !== "" ? (
+				<div>
+					{lineNew?.map((line: string, index: number) => {
+						return <MessageLine line={line} key={index} />;
+					})}
+				</div>
+			): (
+				<div>
+					{lines?.map((line: string, index: number) => {
+						return <MessageLine line={line} key={index} />;
+					})}
+				</div>
+			)}
 		</>
 	);
 };
