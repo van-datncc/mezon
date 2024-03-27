7a30b817eedea92ce4bedc447c7845902994375b
Thanh97x <122072000+Thanh97x@users.noreply.github.com>
2024-03-27 18:00:14 +0700

    commit task 512

--- libs/components/src/lib/components/FooterProfile/index.tsx
+++ libs/components/src/lib/components/FooterProfile/index.tsx
@@ -1,5 +1,6 @@
 import { HeadPhoneICon, MicIcon, SettingProfile } from '../Icons';
 import MemberProfile from '../MemberProfile';
+import VoiControlPanel from '../VoiceControlPanel';
 
 export type FooterProfileProps = {
 	name: string;
@@ -10,21 +11,28 @@ export type FooterProfileProps = {
 
 function FooterProfile({ name, status, avatar, openSetting }: FooterProfileProps) {
 	return (
-		<button className="flex items-center justify-between border-t-2 border-borderDefault px-4 py-2 font-title text-[15px] font-[500] text-white hover:bg-gray-550/[0.16] shadow-sm transition absolute bottom-0 bg-bgSurface w-full group">
-			<MemberProfile
-				name={name}
-				status={status}
-				avatar={avatar}
-				isHideStatus={false}
-				numberCharacterCollapse={15}
-				classParent="memberProfile"
-			/>
-			<div className="flex items-center gap-2 iconHover bgHover">
-				<MicIcon className="ml-auto w-[18px] h-[18px] opacity-80 iconRed" />
-				<HeadPhoneICon className="ml-auto w-[18px] h-[18px] opacity-80" />
-				<SettingProfile className="ml-auto w-[18px] h-[18px] opacity-80 text-[#AEAEAE]" onClick={openSetting} />
-			</div>
-		</button>
+		<div>
+			<VoiControlPanel />
+			<button className="flex items-center justify-between border-t-2
+			 border-borderDefault px-4 py-2 font-title text-[15px] 
+			 font-[500] text-white hover:bg-gray-550/[0.16] 
+			 shadow-sm transition absolute bottom-0 bg-bgSurface 
+			 w-full group">
+				<MemberProfile
+					name={name}
+					status={status}
+					avatar={avatar}
+					isHideStatus={false}
+					numberCharacterCollapse={15}
+					classParent="memberProfile"
+				/>
+				<div className="flex items-center gap-2 iconHover bgHover">
+					<MicIcon className="ml-auto w-[18px] h-[18px] opacity-80 iconRed" />
+					<HeadPhoneICon className="ml-auto w-[18px] h-[18px] opacity-80" />
+					<SettingProfile className="ml-auto w-[18px] h-[18px] opacity-80 text-[#AEAEAE]" onClick={openSetting} />
+				</div>
+			</button>
+		</div>
 	);
 }
 
