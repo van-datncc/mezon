037df4e666bd821e2b09a484895621940d0f8322
thuthuy1901 <huyetlinh1901@gmail.com>
2024-03-28 11:41:49 +0700

    fix bug UI setting channel

--- libs/components/src/lib/components/ChannelSetting/Component/Modal/modalAskChangeChannel.tsx
+++ libs/components/src/lib/components/ChannelSetting/Component/Modal/modalAskChangeChannel.tsx
@@ -1,15 +1,18 @@
 export type ModalAskChangeChannelProps = {
 	onReset: () => void;
 	onSave: () => void;
+	className?: string;
 };
 
 const ModalAskChangeChannel = (props: ModalAskChangeChannelProps) => {
-	const { onReset, onSave } = props;
+	const { onReset, onSave, className } = props;
 
 	return (
-		<div className="flex flex-row gap-2  bg-gray-500 absolute max-w-[815px] w-full left-1/2 translate-x-[-50%] bottom-4 min-w-96 h-fit p-3 rounded transform text-white">
+		<div
+			className={`flex flex-row gap-2  bg-gray-500 absolute max-w-[815px] w-full left-1/2 translate-x-[-50%] bottom-4 min-w-96 h-fit p-3 rounded transform text-white ${className}`}
+		>
 			<div className="flex-1 flex items-center text-nowrap">
-				<p className="text-[15px]">Carefull - you have unsaved changes!</p>
+				<p className={`text-[15px] ${className ? 'hidden' : ''}`}>Carefull - you have unsaved changes!</p>
 			</div>
 			<div className="flex flex-row justify-end gap-3">
 				<button className="text-[15px] bg-gray-600 rounded-[4px] p-[8px]" onClick={onReset}>
