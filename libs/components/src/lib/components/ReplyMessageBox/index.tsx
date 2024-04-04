1946f4b30d200e189827e70a4f3fa93d653d27cc
devGangz <phongnn@devcamp.edu.vn>
2024-04-05 06:54:38 +0700

    0404

--- libs/components/src/lib/components/ReplyMessageBox/index.tsx
+++ libs/components/src/lib/components/ReplyMessageBox/index.tsx
@@ -1,21 +1,24 @@
-import { selectMemberByUserId, selectReference } from '@mezon/store';
+import { emojiActions, selectMemberByUserId, selectMessageReplyState, selectReference, useAppDispatch } from '@mezon/store';
 import { useSelector } from 'react-redux';
 import * as Icons from '../Icons/index';
 import { useState } from 'react';
 
 
 function ReplyMessageBox() {
+	const dispatch = useAppDispatch();
+
 	const refMessage = useSelector(selectReference);
 	const getSenderMessage = useSelector(selectMemberByUserId(refMessage?.sender_id ?? ''));
-	const [isOpenReply, setIsOpenReply] = useState<boolean>(false);
+    const messageReplyState = useSelector(selectMessageReplyState); 
 
 	const handleRemoveReply = () => {
-		setIsOpenReply(false);
+		dispatch(emojiActions.setMessageReplyState(false));
 	};
+
 
 	return (
 		<>
-			{isOpenReply && (
+			{messageReplyState && (
 				<div className="flex flex-row items-center justify-between w-full my-2  bg-[#2B2D31] p-2 rounded-md text-[14px]">
 					<div className="">
 						Replying to <span className=" text-[#84ADFF] font-semibold">{getSenderMessage?.user?.username}</span>
