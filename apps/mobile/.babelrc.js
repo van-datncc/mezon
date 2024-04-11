e3bcde3b32a16450a1bd0ebb81d5d21edebd7f68
Luc Van Minh <luk.mink@gmail.com>
2024-04-11 21:34:21 +0700

    update mobile build

--- apps/mobile/.babelrc.js
+++ apps/mobile/.babelrc.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
 	api.cache(true);
 
-	if (process.env.NX_TASK_TARGET_TARGET === 'build' || process.env.NX_TASK_TARGET_TARGET.includes('storybook')) {
+	if (
+		process.env.NX_TASK_TARGET_TARGET &&
+		(process.env.NX_TASK_TARGET_TARGET === 'build' || process.env.NX_TASK_TARGET_TARGET.includes('storybook'))
+	) {
 		return {
 			presets: [
 				[
