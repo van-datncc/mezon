998f963e14d7709415ed68f5f8813caa5f54ff9e
Nguyen Nam Phong <phongnn@devcamp.edu.vn>
2024-04-21 16:43:52 +0700

    up-2

--- libs/components/src/lib/components/GifsStickersEmojis/gifs/FeaturedGifs.tsx
+++ libs/components/src/lib/components/GifsStickersEmojis/gifs/FeaturedGifs.tsx
@@ -15,7 +15,7 @@ function FeaturedGifs({ channelId, channelLabel, mode }: FeaturedGifsProps) {
 	const { dataGifCategories, dataGifsSearch, loadingStatusGifs, valueInputToCheckHandleSearch, dataGifsFeartured } = useGifs();
 	return (
 		<>
-			<div className="relative h-24 rounded-md cursor-pointer overflow-hidden group">
+			<div className="relative h-32 rounded-md cursor-pointer overflow-hidden group">
 				<div className="absolute inset-0 bg-black opacity-50 z-20 transition-opacity group-hover:opacity-70"></div>
 				<div className="absolute inset-0 flex items-center justify-center z-30 pointer-events-none gap-2">
 					<Icons.TrendingGifs />
