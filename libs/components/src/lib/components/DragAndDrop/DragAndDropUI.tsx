796ff9cecb08fb2da21ae124deba16704c961731
Nguyen Nam Phong <phongnn@devcamp.edu.vn>
2024-04-22 20:36:53 +0700

    001

--- libs/components/src/lib/components/DragAndDrop/DragAndDropUI.tsx
+++ libs/components/src/lib/components/DragAndDrop/DragAndDropUI.tsx
@@ -19,34 +19,67 @@ function DragAndDropUI({ channelID, onDragEnter, onDragOver, onDragLeave, onDrop
 	const { draggingState, setDraggingState } = useDragAndDrop();
 
 	const [notLeave, setNotLeave] = useState(false);
-
+	const [isOverChild, setIsOverChild] = useState(false);
 	return (
+		// <div>
+		// 	<div
+		// 		id="form-file-upload"
+		// 		onDragEnter={() => {
+		// 			setDraggingState(true);
+		// 		}} // onDragOver={onDragOver}
+		// 		onDragLeave={() => {
+		// 			if (notLeave) {
+		// 				setDraggingState(true);
+		// 			} else if (!notLeave) {
+		// 				setDraggingState(false);
+		// 			}
+		// 		}}
+		// 		onDrop={onDrop}
+		// 		className="w-screen h-screen flex justify-center items-center bg-black  opacity-90 absolute top-0 left-0 z-30"
+		// 	>
+		// 		<div
+		// 			onDragEnter={() => {
+		// 				setDraggingState(true);
+		// 				setNotLeave(true);
+		// 			}}
+		// 			onDragOver={() => {
+		// 				setDraggingState(true);
+		// 			}}
+		// 			onDragLeave={() => {
+		// 				setDraggingState(true);
+		// 			}}
+		// 			className="w-[25rem] h-[15rem] bg-[#5865F2] flex flex-row justify-center  items-center rounded-lg z-50 absolute"
+		// 		></div>{' '}
+		// 	</div>
+		// </div>
 		<div>
 			<div
 				id="form-file-upload"
 				onDragEnter={() => {
 					setDraggingState(true);
-				}} // onDragOver={onDragOver}
+				}}
 				onDragLeave={() => {
-					if (notLeave) {
-						setDraggingState(true);
-					} else if (!notLeave) {
+					if (!isOverChild) {
+						// Nếu không ở trên div con, setDraggingState(false)
 						setDraggingState(false);
 					}
+					setNotLeave(false);
 				}}
 				onDrop={onDrop}
 				className="w-screen h-screen flex justify-center items-center bg-black  opacity-90 absolute top-0 left-0 z-30"
 			>
 				<div
 					onDragEnter={() => {
+						setIsOverChild(true); // Đang ở trên div con
 						setDraggingState(true);
 						setNotLeave(true);
 					}}
 					onDragOver={() => {
+						setIsOverChild(true);
 						setDraggingState(true);
 					}}
 					onDragLeave={() => {
-						setDraggingState(true);
+						setIsOverChild(false); // Rời khỏi div con
 					}}
 					className="w-[25rem] h-[15rem] bg-[#5865F2] flex flex-row justify-center  items-center rounded-lg z-50 absolute"
 				></div>{' '}
