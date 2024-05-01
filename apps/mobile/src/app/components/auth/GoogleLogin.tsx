284b1446897f537041e99a05f5158ea176135b77
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-05-02 01:11:58 +0700

    feat: UI clan mobile

--- apps/mobile/src/app/components/auth/GoogleLogin.tsx
+++ apps/mobile/src/app/components/auth/GoogleLogin.tsx
@@ -1,76 +1,79 @@
-import { Image, Platform, Pressable, StyleSheet, Text, View } from 'react-native'
-import React, {useEffect} from 'react'
-import Images from 'apps/mobile/src/assets/Images'
+import { accountActions, authActions, useAppDispatch } from '@mezon/store';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { useNavigation } from '@react-navigation/native';
+import Images from 'apps/mobile/src/assets/Images';
+import React, { useEffect } from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 const GOOGLE_ANDROID_ID = '648946579638-qtugur5pktrkh30q0ampp76fnaekcmk7.apps.googleusercontent.com';
 const GOOGLE_WEB_ID = '648946579638-331cst20cdecpef6ov0o0qauupfhq41n.apps.googleusercontent.com';
 
 const GoogleLogin = () => {
-    useEffect(() => {
-        GoogleSignin.configure({
-            webClientId: GOOGLE_WEB_ID,
-            offlineAccess: true,
-            forceCodeForRefreshToken: true,
-        });
-    }, []);
-    const navigation = useNavigation()
-    async function onGoogleButtonPress() {
-        try {
-            // Get the users ID token
-            await GoogleSignin.hasPlayServices();
-            const { idToken } = await GoogleSignin.signIn();
-            console.log('Tom log  => idToken', idToken);
-        } catch (error) {
-            console.log('error onGoogleButtonPress', error);
-        }
+	const dispatch = useAppDispatch();
 
-    }
-    return (
-        <Pressable style={styles.googleButton}
-            onPress={onGoogleButtonPress}
-        >
-            <View style={styles.socialButtonsContainer}>
-                <View style={styles.signinButtonLogoContainer}>
-                    <Image source={Images.ICON_GOOGLE} style={styles.signinButtonLogo} />
-                </View>
-                <Text style={styles.socialSigninButtonText}>Continue with Google</Text>
-            </View>
-        </Pressable>
-    )
-}
+	useEffect(() => {
+		GoogleSignin.configure({
+			webClientId: GOOGLE_WEB_ID,
+			offlineAccess: true,
+			forceCodeForRefreshToken: true,
+		});
+	}, []);
+	const navigation = useNavigation();
+	async function onGoogleButtonPress() {
+		try {
+			// Get the users ID token
+			await GoogleSignin.hasPlayServices();
+			const { idToken } = await GoogleSignin.signIn();
+			const action = await dispatch(authActions.authenticateGoogle(idToken));
+			const session = action.payload;
+			console.log('Tom log  => session', session);
+			dispatch(accountActions.setAccount(session));
+		} catch (error) {
+			console.log('error onGoogleButtonPress', error);
+		}
+	}
+	return (
+		<Pressable style={styles.googleButton} onPress={onGoogleButtonPress}>
+			<View style={styles.socialButtonsContainer}>
+				<View style={styles.signinButtonLogoContainer}>
+					<Image source={Images.ICON_GOOGLE} style={styles.signinButtonLogo} />
+				</View>
+				<Text style={styles.socialSigninButtonText}>Continue with Google</Text>
+			</View>
+		</Pressable>
+	);
+};
 
-export default GoogleLogin
+export default GoogleLogin;
 
 const styles = StyleSheet.create({
-    googleButton: {
-        backgroundColor: "#D1E0FF",
-        paddingVertical: 15,
-        marginHorizontal: 20,
-        borderRadius: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    socialButtonsContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-    },
-    signinButtonLogoContainer: {
-        backgroundColor: "#155EEF",
-        padding: 2,
-        borderRadius: 3,
-        position: 'absolute',
-        left: 25,
-    },
-    signinButtonLogo: {
-        height: 18,
-        width: 18,
-    },
-    socialSigninButtonText: {
-        color: "#155EEF",
-        fontSize: 16,
-        lineHeight: 13 * 1.4,
-    },
-})
+	googleButton: {
+		backgroundColor: '#D1E0FF',
+		paddingVertical: 15,
+		marginHorizontal: 20,
+		borderRadius: 8,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	socialButtonsContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+	},
+	signinButtonLogoContainer: {
+		backgroundColor: '#155EEF',
+		padding: 2,
+		borderRadius: 3,
+		position: 'absolute',
+		left: 25,
+	},
+	signinButtonLogo: {
+		height: 18,
+		width: 18,
+	},
+	socialSigninButtonText: {
+		color: '#155EEF',
+		fontSize: 16,
+		lineHeight: 13 * 1.4,
+	},
+});
--- apps/mobile/src/app/screens/home/homedrawer/ChannelList.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/ChannelList.tsx
@@ -1,34 +1,100 @@
-import React from "react";
-import {FlatList, StyleSheet, Text, View} from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import Dots from '../../../../assets/svg/guildMoreOptions1.svg'
-import { ChannelListSection, ChannelListContext } from "./Reusables";
-import {styles} from "./styles";
+import React from 'react';
+import { FlatList, Text, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Feather from 'react-native-vector-icons/Feather';
+import Dots from '../../../../assets/svg/guildMoreOptions1.svg';
+import { darkColor } from '../../../constants/Colors';
+import { useAnimatedState } from '../../../hooks/useAnimatedState';
+import { Colors } from '../../../themes';
+import { ChannelListContext, ChannelListSection } from './Reusables';
+import { styles } from './styles';
 
 const ChannelList = React.memo((props: any) => {
-    const channelData = {"id":1,"image":"https://img.freepik.com/free-photo/glowing-spaceship-orbits-planet-starry-galaxy-generated-by-ai_188544-9655.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1713139200&semt=sph","title":"Space Explore","channels":[{"id":1,"category":"information","items":[{"id":1,"title":"welcome-and-rules","type":"text"},{"id":2,"title":"notes-resources","type":"text"}]},{"id":2,"category":"text channels","items":[{"id":3,"title":"general","type":"text"},{"id":4,"title":"casual-chat","type":"text"},{"id":5,"title":"session-planning","type":"text"},{"id":6,"title":"off-topic","type":"text"}]},{"id":3,"category":"video channels","items":[{"id":7,"title":"video-chat","type":"text"},{"id":8,"title":"lounge","type":"voice"},{"id":9,"title":"study room 1","type":"voice"},{"id":10,"title":"study room 2","type":"voice"}]}]};
+	const channelData = {
+		id: 1,
+		image: 'https://img.freepik.com/free-photo/glowing-spaceship-orbits-planet-starry-galaxy-generated-by-ai_188544-9655.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1713139200&semt=sph',
+		title: 'Space Explore',
+		channels: [
+			{
+				id: 1,
+				category: 'information',
+				items: [
+					{ id: 1, title: 'welcome-and-rules', type: 'text' },
+					{ id: 2, title: 'notes-resources', type: 'text' },
+				],
+			},
+			{
+				id: 2,
+				category: 'text channels',
+				items: [
+					{ id: 3, title: 'general', type: 'text' },
+					{ id: 4, title: 'casual-chat', type: 'text' },
+					{ id: 5, title: 'session-planning', type: 'text' },
+					{ id: 6, title: 'off-topic', type: 'text' },
+				],
+			},
+			{
+				id: 3,
+				category: 'video channels',
+				items: [
+					{ id: 7, title: 'video-chat', type: 'text' },
+					{ id: 8, title: 'lounge', type: 'voice' },
+					{ id: 9, title: 'study room 1', type: 'voice' },
+					{ id: 10, title: 'study room 2', type: 'voice' },
+				],
+			},
+		],
+	};
+	const [collapseChannelItems, setCollapseChannelItems] = useAnimatedState([]);
 
-    return (
-        <ChannelListContext.Provider value={{navigation: props.navigation}}>
-            <View style={[styles.mainList, {backgroundColor: '#232323',}]}>
-                <ServerListHeader title={'KOMU'} />
-                <FlatList
-                    data={[...channelData.channels, ...channelData.channels]}
-                    renderItem={({ item }) => <ChannelListSection data={item} />}
-                />
-            </View>
-        </ChannelListContext.Provider>
-    )
+	const toggleCollapseChannel = (index: string) => {
+		if (collapseChannelItems.includes(index)) {
+			setCollapseChannelItems(collapseChannelItems.filter((item) => item !== index)); // Collapse if already Collapse
+		} else {
+			setCollapseChannelItems([...collapseChannelItems, index]); // Expand if not Collapse
+		}
+	};
+	return (
+		<ChannelListContext.Provider value={{ navigation: props.navigation }}>
+			<View style={[styles.mainList, { backgroundColor: '#232323' }]}>
+				<ServerListHeader title={'KOMU'} />
+				<View style={styles.channelListSearch}>
+					<View style={styles.channelListSearchWrapperInput}>
+						<Feather size={18} name="search" style={{ color: Colors.black }} />
+						<TextInput placeholder={'Search'} placeholderTextColor={Colors.textGray} style={styles.channelListSearchInput} />
+					</View>
+					<View
+						style={{
+							alignItems: 'center',
+							justifyContent: 'center',
+							display: 'flex',
+							borderRadius: 50,
+							backgroundColor: darkColor.Border_Focus,
+							width: 30,
+							height: 30,
+						}}
+					>
+						<Feather size={16} name="user-plus" style={{ color: darkColor.Backgound_Subtle }} />
+					</View>
+				</View>
+				<FlatList
+					data={[...channelData.channels, ...channelData.channels]}
+					renderItem={({ item, index }) => (
+						<ChannelListSection data={item} index={index} onPressHeader={toggleCollapseChannel} collapseItems={collapseChannelItems} />
+					)}
+				/>
+			</View>
+		</ChannelListContext.Provider>
+	);
 });
 
-const ServerListHeader = React.memo((props: {title: string}) => {
-
-    return (
-        <TouchableOpacity style={styles.listHeader}>
-            <Text style={styles.titleHeaderChannel}>{props.title}</Text>
-            <Dots width={30} height={30} />
-        </TouchableOpacity>
-    )
+const ServerListHeader = React.memo((props: { title: string }) => {
+	return (
+		<TouchableOpacity style={styles.listHeader}>
+			<Text style={styles.titleHeaderChannel}>{props.title}</Text>
+			<Dots width={30} height={30} />
+		</TouchableOpacity>
+	);
 });
 
 export default ChannelList;
