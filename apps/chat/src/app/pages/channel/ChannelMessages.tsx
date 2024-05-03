41ac7c83a74cf33e9279c6f7787862ce501b48f5
Phuc Duong <phuc.duong@zestif.com>
2024-05-03 17:55:32 +0700

    implemnet-lib-loading

--- apps/chat/src/app/pages/channel/ChannelMessages.tsx
+++ apps/chat/src/app/pages/channel/ChannelMessages.tsx
@@ -1,8 +1,9 @@
 import { ChatWelcome } from '@mezon/components';
-import { getJumpToMessageId, useChatMessages, useJumpToMessage, useReference } from '@mezon/core';
+import { useChatMessages } from '@mezon/core';
 import { useEffect, useRef, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { ChannelMessage } from './ChannelMessage';
+import AutoSizer from "react-virtualized-auto-sizer";
+import { VariableSizeList as List } from "react-window";
 
 type ChannelMessagesProps = {
 	channelId: string;
@@ -15,84 +16,96 @@ type ChannelMessagesProps = {
 export default function ChannelMessages({ channelId, channelLabel, type, avatarDM, mode }: ChannelMessagesProps) {
 	const containerRef = useRef<HTMLDivElement>(null);
 	const { messages, unreadMessageId, lastMessageId, hasMoreMessage, loadMoreMessage } = useChatMessages({ channelId });
-	const [position, setPosition] = useState(containerRef.current?.scrollTop || 0);
-	const [messageid, setMessageIdToJump] = useState(getJumpToMessageId());
-	const [timeToJump, setTimeToJump] = useState(1000);
-	const [positionToJump, setPositionToJump] = useState<ScrollLogicalPosition>('start');
-	const { jumpToMessage } = useJumpToMessage();
-	const { idMessageReplied } = useReference();
 	const fetchData = () => {
 		loadMoreMessage();
 	};
-	useEffect(() => {
-		if (idMessageReplied) {
-			setMessageIdToJump(idMessageReplied);
-			setTimeToJump(0);
-			setPositionToJump('center');
-		} else {
-			setMessageIdToJump(getJumpToMessageId());
-			setTimeToJump(1000);
-			setPositionToJump('start');
-		}
-	}, [getJumpToMessageId, idMessageReplied]);
+
+	const listRef = useRef<any>({});
+	const rowHeights = useRef<any>({});
 
 	useEffect(() => {
-		let timeoutId: NodeJS.Timeout | null = null;
-		if (messageid) {
-			timeoutId = setTimeout(() => {
-				jumpToMessage(messageid, positionToJump);
-			}, timeToJump);
+		if (messages.length > 0) {
+			scrollToBottom();
 		}
-		return () => {
-			if (timeoutId) {
-				clearTimeout(timeoutId);
+	}, [messages.length]);
+
+	function getRowHeight(index: any) {
+		return rowHeights.current[index] + 1 || 60;
+	}
+
+	function Row({ index, style }: any) {
+		const rowRef = useRef<any>({});
+		useEffect(() => {
+			if (rowRef.current) {
+				const newHeight = rowRef.current.clientHeight;
+				setRowHeight(index, newHeight);
 			}
-		};
-	}, [messageid, jumpToMessage]);
+		}, [rowRef.current]);
+		return (
+			<div style={style}>
+				<div ref={rowRef}>
+					<ChannelMessage
+						mode={mode}
+						key={messages[index].id}
+						lastSeen={messages[index].id === unreadMessageId && messages[index].id !== lastMessageId}
+						message={messages[index]}
+						preMessage={messages.length > 0 ? messages[index - 1] : undefined}
+						channelId={channelId}
+						channelLabel={channelLabel || ''}
+					/>
+				</div>
+			</div>
+		);
+	}
 
-	const handleScroll = (e: any) => {
-		setPosition(e.target.scrollTop);
+	function setRowHeight(index: any, size: any) {
+		listRef.current.resetAfterIndex(0);
+		rowHeights.current = { ...rowHeights.current, [index]: size };
+	}
+
+	function scrollToBottom() {
+		console.log('on Bottom:', messages.length - 50)
+		if (listRef.current) {
+			if (messages.length > 50) {
+				listRef.current?.scrollToItem(60, "end");
+			} else {
+				listRef.current?.scrollToItem(messages.length, "end");
+			}
+		}
+	}
+
+	const onScroll = ({ scrollOffset }: any) => {
+		if (scrollOffset < 50 && hasMoreMessage && messages.length > 49) {
+			loadMoreMessage();
+		}
 	};
+
 
 	return (
 		<div
 			className="bg-[#26262B] relative"
-			id="scrollLoading"
-			ref={containerRef}
 			style={{
 				height: '100%',
-				overflowY: 'scroll',
 				display: 'flex',
 				flexDirection: 'column-reverse',
 				overflowX: 'hidden',
 			}}
 		>
-			<InfiniteScroll
-				dataLength={messages.length}
-				next={fetchData}
-				style={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden' }}
-				inverse={true}
-				hasMore={hasMoreMessage}
-				loader={<h4 className="h-[50px] py-[18px] text-center">Loading...</h4>}
-				scrollableTarget="scrollLoading"
-				refreshFunction={fetchData}
-				pullDownToRefresh={containerRef.current !== null && containerRef.current.scrollHeight > containerRef.current.clientHeight}
-				pullDownToRefreshThreshold={50}
-				onScroll={handleScroll}
-			>
-				<ChatWelcome type={type} name={channelLabel} avatarDM={avatarDM} />
-				{messages.map((message, i) => (
-					<ChannelMessage
-						mode={mode}
-						key={message.id}
-						lastSeen={message.id === unreadMessageId && message.id !== lastMessageId}
-						message={message}
-						preMessage={messages.length > 0 ? messages[i - 1] : undefined}
-						channelId={channelId}
-						channelLabel={channelLabel || ''}
-					/>
-				))}
-			</InfiniteScroll>
+			<AutoSizer style={{ width: '100%', height: '100%' }}>
+				{({ height, width }) => (
+					<List
+						height={height - 15}
+						itemCount={messages.length}
+						itemSize={getRowHeight}
+						ref={listRef}
+						width={width}
+						onScroll={onScroll}
+					>
+						{Row}
+					</List>
+				)}
+
+			</AutoSizer>
 		</div>
 	);
 }
