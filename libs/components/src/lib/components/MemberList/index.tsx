7081f9c1cc123c51c1d535647716a14b4930ed2a
anbuihoang <an.buihoang@ncc.asia>
2024-05-06 14:48:49 +0700

    Update font discord

--- libs/components/src/lib/components/MemberList/index.tsx
+++ libs/components/src/lib/components/MemberList/index.tsx
@@ -12,7 +12,7 @@ function MemberList() {
 	return (
 		<div className="self-stretch h-[268px] flex-col justify-start items-start flex p-4 gap-[24px] w-full">
 			<div className="w-full">
-				<p className="mb-3 text-[#AEAEAE] text-[14px] font-bold flex items-center gap-[4px] font-title text-xs tracking-wide uppercase">
+				<p className="mb-3 text-[#AEAEAE] text-[14px] font-semibold flex items-center gap-[4px] font-title text-xs tracking-wide uppercase">
 					MEMBER - {onlineMembers.length}
 				</p>
 				<div className="flex flex-col gap-4 ">
@@ -22,7 +22,7 @@ function MemberList() {
 				</div>
 				{offlineMembers.length > 0 && (
 					<>
-						<p className="mt-7 mb-3 text-[#AEAEAE] text-[14px] font-bold flex items-center gap-[4px] font-title text-xs tracking-wide uppercase">
+						<p className="mt-7 mb-3 text-[#AEAEAE] text-[14px] font-semibold flex items-center gap-[4px] font-title text-xs tracking-wide uppercase">
 							Offline - {offlineMembers.length}
 						</p>
 						<div className="flex flex-col gap-4 mb-3">
