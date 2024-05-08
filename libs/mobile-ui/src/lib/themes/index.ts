c5f319515885944374dd3f19d01ed1825725f0b0
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-05-08 10:17:48 +0700

    push folder

---
+++ libs/mobile-ui/src/lib/themes/index.ts
@@ -0,0 +1,17 @@
+import Colors from "./Colors";
+import Fonts, { size } from "./Fonts";
+import {
+	Metrics,
+	horizontalScale,
+	moderateScale,
+	verticalScale,
+} from "./Metrics";
+
+export {
+	Colors,
+	Metrics,
+	horizontalScale,
+	moderateScale,
+	verticalScale,
+	size,
+};
