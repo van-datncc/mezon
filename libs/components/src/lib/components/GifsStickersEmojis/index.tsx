b8d8880d6778d58f1497c66c1144093bfdb65dfc
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-05-16 17:00:38 +0700

    fixed bug Pop-up emotions are overlapped

--- libs/components/src/lib/components/GifsStickersEmojis/index.tsx
+++ libs/components/src/lib/components/GifsStickersEmojis/index.tsx
@@ -59,26 +59,29 @@ const GifStickerEmojiPopup = ({ messageEmoji, emojiAction, mode }: GifStickerEmo
 			className={`w-[370px] sbm:w-[500px] h-fit rounded-lg dark:bg-bgSecondary bg-bgLightMode shadow shadow-neutral-900 ${emojiAction === EmojiPlaces.EMOJI_REACTION || emojiAction === EmojiPlaces.EMOJI_REACTION_BOTTOM ? 'min-h-[400px]' : 'min-h-[500px]'}`}
 		>
 			<div className="w-full">
-				<div className="flex justify-start flex-row mt-3 border-b border-blue-500 pb-1 pt-1">
-					<button
-						className={`relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.GIFS ? ' font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-						onClick={() => handleTabClick(SubPanelName.GIFS)}
-					>
-						Gifs
-					</button>
-					<button
-						className={`relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.STICKERS ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-						onClick={() => handleTabClick(SubPanelName.STICKERS)}
-					>
-						Stickers
-					</button>
-					<button
-						className={`relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.EMOJI ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-						onClick={() => handleTabClick(SubPanelName.EMOJI)}
-					>
-						Emoji
-					</button>
-				</div>
+				{emojiAction !== EmojiPlaces.EMOJI_REACTION && emojiAction !== EmojiPlaces.EMOJI_REACTION_BOTTOM && (
+					<div className="flex justify-start flex-row mt-3 border-b border-blue-500 pb-1 pt-1">
+						<button
+							className={`relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.GIFS ? ' font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
+							onClick={() => handleTabClick(SubPanelName.GIFS)}
+						>
+							Gifs
+						</button>
+						<button
+							className={`relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.STICKERS ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
+							onClick={() => handleTabClick(SubPanelName.STICKERS)}
+						>
+							Stickers
+						</button>
+						<button
+							className={`relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.EMOJI ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
+							onClick={() => handleTabClick(SubPanelName.EMOJI)}
+						>
+							Emoji
+						</button>
+					</div>
+				)}
+
 				<InputSearch />
 			</div>
 
