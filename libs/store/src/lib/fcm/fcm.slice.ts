0fb424e8848a97b7013841cda3534fd9a6909193
Nhan Nguyen <nguyentran@ncc.asia>
2024-05-20 11:13:42 +0700

    fix conflict

--- libs/store/src/lib/fcm/fcm.slice.ts
+++ libs/store/src/lib/fcm/fcm.slice.ts
@@ -1,11 +1,32 @@
 import {  createAsyncThunk } from '@reduxjs/toolkit';
-import { ensureSession, getMezonCtx } from '../helpers';
+import { MezonValueContext, ensureSession, getMezonCtx } from '../helpers';
+import memoize from 'memoizee';
 
-export const registFcmDeviceToken = createAsyncThunk('fcm/registFcmDeviceToken', async ( tokenId : string, thunkAPI) => {
+const REGIS_FCM_TOKEN_CACHED_TIME = 1000 * 60 * 3;
+
+type FcmDeviceTokenPayload = {
+	tokenId: string;
+	deviceId: string;
+	platform?: string;
+};
+
+export const registFcmDeviceTokenCached = memoize(
+	(mezon: MezonValueContext, tokenId: string, deviceId: string, platform: string) =>
+		mezon.client.registFCMDeviceToken(mezon.session, tokenId, deviceId, platform || ""),
+	{
+		promise: true,
+		maxAge: REGIS_FCM_TOKEN_CACHED_TIME,
+		normalizer: (args) => {
+			return args[1] + args[2] + args[3];
+		},
+	},
+);
+
+export const registFcmDeviceToken = createAsyncThunk('fcm/registFcmDeviceToken', async ( {tokenId, deviceId, platform}:FcmDeviceTokenPayload, thunkAPI) => {
 	try {
-		
-		const mezon = await ensureSession(getMezonCtx(thunkAPI));
-		const response = await mezon.client.registFCMDeviceToken(mezon.session, tokenId);
+		const mezon = await ensureSession(getMezonCtx(thunkAPI));		
+
+		const response = await registFcmDeviceTokenCached(mezon, tokenId, deviceId, platform || "");
 		if (!response) {
 			return thunkAPI.rejectWithValue([]);
 		}
@@ -15,4 +36,5 @@ export const registFcmDeviceToken = createAsyncThunk('fcm/registFcmDeviceToken',
 		return thunkAPI.rejectWithValue(errmsg.message);
 	}
 });
+
 export const fcmActions = {  registFcmDeviceToken };
