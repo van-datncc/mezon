c80174398287b8d6d37d19315da37d2b7e005259
Nhan Nguyen <nguyentran@ncc.asia>
2024-05-22 09:21:09 +0700

    change login button to redirect mode

--- libs/components/src/lib/components/LoginForm/GoogleButton/index.tsx
+++ libs/components/src/lib/components/LoginForm/GoogleButton/index.tsx
@@ -6,7 +6,7 @@ const GoogleButtonLogin: React.FC = () => {
 	const { loginByGoogle } = useAuth();
 	const googleLogin = useGoogleLogin({
 		flow: 'auth-code',
-		ux_mode: 'popup',
+		ux_mode: 'redirect', //'popup'
 		onSuccess: async ({ code }) => {
 			await loginByGoogle(code);
 		},
