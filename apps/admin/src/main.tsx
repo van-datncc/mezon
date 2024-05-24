30b12a4bb8cfebbf5b834f7554bdd1b084adb44b
Nhan Nguyen <nguyentran@ncc.asia>
2024-05-24 11:41:51 +0700

    remove voice

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,14 +1,11 @@
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { StrictMode } from 'react';
 import App from './app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</StrictMode>,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
