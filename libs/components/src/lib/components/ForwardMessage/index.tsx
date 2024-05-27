14d083fd8d631cbeb3e5f376546493da71252ffd
anbuihoang <an.buihoang@ncc.asia>
2024-05-27 17:18:38 +0700

    search does not work in forward message

--- libs/components/src/lib/components/ForwardMessage/index.tsx
+++ libs/components/src/lib/components/ForwardMessage/index.tsx
@@ -1,12 +1,12 @@
 import { Icons } from '@mezon/components';
-import { useApp, useChannels, useDirect, useSendForwardMessage } from '@mezon/core';
+import { useApp, useAuth, useChannels, useDirect, useSendForwardMessage } from '@mezon/core';
 import { RootState, channelsActions, useAppDispatch } from '@mezon/store';
 import { useMezon } from '@mezon/transport';
-import { ChannelStatusEnum } from '@mezon/utils';
+import { ChannelStatusEnum, removeDuplicatesById } from '@mezon/utils';
 import { Button, Checkbox, Label, Modal } from 'flowbite-react';
 import { getSelectedMessage, toggleIsShowPopupForwardFalse } from 'libs/store/src/lib/forwardMessage/forwardMessage.slice';
 import { ChannelStreamMode, ChannelType } from 'mezon-js';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import MessageContent from '../MessageWithUser/MessageContent';
 type ModalParam = {
@@ -20,7 +20,7 @@ type OpjectSend = {
 	channel_label?: string;
 };
 const ForwardMessageModal = ({ openModal, onClose }: ModalParam) => {
-	const {appearanceTheme} = useApp();
+	const { appearanceTheme } = useApp();
 	const dispatch = useAppDispatch();
 	const { listDM: dmGroupChatList } = useDirect();
 	const { listChannels } = useChannels();
@@ -28,10 +28,14 @@ const ForwardMessageModal = ({ openModal, onClose }: ModalParam) => {
 	const isLoading = useSelector((state: RootState) => state.channels.loadingStatus);
 	const listGroup = dmGroupChatList.filter((groupChat) => groupChat.type === 2);
 	const listDM = dmGroupChatList.filter((groupChat) => groupChat.type === 3);
-	const [selectedObjectIdSends, setSelectedObjectIdSends] = useState<OpjectSend[]>([]);
 	const { sendForwardMessage } = useSendForwardMessage();
 	const mezon = useMezon();
+	const { userProfile } = useAuth();
 	const selectedMessage = useSelector(getSelectedMessage);
+	const accountId = userProfile?.user?.id ?? '';
+
+	const [selectedObjectIdSends, setSelectedObjectIdSends] = useState<OpjectSend[]>([]);
+	const [searchText, setSearchText] = useState('');
 
 	useEffect(() => {
 		if (isLoading === 'loaded') {
@@ -73,73 +77,227 @@ const ForwardMessageModal = ({ openModal, onClose }: ModalParam) => {
 		dispatch(toggleIsShowPopupForwardFalse());
 	};
 
+	const listMemSearch = useMemo(() => {
+		const listDMSearch = listDM.length
+			? listDM.map((itemDM: any) => {
+					return {
+						id: itemDM?.user_id[0] ?? '',
+						name: itemDM?.channel_label ?? '',
+						avatarUser: itemDM?.channel_avatar[0] ?? '',
+						idDM: itemDM?.id ?? '',
+						typeChat: 3,
+					};
+				})
+			: [];
+		const listGroupSearch = listGroup.length
+			? listGroup.map((itemGr: any) => {
+					return {
+						id: itemGr?.channel_id ?? '',
+						name: itemGr?.channel_label ?? '',
+						avatarUser: '/assets/images/avatar-group.png' ?? '',
+						idDM: itemGr?.id ?? '',
+						typeChat: 2,
+					};
+				})
+			: [];
+
+		const listSearch = [...listDMSearch, ...listGroupSearch];
+		return removeDuplicatesById(listSearch.filter((item) => item.id !== accountId));
+	}, [accountId, listDM, listGroup]);
+
+	const listChannelSearch = useMemo(() => {
+		const list = listChannels.map((item) => {
+			return {
+				id: item?.id ?? '',
+				name: item?.channel_label ?? '',
+				subText: item?.category_name ?? '',
+				icon: '#',
+				type: item?.type ?? '',
+				clanId: item?.clan_id ?? '',
+				channel_label: item?.channel_label ?? '',
+			};
+		});
+		return list;
+	}, [listChannels]);
+
+	const isNoResult =
+		!listChannelSearch.filter((item) => item.name.indexOf(searchText) > -1).length &&
+		!listMemSearch.filter((item: any) => item.name.indexOf(searchText) > -1).length;
+
+	const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+			e.preventDefault();
+		}
+	};
+
 	return (
-		<Modal theme={{ content: { base: 'w-[550px]' } }} show={openModal} dismissible={true} onClose={onClose}>
-			<div className="dark:bg-bgPrimary bg-bgLightMode pt-4 rounded">
+		<Modal className="bg-bgModalDark" theme={{ content: { base: 'w-[550px]' } }} show={openModal} dismissible={true} onClose={onClose}>
+			<div className="dark:bg-bgSecondary bg-bgLightMode pt-4 rounded">
 				<div>
 					<h1 className="dark:text-white text-textLightTheme text-xl font-semibold text-center">Forward Message</h1>
 				</div>
 				<div className="px-4 pt-4">
 					<input
 						type="text"
-						className="dark:text-[#B5BAC1] text-textLightTheme outline-none w-full h-10 p-[10px] dark:bg-[#26262B] bg-bgModifierHoverLight text-base rounded placeholder:text-sm"
+						className="dark:text-[#B5BAC1] text-textLightTheme outline-none w-full h-10 p-[10px] dark:bg-bgTertiary bg-bgModifierHoverLight text-base rounded placeholder:text-sm"
 						placeholder="Search"
+						onChange={(e) => setSearchText(e.target.value)}
+						onKeyDown={(e) => handleInputKeyDown(e)}
 					/>
-					<div className={`mt-4 mb-2 overflow-y-auto h-[300px] ${appearanceTheme === "light" ? "customScrollLightMode" : "thread-scroll"}`}>
-						{listDM.map((DM, index) => (
-							<div key={DM.id} className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded">
-								<div className="flex flex-1 flex-row items-center">
-									<img src={DM.channel_avatar?.at(0)} alt="" className="size-5 object-cover rounded-full mr-[10px] my-[5px]" />
-									<p className="dark:text-[#B5BAC1] text-textLightTheme">{DM.channel_label}</p>
-								</div>
-								<Checkbox
-									className="w-4 h-4 focus:ring-transparent"
-									id={`checkbox-item-${index}`}
-									onChange={() => handleToggle(DM.id, DM.type || 0)}
-								/>
-							</div>
-						))}
-						{listGroup.map((group, index) => (
-							<div key={group.id} className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded">
-								<div className="flex flex-1 flex-row items-center">
-									<img
-										src={`/assets/images/avatar-group.png`}
-										alt=""
-										className="size-10 min-w-10 min-h-10 object-cover rounded-full mr-[10px] my-[5px]"
-									/>
-									<p className="dark:text-[#B5BAC1] text-textLightTheme">{group.channel_label}</p>
-								</div>
-								<Checkbox
-									className="w-4 h-4 focus:ring-transparent"
-									id={`checkbox-item-${index}`}
-									onChange={() => handleToggle(group.id, group.type || 0)}
-								/>
-							</div>
-						))}
-						{listChannel.map((channel, index) => (
-							<div key={channel.id} className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded">
-								<div className="flex flex-1 flex-row items-center gap-1">
-									{channel.channel_private === ChannelStatusEnum.isPrivate ? (
-										<Icons.HashtagLocked defaultSize="size-5 min-w-5 min-h-5" />
-									) : (
-										<Icons.Hashtag defaultSize="size-5 min-w-5 min-h-5" />
-									)}
-									<p className="dark:text-[#B5BAC1] text-textLightTheme">{channel.channel_label}</p>
-								</div>
-								<Checkbox
-									className="w-4 h-4 focus:ring-transparent"
-									id={`checkbox-item-${index}`}
-									onChange={() => handleToggle(channel.id, channel.type || 0, channel.clan_id, channel.channel_label || '')}
-								/>
-							</div>
-						))}
+					<div className={`mt-4 mb-2 overflow-y-auto h-[300px] ${appearanceTheme === 'light' ? 'customScrollLightMode' : 'thread-scroll'}`}>
+						{!searchText.startsWith('@') && !searchText.startsWith('#') ? (
+							<>
+								{listMemSearch.length
+									? listMemSearch
+											.filter((item: any) => item.name.indexOf(searchText) > -1)
+											.slice(0, 7)
+											.map((item: any) => (
+												<div
+													key={item.id}
+													className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded"
+												>
+													<div className="flex flex-1 flex-row items-center">
+														<img
+															src={item.avatarUser}
+															alt=""
+															className="size-5 object-cover rounded-full mr-[10px] my-[5px]"
+														/>
+														<p className="dark:text-[#B5BAC1] text-textLightTheme">{item.name}</p>
+													</div>
+													<Checkbox
+														className="w-4 h-4 focus:ring-transparent"
+														id={`checkbox-item-${item.idDM}`}
+														onChange={() => handleToggle(item.idDM, item.typeChat || 0)}
+													/>
+												</div>
+											))
+									: null}
+								{listChannelSearch.length
+									? listChannelSearch
+											.filter((item) => item.name.indexOf(searchText) > -1)
+											.slice(0, 8)
+											.map((channel: any) => {
+												return (
+													<div
+														key={channel.id}
+														className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded"
+													>
+														<div className="flex flex-1 flex-row items-center gap-1">
+															{channel.channel_private === ChannelStatusEnum.isPrivate ? (
+																<Icons.HashtagLocked defaultSize="size-5 min-w-5 min-h-5" />
+															) : (
+																<Icons.Hashtag defaultSize="size-5 min-w-5 min-h-5" />
+															)}
+															<p className="dark:text-[#B5BAC1] text-textLightTheme">{channel.name}</p>
+														</div>
+														<Checkbox
+															className="w-4 h-4 focus:ring-transparent"
+															id={`checkbox-item-${channel.id}`}
+															onChange={() =>
+																handleToggle(
+																	channel.id,
+																	channel.type || 0,
+																	channel.clanId,
+																	channel.channel_label || '',
+																)
+															}
+														/>
+													</div>
+												);
+											})
+									: null}
+								{isNoResult && <span className=" flex flex-row justify-center">Can't seem to find what you're looking for?</span>}
+							</>
+						) : (
+							<>
+								{searchText.startsWith('@') && (
+									<>
+										<span className="text-textPrimary text-left opacity-60 text-[11px] pb-1 uppercase">
+											Search friend and users
+										</span>
+										{listMemSearch.length ? (
+											listMemSearch
+												.filter((item: any) => item.name.indexOf(searchText.substring(1)) > -1)
+												.slice(0, 25)
+												.map((item: any) => {
+													return (
+														<div
+															key={item.id}
+															className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded"
+														>
+															<div className="flex flex-1 flex-row items-center">
+																<img
+																	src={item.avatarUser}
+																	alt=""
+																	className="size-5 object-cover rounded-full mr-[10px] my-[5px]"
+																/>
+																<p className="dark:text-[#B5BAC1] text-textLightTheme">{item.name}</p>
+															</div>
+															<Checkbox
+																className="w-4 h-4 focus:ring-transparent"
+																id={`checkbox-item-${item.id}`}
+																onChange={() => handleToggle(item.idDM, item.typeChat || 0)}
+															/>
+														</div>
+													);
+												})
+										) : (
+											<></>
+										)}
+									</>
+								)}
+								{searchText.startsWith('#') && (
+									<>
+										<span className="text-left opacity-60 text-[11px] pb-1 uppercase">Searching channel</span>
+										{listChannelSearch.length ? (
+											listChannelSearch
+												.filter((item) => item.name.indexOf(searchText.substring(1)) > -1)
+												.slice(0, 25)
+												.map((channel: any) => {
+													return (
+														<div
+															key={channel.id}
+															className="flex items-center px-4 py-1 dark:hover:bg-bgPrimary1 hover:bg-bgLightModeThird rounded"
+														>
+															<div className="flex flex-1 flex-row items-center gap-1">
+																{channel.channel_private === ChannelStatusEnum.isPrivate ? (
+																	<Icons.HashtagLocked defaultSize="size-5 min-w-5 min-h-5" />
+																) : (
+																	<Icons.Hashtag defaultSize="size-5 min-w-5 min-h-5" />
+																)}
+																<p className="dark:text-[#B5BAC1] text-textLightTheme">{channel.name}</p>
+															</div>
+															<Checkbox
+																className="w-4 h-4 focus:ring-transparent"
+																id={`checkbox-item-${channel.id}`}
+																onChange={() =>
+																	handleToggle(
+																		channel.id,
+																		channel.type || 0,
+																		channel.clanId,
+																		channel.channel_label || '',
+																	)
+																}
+															/>
+														</div>
+													);
+												})
+										) : (
+											<></>
+										)}
+									</>
+								)}
+							</>
+						)}
 					</div>
 				</div>
 				<div className="px-4">
 					<div className="mb-2 block">
 						<Label htmlFor="clearAfter" value="Shared content" className="dark:text-[#B5BAC1] text-xs uppercase font-semibold" />
 					</div>
-					<div className={`h-20 overflow-y-auto dark:bg-bgProfileBody bg-bgLightModeThird p-[5px] rounded ${appearanceTheme === "light" ? "customScrollLightMode" : "thread-scroll"}`}>
+					<div
+						className={`h-20 overflow-y-auto dark:bg-bgProfileBody bg-bgLightModeThird p-[5px] rounded ${appearanceTheme === 'light' ? 'customScrollLightMode' : 'thread-scroll'}`}
+					>
 						<MessageContent message={selectedMessage} newMessage="" />
 					</div>
 					<div className="flex justify-end p-4 rounded-b gap-4">
