7c62a6d3b35cadc4ce7f4876ccf0c8e9c344aabb
NghiaDT <nghiadt22.work@gmail.com>
2024-05-27 15:40:49 +0700

    upload icon

--- apps/mobile/src/app/screens/home/homedrawer/components/EmojiPicker/EmojiSelector/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/EmojiPicker/EmojiSelector/index.tsx
@@ -3,25 +3,44 @@ import { MemberListIcon, SmilingFaceIcon } from "@mezon/mobile-components";
 import { BicycleIcon, BowlIcon, GameControllerIcon, HeartIcon, LeafIcon, ObjectIcon, RibbonIcon } from "libs/mobile-components/src/lib/icons";
 import { ScrollView } from "react-native-gesture-handler";
 import styles from "./styles";
+import { TouchableOpacity } from "@gorhom/bottom-sheet";
 
 type EmojiSelectorProps = {
     onSelected: (url: string) => void;
     searchText: string;
 };
 
+const cate = [
+    <MemberListIcon />,
+    <SmilingFaceIcon height={24} width={24} />,
+    <GameControllerIcon />,
+    <HeartIcon />,
+    <ObjectIcon />,
+    <LeafIcon />,
+    <BicycleIcon />,
+    <BowlIcon />,
+    <RibbonIcon />,
+    <MemberListIcon />,
+    <SmilingFaceIcon height={24} width={24} />,
+    <GameControllerIcon />,
+    <HeartIcon />,
+    <ObjectIcon />,
+    <LeafIcon />,
+    <BicycleIcon />,
+    <BowlIcon />,
+    <RibbonIcon />
+]
+
 export default function EmojiSelector({ }: EmojiSelectorProps) {
     return (
         <>
             <ScrollView horizontal contentContainerStyle={styles.cateContainer}>
-                <MemberListIcon />
-                <SmilingFaceIcon height={24} width={24}/>
-                <GameControllerIcon />
-                <HeartIcon />
-                <ObjectIcon />
-                <LeafIcon />
-                <BicycleIcon />
-                <BowlIcon />
-                <RibbonIcon />
+                {cate.map((item, index) => (
+                    <TouchableOpacity
+                        key={index} style={styles.cateItem}>
+                        {item}
+                    </TouchableOpacity>
+                ))}
             </ScrollView>
         </>
     )
