44e151b6d18b95519d7b483f3fdd886c17b636fa
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-05-27 11:55:44 +0700

    update: optimize show gif picker

--- apps/mobile/src/app/screens/home/homedrawer/components/EmojiPicker/GifSelector/GifItem/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/EmojiPicker/GifSelector/GifItem/index.tsx
@@ -22,11 +22,21 @@ export default function GiftItem({ loading, data, onPress }: GifItemProps) {
 		<View style={styles.container}>
 			{data &&
 				Array.isArray(data) &&
-				data.map((item, index) => (
-					<TouchableOpacity style={styles.content} onPress={() => handlePressGif(item.media_formats.gif.url)} key={index.toString()}>
-						<FastImage source={{ uri: item.media_formats.gif.url }} style={{ height: '100%', width: '100%' }} />
-					</TouchableOpacity>
-				))}
+				data.map((item, index) => {
+					return (
+						<TouchableOpacity style={styles.content} onPress={() => handlePressGif(item.media_formats.gif.url)} key={index.toString()}>
+							<FastImage
+								source={{
+									uri: item.media_formats.tinygif.url,
+									cache: FastImage.cacheControl.web,
+									priority: FastImage.priority.high
+								}}
+								resizeMode={FastImage.resizeMode.cover}
+								style={{ height: '100%', width: '100%' }}
+							/>
+						</TouchableOpacity>
+					);
+				})}
 		</View>
 	);
 }
