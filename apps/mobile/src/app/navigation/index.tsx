5540839917833d89728f027c94366e26e0c954cb
NghiaDT <nghiadt22.work@gmail.com>
2024-06-11 09:56:36 +0700

    revert .env

--- apps/mobile/src/app/navigation/index.tsx
+++ apps/mobile/src/app/navigation/index.tsx
@@ -11,10 +11,8 @@ import { toastConfig } from '../configs/toastConfig';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 const mezon: CreateMezonClientOptions = {
-	// host: process.env.NX_CHAT_APP_API_HOST as string,
-	// key: process.env.NX_CHAT_APP_API_KEY as string,
-	host: "mezon.vn",
-	key: "m3zonPr0dkey",
+	host: process.env.NX_CHAT_APP_API_HOST as string,
+	key: process.env.NX_CHAT_APP_API_KEY as string,
 	port: process.env.NX_CHAT_APP_API_PORT as string,
 	ssl: process.env.NX_CHAT_APP_API_SECURE === 'true',
 };
