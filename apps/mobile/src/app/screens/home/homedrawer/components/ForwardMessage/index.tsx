b314f4560023043dbec3ab9e47be879824bf532e
PhatNCC <phat.phanvan@ncc.asia>
2024-06-19 17:44:03 +0700

    fix UI forward message

--- apps/mobile/src/app/screens/home/homedrawer/components/ForwardMessage/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ForwardMessage/index.tsx
@@ -1,17 +1,20 @@
 import { useAuth, useChannels, useDirect, useSendForwardMessage } from '@mezon/core';
-import { CrossIcon, HashSignIcon, HashSignLockIcon } from '@mezon/mobile-components';
-import { Colors, Fonts, size } from '@mezon/mobile-ui';
+import { CheckIcon, CrossIcon, HashSignIcon, HashSignLockIcon, UserGroupIcon } from '@mezon/mobile-components';
+import { Colors } from '@mezon/mobile-ui';
 import { useMezon } from '@mezon/transport';
 import { ChannelStatusEnum, IMessageWithUser, removeDuplicatesById } from '@mezon/utils';
 import { getSelectedMessage } from 'libs/store/src/lib/forwardMessage/forwardMessage.slice';
 import { ChannelStreamMode, ChannelType } from 'mezon-js';
-import React, { useEffect, useMemo, useState } from 'react';
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { default as CheckBox } from 'react-native-bouncy-checkbox';
 import { TextInput } from 'react-native-gesture-handler';
 import Modal from 'react-native-modal';
 import { useSelector } from 'react-redux';
 import MessageItem from '../../MessageItem';
+import { styles } from './styles';
+import Toast from 'react-native-toast-message';
+import { useTranslation } from 'react-i18next';
 
 type OpjectSend = {
 	id: string;
@@ -38,6 +41,7 @@ const ForwardMessageModal = ({ show, onClose, message }: ForwardMessageModalProp
 	const listDM = dmGroupChatList.filter((groupChat) => groupChat.type === 3);
 	const listGroup = dmGroupChatList.filter((groupChat) => groupChat.type === 2);
 	const accountId = userProfile?.user?.id ?? '';
+	const { t } = useTranslation('message');
 
 	const listMemSearch = useMemo(() => {
 		const listDMSearch = listDM.length
@@ -94,8 +98,6 @@ const ForwardMessageModal = ({ show, onClose, message }: ForwardMessageModalProp
 
 	const isChecked = (id: string, type: number) => {
 		const existingIndex = selectedObjectIdSends.findIndex((item) => item.id === id && item.type === type);
-		// console.log(id, type, existingIndex);
-
 		return existingIndex !== -1;
 	};
 
@@ -119,6 +121,13 @@ const ForwardMessageModal = ({ show, onClose, message }: ForwardMessageModalProp
 					);
 				}
 			}
+			Toast.show({
+				type: 'success',
+				props: {
+					text2: t('forwardMessagesSuccessfully'),
+					leadingIcon: <CheckIcon color={Colors.green} width={30} height={17} />
+				}
+			});
 		} catch (error) {
 			console.log('Tom log  => error', error);
 		}
@@ -134,15 +143,21 @@ const ForwardMessageModal = ({ show, onClose, message }: ForwardMessageModalProp
 				return (
 					<View style={styles.item} key={index.toString()}>
 						<View style={styles.memberContent} key={index.toString()}>
-							<Image source={{ uri: item.avatarUser }} style={styles.memberAvatar} />
-							<Text style={styles.memberName}>{item.name}</Text>
+							{item?.typeChat === 2 ? (
+								<View style={styles.groupAvatar}>
+									<UserGroupIcon />
+								</View>
+							): (
+								<Image source={{ uri: item.avatarUser }} style={styles.memberAvatar} />
+							)}
+							<Text style={styles.memberName} numberOfLines={1}>{item.name}</Text>
 						</View>
 
 						<View>
 							<CheckBox
 								isChecked={isChecked(item.idDM, item.typeChat || 0)}
 								size={24}
-								fillColor={Colors.green}
+								fillColor={Colors.textViolet}
 								unFillColor={Colors.bgGrayLight}
 								innerIconStyle={{ borderWidth: 2 }}
 								onPress={() => {
@@ -175,7 +190,7 @@ const ForwardMessageModal = ({ show, onClose, message }: ForwardMessageModalProp
 							<CheckBox
 								isChecked={isChecked(channel.id, channel.type || 0)}
 								size={24}
-								fillColor={Colors.green}
+								fillColor={Colors.textViolet}
 								unFillColor={Colors.bgGrayLight}
 								innerIconStyle={{ borderWidth: 2 }}
 								onPress={() => {
@@ -188,159 +203,71 @@ const ForwardMessageModal = ({ show, onClose, message }: ForwardMessageModalProp
 			});
 	};
 
-	useEffect(() => {
-		// console.log("vvv", message);
-	}, [message]);
-
 	return (
-		<View>
-			<Modal
-				isVisible={show}
-				animationIn={'fadeIn'}
-				hasBackdrop={true}
-				coverScreen={true}
-				avoidKeyboard={false}
-				onBackdropPress={onClose}
-				backdropColor={'rgba(0,0,0, 0.7)'}
-			>
-				<View style={styles.sheetContainer}>
-					<View style={styles.headerModal}>
-						<TouchableOpacity onPress={onClose}>
-							<CrossIcon height={16} width={16} />
-						</TouchableOpacity>
-						<Text style={styles.headerText}>Forward Message</Text>
-						<View style={{ width: 16 }}></View>
-					</View>
+		<Modal
+			isVisible={show}
+			animationIn={'fadeIn'}
+			hasBackdrop={true}
+			coverScreen={true}
+			avoidKeyboard={false}
+			onBackdropPress={onClose}
+			backdropColor={'rgba(0,0,0, 0.7)'}
+		>
+			<View style={styles.sheetContainer}>
+				<View style={styles.headerModal}>
+					<TouchableOpacity onPress={onClose}>
+						<CrossIcon height={16} width={16} />
+					</TouchableOpacity>
+					<Text style={styles.headerText}>Forward Message</Text>
+					<View style={{ width: 16 }}></View>
+				</View>
 
+				<View style={styles.searchWrapper}>
 					<View style={styles.inputWrapper}>
 						<TextInput
 							style={styles.input}
 							onChangeText={setSearchText}
 							placeholderTextColor={'white'}
-							placeholder="Search" />
+							placeholder="Search"
+						/>
 					</View>
-
-					<ScrollView style={{ marginVertical: 10 }}>
-						{!searchText.startsWith('@') && !searchText.startsWith('#') ? (
-							<>
-								{renderMember()}
-								{renderChannel()}
-							</>
-						) : (
-							<>
-								{searchText.startsWith('@') && (
-									<>
-										<Text style={{ color: 'white' }}>Search friend and users</Text>
-										{renderMember()}
-									</>
-								)}
-
-								{searchText.startsWith('#') && (
-									<>
-										<Text style={{ color: 'white' }}>Searching channel</Text>
-										{renderChannel()}
-									</>
-								)}
-							</>
-						)}
-					</ScrollView>
-					{message && <MessageItem message={message} mode={ChannelStreamMode.STREAM_MODE_CHANNEL} />}
-
-					<TouchableOpacity style={styles.btn} onPress={() => sentToMessage()}>
-						<Text style={styles.btnText}>Send</Text>
-					</TouchableOpacity>
 				</View>
-			</Modal>
-		</View>
+
+				<ScrollView>
+					{!searchText.startsWith('@') && !searchText.startsWith('#') ? (
+						<>
+							{renderMember()}
+							{renderChannel()}
+						</>
+					) : (
+						<>
+							{searchText.startsWith('@') && (
+								<>
+									<Text style={styles.typeSearch}>Search friend and users</Text>
+									{renderMember()}
+								</>
+							)}
+
+							{searchText.startsWith('#') && (
+								<>
+									<Text style={styles.typeSearch}>Searching channel</Text>
+									{renderChannel()}
+								</>
+							)}
+						</>
+					)}
+				</ScrollView>
+				<View style={styles.messageWrapper}>
+					<ScrollView>
+						{message && <MessageItem message={message} mode={ChannelStreamMode.STREAM_MODE_CHANNEL} />}
+					</ScrollView>
+				</View>
+				<TouchableOpacity style={styles.btn} onPress={() => sentToMessage()}>
+					<Text style={styles.btnText}>Send</Text>
+				</TouchableOpacity>
+			</View>
+		</Modal>
 	);
 };
-
-const styles = StyleSheet.create({
-	sheetContainer: {
-		overflow: 'hidden',
-		padding: 24,
-		backgroundColor: Colors.primary,
-		alignSelf: 'center',
-		borderRadius: 10,
-		maxHeight: '70%',
-		maxWidth: '90%',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'space-between',
-	},
-	headerModal: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
-	headerText: {
-		color: Colors.white,
-		fontSize: Fonts.size.medium,
-		textAlign: 'center',
-		fontWeight: '600',
-	},
-	btn: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: Colors.green,
-		paddingVertical: 10,
-		borderRadius: 50,
-	},
-	btnText: {
-		color: Colors.white,
-	},
-
-	inputWrapper: {
-		backgroundColor: Colors.secondary,
-		borderRadius: 10,
-		paddingHorizontal: 15,
-		marginVertical: 20,
-	},
-
-	input: {
-		color: Colors.white,
-		fontSize: Fonts.size.small,
-		paddingVertical: 0,
-		height: size.s_40,
-	},
-
-	item: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-	},
-
-	memberContent: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 10,
-		paddingVertical: 10,
-	},
-	memberAvatar: {
-		height: 36,
-		width: 36,
-		borderRadius: 50,
-	},
-	memberName: {
-		color: Colors.white,
-		fontSize: Fonts.size.small,
-	},
-	channelItem: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 10,
-		paddingVertical: 10,
-	},
-	channelName: {
-		color: Colors.white,
-		fontSize: Fonts.size.small,
-	},
-});
 
 export default ForwardMessageModal;
