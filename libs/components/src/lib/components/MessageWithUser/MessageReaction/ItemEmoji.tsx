7c3888981eb17571571cc2b908f7bda5064622d2
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-06-20 14:29:24 +0700

    4

--- libs/components/src/lib/components/MessageWithUser/MessageReaction/ItemEmoji.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageReaction/ItemEmoji.tsx
@@ -2,7 +2,7 @@ import { Icons } from '@mezon/components';
 import { useAuth, useChatReaction, useEmojiSuggestion } from '@mezon/core';
 import { reactionActions, selectCurrentChannel, selectEmojiHover, selectUserReactionPanelState } from '@mezon/store';
 import { EmojiDataOptionals, SenderInfoOptionals, calculateTotalCount, getSrcEmoji } from '@mezon/utils';
-import { forwardRef, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { forwardRef, useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import UserReactionPanel from './UserReactionPanel';
 
@@ -12,12 +12,12 @@ type EmojiItemProps = {
 };
 
 function ItemEmoji({ emoji, mode }: EmojiItemProps) {
+	const dispatch = useDispatch();
 	const userId = useAuth();
 	const { reactionMessageDispatch } = useChatReaction();
 	const userReactionPanelState = useSelector(selectUserReactionPanelState);
 	const emojiHover = useSelector(selectEmojiHover);
 	const currentChannel = useSelector(selectCurrentChannel);
-	const dispatch = useDispatch();
 	const { emojiListPNG } = useEmojiSuggestion();
 	const getUrlItem = getSrcEmoji(emoji.emoji ?? '', emojiListPNG);
 	const count = calculateTotalCount(emoji.senders);
@@ -25,27 +25,22 @@ function ItemEmoji({ emoji, mode }: EmojiItemProps) {
 	const emojiItemRef = useRef<HTMLDivElement | null>(null);
 	const userPanelRef = useRef<HTMLDivElement | null>(null);
 
-	async function reactOnExistEmoji(
-		id: string,
-		mode: number,
-		messageId: string,
-		emoji: string,
-		count: number,
-		message_sender_id: string,
-		action_delete: boolean,
-	) {
-		await reactionMessageDispatch(
-			id,
-			mode ?? 2,
-			currentChannel?.id ?? '',
-			currentChannel?.channel_label ?? '',
-			messageId ?? '',
-			emoji ?? '',
-			1,
-			message_sender_id ?? '',
-			false,
-		);
-	}
+	const reactOnExistEmoji = useCallback(
+		async (id: string, mode: number, messageId: string, emoji: string, count: number, message_sender_id: string, action_delete: boolean) => {
+			await reactionMessageDispatch(
+				id,
+				mode ?? 2,
+				currentChannel?.id ?? '',
+				currentChannel?.channel_label ?? '',
+				messageId ?? '',
+				emoji ?? '',
+				1,
+				message_sender_id ?? '',
+				false,
+			);
+		},
+		[reactionMessageDispatch, currentChannel],
+	);
 
 	const [topUserPanel, setTopUserPanel] = useState<any>();
 	const [bottomUserPanel, setBottomUserPanel] = useState<any>();
@@ -55,25 +50,25 @@ function ItemEmoji({ emoji, mode }: EmojiItemProps) {
 	const [arrowBottom, setArrowBottom] = useState<boolean>(false);
 	const [isRightLimit, setIsRightLimit] = useState<boolean>(false);
 
-	const onHoverEnter = () => {
+	const onHoverEnter = useCallback(() => {
 		dispatch(reactionActions.setEmojiHover(emoji));
 		dispatch(reactionActions.setUserReactionPanelState(true));
-	};
+	}, [dispatch, emoji]);
 
-	const onHoverLeave = () => {
+	const onHoverLeave = useCallback(() => {
 		resetState();
-	};
+	}, [dispatch]);
 
-	const resetState = () => {
+	const resetState = useCallback(() => {
 		dispatch(reactionActions.setEmojiHover(null));
 		dispatch(reactionActions.setUserReactionPanelState(false));
-	};
+	}, [dispatch]);
 
 	useEffect(() => {
 		if (!userReactionPanelState) {
 			resetState();
 		}
-	}, [userReactionPanelState]);
+	}, [userReactionPanelState, resetState]);
 
 	useLayoutEffect(() => {
 		if (emojiHover && emojiItemRef.current && userPanelRef) {
@@ -183,9 +178,8 @@ const ItemDetail = forwardRef<HTMLDivElement, ItemDetailProps>(
 					onMouseEnter={onMouse}
 					onMouseLeave={onLeave}
 					className={`rounded-md w-fit min-w-12 gap-3 h-6 flex flex-row z-40
-			cursor-pointer justify-center items-center relative
-			${userSenderCount > 0 ? 'dark:bg-[#373A54] bg-gray-200 border-blue-600 border' : 'dark:bg-[#2B2D31] bg-bgLightMode border-[#313338]'}
-		  `}
+					cursor-pointer justify-center items-center relative
+					${userSenderCount > 0 ? 'dark:bg-[#373A54] bg-gray-200 border-blue-600 border' : 'dark:bg-[#2B2D31] bg-bgLightMode border-[#313338]'}`}
 					onClick={onClickReactExist}
 				>
 					<span className="absolute left-[5px]">
--- libs/components/src/lib/components/MessageWithUser/MessageReaction/UserReactionPanel.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageReaction/UserReactionPanel.tsx
@@ -3,7 +3,7 @@ import { useAuth, useChatReaction, useEmojiSuggestion } from '@mezon/core';
 import { reactionActions, selectCurrentChannel } from '@mezon/store';
 import { AvatarComponent, NameComponent } from '@mezon/ui';
 import { EmojiDataOptionals, SenderInfoOptionals, calculateTotalCount, getSrcEmoji } from '@mezon/utils';
-import { useCallback, useState } from 'react';
+import { Fragment, useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 type UserReactionPanelProps = {
@@ -54,44 +54,22 @@ const UserReactionPanel = ({ emojiShowPanel, mode }: UserReactionPanelProps) => 
 						onMouseLeave={onLeavePanel}
 						onClick={(e) => e.stopPropagation()}
 						className={`z-50   w-[18rem]
-				dark:bg-[#28272b] bg-white border-[#28272b] rounded-sm min-h-5 max-h-[25rem] ${window.innerWidth < 640 ? 'absolute  bottom-7' : 'p-1 bottom-0'}`}
+						dark:bg-[#28272b] bg-white border-[#28272b] rounded-sm min-h-5 max-h-[25rem]
+				 		${window.innerWidth < 640 ? 'absolute  bottom-7' : 'p-1 bottom-0'}`}
 					>
-						<div>
-							<div className="flex flex-row items-center m-2 dark:text-white text-black">
-								<img src={getSrcEmoji(emojiShowPanel.emoji ?? '', emojiListPNG)} className="w-5 h-5"></img>{' '}
-								<p className="text-sm ml-2">{count}</p>
-							</div>
-							<hr className="h-[0.1rem] dark:bg-blue-900 bg-[#E1E1E1] border-none"></hr>
-						</div>
-
+						<PanelHeader emoji={emojiShowPanel.emoji} emojiListPNG={emojiListPNG} count={count} />
 						{senderList.map((sender: SenderInfoOptionals, index: number) => {
 							if (sender.count && sender.count > 0) {
 								return (
-									<div key={`${index}_${sender.sender_id}`}>
-										<div className="m-2 flex flex-row justify-start mb-2 items-center gap-2 relative">
-											<AvatarComponent id={sender.sender_id ?? ''} />
-											<NameComponent id={sender.sender_id ?? ''} />
-											<p className="text-xs absolute right-8 dark:text-textDarkTheme text-textLightTheme">{sender.count}</p>
-											{sender.sender_id === userId.userId && sender.count && sender.count > 0 && (
-												<div
-													onClick={(e: any) => {
-														e.stopPropagation();
-														removeEmojiSender(
-															emojiShowPanel.id ?? '',
-															emojiShowPanel.message_id ?? '',
-															emojiShowPanel.emoji ?? '',
-															sender.sender_id ?? '',
-															sender.count ?? 0,
-														);
-														hideSenderOnPanel(emojiShowPanel, sender.sender_id ?? '');
-													}}
-													className="right-1 absolute cursor-pointer"
-												>
-													<Icons.Close defaultSize="w-3 h-3" />
-												</div>
-											)}
-										</div>
-									</div>
+									<Fragment key={`${index}_${sender.sender_id}`}>
+										<SenderItem
+											sender={sender}
+											emojiShowPanel={emojiShowPanel}
+											userId={userId}
+											removeEmojiSender={removeEmojiSender}
+											hideSenderOnPanel={hideSenderOnPanel}
+										/>
+									</Fragment>
 								);
 							}
 							return null;
@@ -104,3 +82,56 @@ const UserReactionPanel = ({ emojiShowPanel, mode }: UserReactionPanelProps) => 
 };
 
 export default UserReactionPanel;
+
+type PanelHeaderProps = {
+	emoji: string | undefined;
+	emojiListPNG: any;
+	count: number;
+};
+
+const PanelHeader: React.FC<PanelHeaderProps> = ({ emoji, emojiListPNG, count }) => {
+	return (
+		<div>
+			<div className="flex flex-row items-center m-2 dark:text-white text-black">
+				<img src={getSrcEmoji(emoji ?? '', emojiListPNG)} className="w-5 h-5" />
+				<p className="text-sm ml-2">{count}</p>
+			</div>
+			<hr className="h-[0.1rem] dark:bg-blue-900 bg-[#E1E1E1] border-none" />
+		</div>
+	);
+};
+
+type SenderItemProps = {
+	sender: any;
+	emojiShowPanel: any;
+	userId: any;
+	removeEmojiSender: (id: string, messageId: string, emoji: string, message_sender_id: string, countRemoved: number) => Promise<void>;
+	hideSenderOnPanel: (emojiData: any, senderId: string) => void;
+};
+
+const SenderItem: React.FC<SenderItemProps> = ({ sender, emojiShowPanel, userId, removeEmojiSender, hideSenderOnPanel }) => {
+	const handleRemoveEmojiSender = async (e: React.MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation();
+		await removeEmojiSender(
+			emojiShowPanel.id ?? '',
+			emojiShowPanel.message_id ?? '',
+			emojiShowPanel.emoji ?? '',
+			sender.sender_id ?? '',
+			sender.count ?? 0,
+		);
+		hideSenderOnPanel(emojiShowPanel, sender.sender_id ?? '');
+	};
+
+	return (
+		<div className="m-2 flex flex-row justify-start mb-2 items-center gap-2 relative">
+			<AvatarComponent id={sender.sender_id ?? ''} />
+			<NameComponent id={sender.sender_id ?? ''} />
+			<p className="text-xs absolute right-8 dark:text-textDarkTheme text-textLightTheme">{sender.count}</p>
+			{sender.sender_id === userId.userId && sender.count && sender.count > 0 && (
+				<div onClick={handleRemoveEmojiSender} className="right-1 absolute cursor-pointer">
+					<Icons.Close defaultSize="w-3 h-3" />
+				</div>
+			)}
+		</div>
+	)
+}
