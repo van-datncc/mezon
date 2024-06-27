f6ba3da6d5c1a5527864c0948aa999704f9a3870
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-06-27 10:15:12 +0700

    update

--- apps/mobile/src/app/components/MemberStatus/MemberProfile/index.tsx
+++ apps/mobile/src/app/components/MemberStatus/MemberProfile/index.tsx
@@ -24,7 +24,7 @@ export default function MemberProfile({
 
     const name = useMemo(() => {
         if (user) {
-          return nickName || user.user.display_name || user.user.username
+          return nickName || user?.user?.display_name || user?.user?.username
         }
     }, [user])
     return (
