3a05bc89dbaaee3fa7004111df9bab532cf63f9e
Tungnqs <thanhlamtainguyen@gmail.com>
2024-07-02 16:22:40 +0700

    fix edit UI, fix msg hover popup, add right click dm group

--- libs/components/src/lib/components/PanelMember/PanelGroupDm.tsx
+++ libs/components/src/lib/components/PanelMember/PanelGroupDm.tsx
@@ -1,12 +1,40 @@
-import ItemPanelMember from "./ItemPanelMember";
+import { Dropdown } from 'flowbite-react';
+import ItemPanelMember from './ItemPanelMember';
 
 const PanelGroupDM = () => {
-    return(
-        <>
-            <ItemPanelMember children="Change Icon" />
-            <ItemPanelMember children="Leave Group" />
-        </>
-    );
-}
+	return (
+		<>
+			<div className="border-b dark:border-[#2e2f34]">
+				<ItemPanelMember children="Mark as read" />
+			</div>
+			<div className="border-b dark:border-[#2e2f34]">
+				<ItemPanelMember children="Invites" />
+				<ItemPanelMember children="Change icon" />
+			</div>
+			<div className="border-b dark:border-[#2e2f34]">
+				<Dropdown
+					trigger="hover"
+					dismissOnClick={false}
+					renderTrigger={() => (
+						<div>
+							<ItemPanelMember children="Invite to Clan" dropdown />
+						</div>
+					)}
+					label=""
+					placement="left-start"
+					className="dark:!bg-bgProfileBody !bg-bgLightPrimary !left-[-6px] border-none py-[6px] px-[8px] w-[200px]"
+				>
+					<ItemPanelMember children="For 15 Minutes" />
+					<ItemPanelMember children="For 1 Hour" />
+					<ItemPanelMember children="For 3 Hours" />
+					<ItemPanelMember children="For 8 Hours" />
+                    <ItemPanelMember children="For 24 Hours" />
+                    <ItemPanelMember children="Until I turn it back on" />
+				</Dropdown>
+			</div>
+			<ItemPanelMember children="Leave Group" />
+		</>
+	);
+};
 
 export default PanelGroupDM;
--- libs/components/src/lib/components/PanelMember/index.tsx
+++ libs/components/src/lib/components/PanelMember/index.tsx
@@ -2,14 +2,13 @@ import { useAuth, useFriends } from '@mezon/core';
 import { selectAddFriends, selectCurrentChannel } from '@mezon/store';
 import { ChannelMembersEntity } from '@mezon/utils';
 import { Dropdown } from 'flowbite-react';
+import { ChannelType } from 'mezon-js';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Coords } from '../ChannelLink';
+import { directMessageValueProps } from '../DmList/DMListItem';
 import GroupPanelMember from './GroupPanelMember';
 import ItemPanelMember from './ItemPanelMember';
-import { string } from 'yup';
-import { directMessageValueProps } from '../DmList/DMListItem';
-import { ChannelType } from 'mezon-js';
 import PanelGroupDM from './PanelGroupDm';
 
 type PanelMemberProps = {
@@ -39,9 +38,9 @@ const PanelMember = ({ coords, member, directMessageValue, name, onClose, onRemo
 	};
 
 	const checkAddFriend = useSelector(selectAddFriends(directMessageValue ? directMessageValue?.userId[0] : member?.user?.id || ''));
-	const checkCreateUser = useMemo(() => userProfile?.user?.id === currentChannel?.creator_id,[currentChannel?.creator_id, userProfile?.user?.id]);
-	const checkUser = useMemo(() => userProfile?.user?.id === member?.user?.id,[member?.user?.id, userProfile?.user?.id]);
-	const {deleteFriend, addFriend} = useFriends();
+	const checkCreateUser = useMemo(() => userProfile?.user?.id === currentChannel?.creator_id, [currentChannel?.creator_id, userProfile?.user?.id]);
+	const checkUser = useMemo(() => userProfile?.user?.id === member?.user?.id, [member?.user?.id, userProfile?.user?.id]);
+	const { deleteFriend, addFriend } = useFriends();
 
 	return (
 		<div
@@ -54,71 +53,82 @@ const PanelMember = ({ coords, member, directMessageValue, name, onClose, onRemo
 				top: positionTop ? 'auto' : coords.mouseY,
 			}}
 			className="fixed top-full dark:bg-bgProfileBody bg-bgLightPrimary shadow z-20 w-[200px] py-[10px] px-[10px] border border-slate-300 dark:border-slate-700 rounded"
-			onClick={(e) => {e.stopPropagation(); onClose()}}
+			onClick={(e) => {
+				e.stopPropagation();
+				onClose();
+			}}
 		>
-			{(directMessageValue && Number(directMessageValue.type) === ChannelType.CHANNEL_TYPE_GROUP) ? 
-				<PanelGroupDM /> :	
-			<>
-			<GroupPanelMember>
-				<ItemPanelMember children="Profile" />
-				<ItemPanelMember children="Mention" />
-				{!checkUser &&
-					<>
-						<ItemPanelMember children="Message" />
-						<ItemPanelMember children="Call" />
-						<ItemPanelMember children="Add Note" />
-						<ItemPanelMember children="Add Friend Nickname" />
-					</>
-				}
-			</GroupPanelMember>
-			<GroupPanelMember>
-				<ItemPanelMember children="Mute" type="checkbox" />
-				{checkUser && 
+			{directMessageValue && Number(directMessageValue.type) === ChannelType.CHANNEL_TYPE_GROUP ? (
+				<PanelGroupDM />
+			) : (
 				<>
-					<ItemPanelMember children="Deafen" type="checkbox" />
-					<ItemPanelMember children="Edit Serve Profile" />
-					<ItemPanelMember children="Apps" />
+					<GroupPanelMember>
+						<ItemPanelMember children="Profile" />
+						<ItemPanelMember children="Mention" />
+						{!checkUser && (
+							<>
+								<ItemPanelMember children="Message" />
+								<ItemPanelMember children="Call" />
+								<ItemPanelMember children="Add Note" />
+								<ItemPanelMember children="Add Friend Nickname" />
+							</>
+						)}
+					</GroupPanelMember>
+					<GroupPanelMember>
+						<ItemPanelMember children="Mute" type="checkbox" />
+						{checkUser && (
+							<>
+								<ItemPanelMember children="Deafen" type="checkbox" />
+								<ItemPanelMember children="Edit Serve Profile" />
+								<ItemPanelMember children="Apps" />
+							</>
+						)}
+						{!checkUser && (
+							<>
+								<Dropdown
+									trigger="hover"
+									dismissOnClick={false}
+									renderTrigger={() => (
+										<div>
+											<ItemPanelMember children="Invite to Clan" dropdown />
+										</div>
+									)}
+									label=""
+									placement="left-start"
+									className="dark:!bg-bgProfileBody !bg-bgLightPrimary !left-[-6px] border-none py-[6px] px-[8px] w-[200px]"
+								>
+									<ItemPanelMember children="Komu" />
+									<ItemPanelMember children="Clan 1" />
+									<ItemPanelMember children="Clan 2" />
+									<ItemPanelMember children="Clan 3" />
+								</Dropdown>
+								{checkAddFriend ? (
+									<ItemPanelMember
+										children="Remove Friend"
+										onClick={() => deleteFriend(name || '', directMessageValue?.userId[0] || '')}
+									/>
+								) : (
+									<ItemPanelMember children="Add Friend" onClick={() => addFriend({ usernames: [name || ''], ids: [] })} />
+								)}
+								<ItemPanelMember children="Block" />
+							</>
+						)}
+					</GroupPanelMember>
+					{checkCreateUser && !checkUser && (
+						<GroupPanelMember>
+							<ItemPanelMember children="Move View" />
+							<ItemPanelMember children={`Timeout ${member?.user?.username}`} danger />
+							<ItemPanelMember onClick={handleRemoveMember} children={`Kick ${member?.user?.username}`} danger />
+							<ItemPanelMember children={`Ban ${member?.user?.username}`} danger />
+						</GroupPanelMember>
+					)}
+					{checkUser && (
+						<GroupPanelMember>
+							<ItemPanelMember children="Roles" />
+						</GroupPanelMember>
+					)}
 				</>
-				}
-				{!checkUser &&
-					<>
-						<Dropdown
-							trigger="hover"
-							dismissOnClick={false}
-							renderTrigger={() => (
-								<div>
-									<ItemPanelMember children="Invite to Clan" dropdown />
-								</div>
-							)}
-							label=""
-							placement="left-start"
-							className="dark:!bg-bgProfileBody !bg-bgLightPrimary !left-[-6px] border-none py-[6px] px-[8px] w-[200px]"
-						>
-							<ItemPanelMember children="Komu" />
-							<ItemPanelMember children="Clan 1" />
-							<ItemPanelMember children="Clan 2" />
-							<ItemPanelMember children="Clan 3" />
-						</Dropdown>
-						{checkAddFriend ? <ItemPanelMember children="Remove Friend" onClick={() => deleteFriend(name || '', directMessageValue?.userId[0] || '')}/> : <ItemPanelMember children="Add Friend" onClick={() => addFriend({usernames:[name || ''], ids:[]})}/>}
-						<ItemPanelMember children="Block" />
-					</>
-				}
-			</GroupPanelMember>
-			{checkCreateUser && !checkUser && (
-				<GroupPanelMember>
-					<ItemPanelMember children="Move View" />
-					<ItemPanelMember children={`Timeout ${member?.user?.username}`} danger />
-					<ItemPanelMember onClick={handleRemoveMember} children={`Kick ${member?.user?.username}`} danger />
-					<ItemPanelMember children={`Ban ${member?.user?.username}`} danger />
-				</GroupPanelMember>
 			)}
-			{checkUser && 
-				<GroupPanelMember>
-					<ItemPanelMember children="Roles" />
-				</GroupPanelMember>
-			}
-			</>
-			}
 		</div>
 	);
 };
