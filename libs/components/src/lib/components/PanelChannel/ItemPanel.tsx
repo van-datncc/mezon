0821d3ee77a4d322feb692308d3444cf40ffeb35
anbuihoang <an.buihoang@ncc.asia>
2024-07-03 17:33:50 +0700

    fix Interface error when creating a new thread

--- libs/components/src/lib/components/PanelChannel/ItemPanel.tsx
+++ libs/components/src/lib/components/PanelChannel/ItemPanel.tsx
@@ -9,10 +9,14 @@ type ItemPanelProps = {
 	onClick?: () => void;
 };
 
-const ItemPanel = ({ children, dropdown, danger, type, onClick }: ItemPanelProps) => {
+const ItemPanel = ({ children, dropdown, type, danger, onClick }: ItemPanelProps) => {
 	return (
 		<button onClick={onClick} className="flex items-center w-full justify-between rounded-sm hover:bg-bgSelectItem hover:[&>*]:text-[#fff] pr-2">
-			<li className={`text-[14px] ${danger ? 'text-colorDanger' : 'dark:text-[#B5BAC1] text-colorTextLightMode'} font-medium w-full py-[6px] px-[8px] text-left cursor-pointer list-none textWhiteHoverImportant m-0`}>{children}</li>
+			<li
+				className={`text-[14px] ${danger ? 'dark:text-colorDanger text-colorDanger' : 'dark:text-[#B5BAC1] text-textSecondary800'} dark:text-[#B5BAC1] text-colorTextLightMode font-medium w-full py-[6px] px-[8px] text-left cursor-pointer list-none textWhiteHoverImportant m-0`}
+			>
+				{children}
+			</li>
 			{dropdown && <Icons.RightIcon defaultFill="#fff" />}
 			{type === 'checkbox' && <Checkbox id="accept" defaultChecked />}
 			{type === 'radio' && <Radio className="" value="change here" />}
