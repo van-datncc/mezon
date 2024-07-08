7169891012ce5b2c40b4b503cc5911fec025f75b
Tungnqs <thanhlamtainguyen@gmail.com>
2024-07-08 14:49:12 +0700

    create sidebar for homepage tablet and mobile screen

--- apps/chat/src/app/pages/homepage/index.tsx
+++ apps/chat/src/app/pages/homepage/index.tsx
@@ -1,21 +1,27 @@
 import { Icons } from '@mezon/components';
 import { selectIsLogin } from '@mezon/store';
 import { Image } from '@mezon/ui';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import BannerImg from '../../../assets/homepage-banner.png';
+import SideBar from './sidebar';
 
 function Homepage() {
 	const isLogin = useSelector(selectIsLogin);
+	const [sideBarIsOpen, setSideBarIsOpen] = useState(false);
+
+	const toggleSideBar = () => {
+		setSideBarIsOpen(!sideBarIsOpen);
+	};
 	return (
 		<div className="relative">
-			{/* <img src={Background} alt="" className="absolute top-0 z-0 w-full" /> */}
 			<div
-				className="layout relative z-10 flex justify-center text-textDarkTheme"
+				className="layout relative z-10 flex flex-col items-center text-textDarkTheme"
 				style={{ backgroundImage: 'url(../../../assets/homepage-bg.png)' }}
 			>
-				<div className="container w-10/12">
-					<div className="header mt-5 flex items-center justify-between">
+				<div className={`header w-10/12 max-lg:w-full max-lg:px-[35px] mt-5 flex items-center ${sideBarIsOpen ? 'justify-end' : 'justify-between'} `}>
+					{!sideBarIsOpen && (
 						<Link to={'/mezon'} className="left flex gap-[10px] items-center">
 							<Image
 								src={`assets/images/mezon-logo-black.svg`}
@@ -26,35 +32,36 @@ function Homepage() {
 							/>
 							<div className="uppercase font-bold tracking-wide text-[20px]">Mezon</div>
 						</Link>
-
-						<div className="mid flex gap-[30px] items-center font-semibold max-lg:hidden">
-							<div className="hover:underline cursor-pointer">Download</div>
-							<div className="hover:underline cursor-pointer">Nitro</div>
-							<div className="hover:underline cursor-pointer">Discover</div>
-							<div className="hover:underline cursor-pointer">Safety</div>
-							<div className="hover:underline cursor-pointer">Support</div>
-							<div className="hover:underline cursor-pointer">Blog</div>
-							<div className="hover:underline cursor-pointer">Careers</div>
-						</div>
-						<div className="flex gap-2 w-fit">
-							{isLogin ? (
-								<Link
-									className="right px-[16px] py-[7px] bg-white rounded-3xl text-black font-semibold hover:text-[#5865f2] w-[120px]"
-									to={'/mezon'}
-								>
-									Open Mezon
-								</Link>
-							) : (
-								<Link
-									className="right px-[16px] py-[7px] bg-white rounded-3xl text-black font-semibold hover:text-[#5865f2]"
-									to={'/mezon'}
-								>
-									Login
-								</Link>
-							)}
-							<Icons.HomepageMenu className="hidden w-[40px] max-lg:block" />
-						</div>
+					)}
+					<div className="mid flex gap-[40px] items-center font-semibold max-lg:hidden">
+						<div className="hover:underline cursor-pointer">Download</div>
+						<div className="hover:underline cursor-pointer">Nitro</div>
+						<div className="hover:underline cursor-pointer">Discover</div>
+						<div className="hover:underline cursor-pointer">Safety</div>
+						<div className="hover:underline cursor-pointer">Support</div>
+						<div className="hover:underline cursor-pointer">Blog</div>
+						<div className="hover:underline cursor-pointer">Careers</div>
 					</div>
+					<div className="flex gap-2 w-fit">
+						{isLogin ? (
+							<Link
+								className="right px-[16px] py-[7px] bg-white rounded-3xl text-black font-semibold hover:text-[#5865f2] w-[120px]"
+								to={'/mezon'}
+							>
+								Open Mezon
+							</Link>
+						) : (
+							<Link
+								className="right px-[16px] py-[7px] bg-white rounded-3xl text-black font-semibold hover:text-[#5865f2]"
+								to={'/mezon'}
+							>
+								Login
+							</Link>
+						)}
+						<Icons.HomepageMenu className="hidden w-[40px] max-lg:block" onClick={toggleSideBar} />
+					</div>
+				</div>
+				<div className="container w-10/12">
 					<div className="main-content">
 						<div className="block1 flex items-center justify-center max-lg:flex-col">
 							<img src={BannerImg} alt="" className="object-cover w-6/12 max-md:w-8/12 hidden max-lg:block" />
@@ -88,6 +95,7 @@ function Homepage() {
 					</div>
 				</div>
 			</div>
+			<SideBar sideBarIsOpen={sideBarIsOpen} toggleSideBar={toggleSideBar} />
 		</div>
 	);
 }
