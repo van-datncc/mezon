c79efced5d2dd2fdf6c1f8adcc3e8f27da891d2e
Tungnqs <thanhlamtainguyen@gmail.com>
2024-07-08 15:30:36 +0700

    fix console warning in homepage

--- libs/components/src/lib/components/Icons/index.tsx
+++ libs/components/src/lib/components/Icons/index.tsx
@@ -3056,12 +3056,13 @@ export function HomepageDownload(props: React.HTMLAttributes<SVGElement>) {
 
 export function HomepageMenu(props: React.HTMLAttributes<SVGElement>) {
 	return (
-		<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-			<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-			<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+		<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="" {...props}>
+			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+			<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
 			<g id="SVGRepo_iconCarrier">
-				{' '}
-				<path d="M4 6H20M4 12H20M4 18H20" stroke="#fcfcfc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>{' '}
+				<path fillRule="evenodd" clipRule="evenodd" d="M19.5 8.25H4.5V6.75H19.5V8.25Z" fill="#fcfcfc"></path>{' '}
+				<path fillRule="evenodd" clipRule="evenodd" d="M19.5 12.75H4.5V11.25H19.5V12.75Z" fill="#fcfcfc"></path>{' '}
+				<path fillRule="evenodd" clipRule="evenodd" d="M19.5 17.25H4.5V15.75H19.5V17.25Z" fill="#fcfcfc"></path>{' '}
 			</g>
 		</svg>
 	);
@@ -3069,27 +3070,29 @@ export function HomepageMenu(props: React.HTMLAttributes<SVGElement>) {
 
 export function MenuClose(props: React.HTMLAttributes<SVGElement>) {
 	return (
-		<svg viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="#000000" className='' {...props}>
-			<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-			<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+		<svg
+			viewBox="0 -0.5 21 21"
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			fill="#000000"
+			className=""
+			{...props}
+		>
+			<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+			<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
 			<g id="SVGRepo_iconCarrier">
-				{' '}
-				<title>close [#1511]</title> <desc>Created with Sketch.</desc> <defs> </defs>{' '}
-				<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-					{' '}
+				<title>close [#1511]</title> <desc>Created with Sketch.</desc> <defs> </defs>
+				<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
 					<g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" fill="#000000">
-						{' '}
 						<g id="icons" transform="translate(56.000000, 160.000000)">
-							{' '}
 							<polygon
 								id="close-[#1511]"
 								points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"
-							>
-								{' '}
-							</polygon>{' '}
-						</g>{' '}
-					</g>{' '}
-				</g>{' '}
+							></polygon>
+						</g>
+					</g>
+				</g>
 			</g>
 		</svg>
 	);
