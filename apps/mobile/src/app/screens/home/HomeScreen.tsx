c5594dff14b8891488791fc91d73b1fd63bf4924
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-07-15 15:22:19 +0700

    update: logic call mainloader

--- apps/mobile/src/app/screens/home/HomeScreen.tsx
+++ apps/mobile/src/app/screens/home/HomeScreen.tsx
@@ -1,22 +1,17 @@
-import { load, STORAGE_IS_FROM_FCM } from '@mezon/mobile-components';
+import { load, save, STORAGE_CLAN_ID, STORAGE_IS_FROM_FCM } from '@mezon/mobile-components';
 import { Metrics } from '@mezon/mobile-ui';
 import {
 	appActions,
+	authActions,
 	channelsActions,
 	clansActions,
-	directActions,
-	friendsActions,
 	getStoreAsync,
 	messagesActions,
-	notificationActions,
-	selectAllClans,
 	selectCurrentChannelId,
 	selectCurrentClan,
-	selectIsLogin,
 } from '@mezon/store-mobile';
 import { useMezon } from '@mezon/transport';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { gifsActions } from 'libs/store/src/lib/giftStickerEmojiPanel/gifs.slice';
 import { delay } from 'lodash';
 import React, { useEffect } from 'react';
 import { AppState } from 'react-native';
@@ -26,12 +21,6 @@ import LeftDrawerContent from './homedrawer/DrawerContent';
 import HomeDefault from './homedrawer/HomeDefault';
 
 const Drawer = createDrawerNavigator();
-
-type Sessionlike = {
-	token: string;
-	refresh_token: string;
-	created: boolean;
-};
 
 const DrawerScreen = React.memo(({ navigation }: { navigation: any }) => {
 	const dispatch = useDispatch();
@@ -74,22 +63,18 @@ const DrawerScreen = React.memo(({ navigation }: { navigation: any }) => {
 
 const HomeScreen = React.memo((props: any) => {
 	const currentClan = useSelector(selectCurrentClan);
-	const clans = useSelector(selectAllClans);
 	const currentChannelId = useSelector(selectCurrentChannelId);
-	const isLogin = useSelector(selectIsLogin);
+	const dispatch = useDispatch();
+	// const { sessionRef } = useMezon();
 
-	const { reconnect, refreshSession } = useMezon();
 	useCheckUpdatedVersion();
 
 	useEffect(() => {
-		if (clans?.length && !currentClan) {
-			setCurrentClanLoader();
-		}
-	}, [clans, currentClan]);
-
-	useEffect(() => {
 		const appStateSubscription = AppState.addEventListener('change', (state) => {
-			delay(handleAppStateChange, 200, state);
+			if (state === 'active') {
+				dispatch(appActions.setLoadingMainMobile(true));
+			}
+			delay(handleAppStateChange, 800, state);
 		});
 
 		return () => {
@@ -97,43 +82,28 @@ const HomeScreen = React.memo((props: any) => {
 		};
 	}, [currentClan, currentChannelId]);
 
-	useEffect(() => {
-		if (isLogin) {
-			mainLoader();
-		}
-	}, [isLogin]);
-
 	const handleAppStateChange = async (state: string) => {
 		const isFromFCM = await load(STORAGE_IS_FROM_FCM);
+		if (isFromFCM?.toString() === 'true') {
+			dispatch(appActions.setLoadingMainMobile(false));
+		}
 		if (state === 'active' && isFromFCM?.toString() !== 'true') {
 			await messageLoader();
 		}
-		await reconnect();
-	};
-
-	const mainLoader = async () => {
-		const store = await getStoreAsync();
-		store.dispatch(notificationActions.fetchListNotification());
-		store.dispatch(friendsActions.fetchListFriends({}));
-		store.dispatch(directActions.fetchDirectMessage({}));
-		store.dispatch(clansActions.fetchClans());
-		store.dispatch(gifsActions.fetchGifCategories());
-		store.dispatch(gifsActions.fetchGifCategoryFeatured());
-		if (currentClan) {
-			store.dispatch(clansActions.joinClan({ clanId: '0' }));
-			store.dispatch(clansActions.joinClan({ clanId: currentClan?.clan_id }));
-			store.dispatch(clansActions.changeCurrentClan({ clanId: currentClan?.clan_id }));
-		}
-		return null;
 	};
 
 	const messageLoader = async () => {
+		if (!currentClan?.clan_id) {
+			dispatch(appActions.setLoadingMainMobile(false));
+			return null;
+		}
 		const store = await getStoreAsync();
-		// const resSession = await store.dispatch(authActions.refreshSession());
-		// const refreshToken = resSession?.payload as Sessionlike;
-		// await refreshSession(refreshToken);
+		// sessionRef.current.token = '';
+		await store.dispatch(authActions.refreshSession());
+		dispatch(appActions.setLoadingMainMobile(false));
 		await store.dispatch(clansActions.joinClan({ clanId: '0' }));
 		await store.dispatch(clansActions.joinClan({ clanId: currentClan?.clan_id }));
+		save(STORAGE_CLAN_ID, currentClan?.clan_id);
 		await store.dispatch(clansActions.changeCurrentClan({ clanId: currentClan?.clan_id, noCache: true }));
 		await store.dispatch(
 			channelsActions.joinChannel({
@@ -143,17 +113,6 @@ const HomeScreen = React.memo((props: any) => {
 			}),
 		);
 		await store.dispatch(messagesActions.jumpToMessage({ messageId: '', channelId: currentChannelId, noCache: true }));
-		return null;
-	};
-
-	const setCurrentClanLoader = async () => {
-		const lastClanId = clans?.[clans?.length - 1]?.clan_id;
-		const store = await getStoreAsync();
-		if (lastClanId) {
-			store.dispatch(clansActions.joinClan({ clanId: '0' }));
-			store.dispatch(clansActions.joinClan({ clanId: lastClanId }));
-			store.dispatch(clansActions.changeCurrentClan({ clanId: lastClanId }));
-		}
 		return null;
 	};
 
