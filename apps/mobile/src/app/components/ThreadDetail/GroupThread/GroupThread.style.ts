46f656ae84430e15050f2440ee5486007dc064fc
NghiaDT <nghiadt22.work>
2024-07-16 17:07:01 +0700

    update thread

--- apps/mobile/src/app/components/ThreadDetail/GroupThread/GroupThread.style.ts
+++ apps/mobile/src/app/components/ThreadDetail/GroupThread/GroupThread.style.ts
@@ -1,13 +1,13 @@
-import { Colors, size } from '@mezon/mobile-ui';
+import { Attributes, size } from '@mezon/mobile-ui';
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
+export const style = (colors: Attributes) => StyleSheet.create({
 	groupThread: {
 		borderRadius: 8,
 		overflow: 'hidden',
 	},
 	title: {
-		color: Colors.textGray,
+		color: colors.text,
 		fontWeight: '600',
 		fontSize: size.label,
 		marginBottom: size.s_10,
