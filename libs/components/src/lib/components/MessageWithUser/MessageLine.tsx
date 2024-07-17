671c77758d79b446c38f6ea8293f9413305493ee
devGangz <phongnn@devcamp.edu.vn>
2024-07-17 22:34:52 +0700

    hide link

--- libs/components/src/lib/components/MessageWithUser/MessageLine.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageLine.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import MarkdownFormatText from '../MarkdownFormatText';
 import MessageImage from './MessageImage';
 import { useMessageLine } from './useMessageLine';
@@ -11,10 +12,16 @@ type MessageLineProps = {
 // TODO: refactor component for message lines
 const MessageLine = ({ line, messageId, mode }: MessageLineProps) => {
 	const { mentions, isOnlyEmoji, imageLinks } = useMessageLine(line);
+	const imageUrlPattern = /^https?:\/\/.*\.(jpg|jpeg|png|gif|bmp|webp)$/i;
+
+	const isOnlyImageLink = useMemo(() => {
+		const check = imageUrlPattern.test(line);
+		return check;
+	}, [line]);
 
 	return (
 		<div className="pt-[0.2rem] pl-0">
-			<MarkdownFormatText mentions={mentions} isOnlyEmoji={isOnlyEmoji} mode={mode} lengthLine={line.length} />
+			{!isOnlyImageLink && <MarkdownFormatText mentions={mentions} isOnlyEmoji={isOnlyEmoji} mode={mode} lengthLine={line.length} />}
 			{imageLinks.length > 0 &&
 				imageLinks?.map((item, index) => {
 					return <MessageImage key={index} attachmentData={{ url: item.matchedText }} />;
