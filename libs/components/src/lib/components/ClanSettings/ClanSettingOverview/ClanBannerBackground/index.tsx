ca6debe55e5890894ca1f938cb8efd3d4fb2ad3e
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-07-18 15:44:07 +0700

    fix-ui-clan-setting-overview part 1

--- libs/components/src/lib/components/ClanSettings/ClanSettingOverview/ClanBannerBackground/index.tsx
+++ libs/components/src/lib/components/ClanSettings/ClanSettingOverview/ClanBannerBackground/index.tsx
@@ -8,127 +8,127 @@ import { useSelector } from 'react-redux';
 import ModalValidateFile from '../../../ModalValidateFile';
 
 type ClanBannerBackgroundProps = {
-	hasChanges: boolean;
-	onUpload: (urlImage: string) => void;
-	onHasChanges: (hasChanges: boolean) => void;
+  hasChanges: boolean;
+  onUpload: (urlImage: string) => void;
+  onHasChanges: (hasChanges: boolean) => void;
 };
 
 const ClanBannerBackground = ({ hasChanges, onUpload, onHasChanges }: ClanBannerBackgroundProps) => {
-	const { sessionRef, clientRef } = useMezon();
-	const currentClan = useSelector(selectCurrentClan);
+  const { sessionRef, clientRef } = useMezon();
+  const currentClan = useSelector(selectCurrentClan);
 
-	const currentClanId = useSelector(selectCurrentClanId) || '';
-	const currentChannelId = useSelector(selectCurrentChannelId) || '';
+  const currentClanId = useSelector(selectCurrentClanId) || '';
+  const currentChannelId = useSelector(selectCurrentChannelId) || '';
 
-	const [urlImage, setUrlImage] = useState<string | undefined>(currentClan?.banner ?? undefined);
-	const [openModal, setOpenModal] = useState<boolean>(false);
-	const fileInputRef = useRef<HTMLInputElement>(null);
+  const [urlImage, setUrlImage] = useState<string | undefined>(currentClan?.banner ?? undefined);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-	const handleFile = (e: any) => {
-		const file = e?.target?.files[0];
-		const session = sessionRef.current;
-		const client = clientRef.current;
+  const handleFile = (e: any) => {
+    const file = e?.target?.files[0];
+    const session = sessionRef.current;
+    const client = clientRef.current;
 
-		if (!file) return;
+    if (!file) return;
 
-		if (!client || !session) {
-			throw new Error('Client or file is not initialized');
-		}
+    if (!client || !session) {
+      throw new Error('Client or file is not initialized');
+    }
 
-		if (!fileTypeImage.includes(file.type)) {
-			setOpenModal(true);
-			e.target.value = null;
-			return;
-		}
+    if (!fileTypeImage.includes(file.type)) {
+      setOpenModal(true);
+      e.target.value = null;
+      return;
+    }
 
-		handleUploadFile(client, session, currentClanId, currentChannelId, file?.name, file).then((attachment: any) => {
-			setUrlImage(attachment.url ?? '');
-			onUpload(attachment.url ?? '');
-		});
-	};
+    handleUploadFile(client, session, currentClanId, currentChannelId, file?.name, file).then((attachment: any) => {
+      setUrlImage(attachment.url ?? '');
+      onUpload(attachment.url ?? '');
+    });
+  };
 
-	const handleOpenFile = () => {
-		if (fileInputRef.current) {
-			fileInputRef.current.click();
-		}
-	};
+  const handleOpenFile = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
 
-	const handleCloseFile = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.stopPropagation();
-		if (urlImage && fileInputRef.current) {
-			setUrlImage('');
-			onUpload('');
-			fileInputRef.current.value = '';
-		}
+  const handleCloseFile = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    if (urlImage && fileInputRef.current) {
+      setUrlImage('');
+      onUpload('');
+      fileInputRef.current.value = '';
+    }
 
-		if (fileInputRef.current && !urlImage) {
-			fileInputRef.current.click();
-		}
-	};
+    if (fileInputRef.current && !urlImage) {
+      fileInputRef.current.click();
+    }
+  };
 
-	useEffect(() => {
-		if (urlImage !== currentClan?.banner) {
-			onHasChanges(true);
-		} else {
-			onHasChanges(false);
-		}
-	}, [urlImage, currentClan?.banner]);
+  useEffect(() => {
+    if (urlImage !== currentClan?.banner) {
+      onHasChanges(true);
+    } else {
+      onHasChanges(false);
+    }
+  }, [urlImage, currentClan?.banner]);
 
-	useEffect(() => {
-		if (!hasChanges && fileInputRef.current) {
-			setUrlImage(currentClan?.banner ?? undefined);
-			fileInputRef.current.value = '';
-		}
-	}, [hasChanges]);
+  useEffect(() => {
+    if (!hasChanges && fileInputRef.current) {
+      setUrlImage(currentClan?.banner ?? undefined);
+      fileInputRef.current.value = '';
+    }
+  }, [hasChanges]);
 
-	return (
-		<div className="flex sbm:flex-row flex-col pt-10 mt-10 border-t border-borderClan gap-x-5 gap-y-[10px]">
-			<div className="flex flex-col flex-1 text-textSecondary">
-				<h3 className="text-xs font-bold dark:text-textSecondary text-textSecondary800 uppercase mb-2">Clan Banner Background</h3>
-				<p className="text-sm font-normal mb-2 dark:text-textSecondary text-textSecondary800">
-					This image will display at the top of your channels list.
-				</p>
-				<p className="text-sm font-normal dark:text-textSecondary text-textSecondary800">
-					The recommended minimum size is 960x540 and recommended aspect ratio is 16:9.
-				</p>
-				<Button
-					className="h-10 w-fit px-4 mt-4 rounded bg-bgLightModeThird text-textLightTheme dark:text-textDarkTheme border dark:border-buttonProfile hover:!bg-[#9e9e9e] dark:bg-transparent dark:hover:!bg-buttonProfile focus:!ring-transparent"
-					onClick={handleOpenFile}
-				>
-					Upload Background
-				</Button>
-			</div>
-			<div className="flex flex-1 sbm:mb-0 mb-5">
-				<div className="relative max-w-[320px] w-full h-[180px]">
-					<label>
-						<div
-							style={{ backgroundImage: `url(${urlImage})` }}
-							className={`bg-cover bg-no-repeat bg-center w-full h-full dark:bg-buttonProfile bg-bgLightModeThird rounded relative cursor-pointer`}
-						>
-							{!urlImage && (
-								<p className="dark:text-white text-textLightTheme text-xl font-semibold text-center pt-[25%]">Choose an Image</p>
-							)}
-						</div>
-						<input ref={fileInputRef} id="upload_banner_background" onChange={(e) => handleFile(e)} type="file" className="hidden" />
-					</label>
-					<button
-						onClick={handleCloseFile}
-						className="absolute top-4 right-4 w-7 h-7 rounded-full bg-[#A7A8AC] hover:bg-[#919193] flex items-center justify-center"
-					>
-						{urlImage ? <Icons.Close /> : <Icons.ImageUploadIcon />}
-					</button>
-				</div>
-			</div>
+  return (
+    <div className="flex sbm:flex-row flex-col pt-10 mt-10 border-t border-borderClan gap-x-5 gap-y-[10px]  dark:border-borderDivider border-borderDividerLight">
+      <div className="flex flex-col flex-1 text-textSecondary">
+        <h3 className="text-xs font-bold dark:text-textSecondary text-textSecondary800 uppercase mb-2">Clan Banner Background</h3>
+        <p className="text-sm font-normal mb-2 dark:text-textSecondary text-textSecondary800">
+          This image will display at the top of your channels list.
+        </p>
+        <p className="text-sm font-normal dark:text-textSecondary text-textSecondary800">
+          The recommended minimum size is 960x540 and recommended aspect ratio is 16:9.
+        </p>
+        <Button
+          className="h-10 w-fit px-4 mt-4 rounded bg-bgLightModeThird text-textLightTheme dark:text-textDarkTheme border dark:border-buttonProfile hover:!bg-[#9e9e9e] dark:bg-transparent dark:hover:!bg-buttonProfile focus:!ring-transparent"
+          onClick={handleOpenFile}
+        >
+          Upload Background
+        </Button>
+      </div>
+      <div className="flex flex-1 sbm:mb-0 mb-5">
+        <div className="relative max-w-[320px] w-full h-[180px]">
+          <label>
+            <div
+              style={{ backgroundImage: `url(${urlImage})` }}
+              className={`bg-cover bg-no-repeat bg-center w-full h-full dark:bg-buttonProfile bg-bgLightModeThird rounded relative cursor-pointer`}
+            >
+              {!urlImage && (
+                <p className="dark:text-white text-textLightTheme text-xl font-semibold text-center pt-[25%]">Choose an Image</p>
+              )}
+            </div>
+            <input ref={fileInputRef} id="upload_banner_background" onChange={(e) => handleFile(e)} type="file" className="hidden" />
+          </label>
+          <button
+            onClick={handleCloseFile}
+            className="absolute top-4 right-4 w-7 h-7 rounded-full bg-[#A7A8AC] hover:bg-[#919193] flex items-center justify-center"
+          >
+            {urlImage ? <Icons.Close /> : <Icons.ImageUploadIcon />}
+          </button>
+        </div>
+      </div>
 
-			<ModalValidateFile
-				openModal={openModal}
-				onClose={() => setOpenModal(false)}
-				image="assets/images/file-and-folder.png"
-				title="Only image files are allowed"
-				content="Just upload type file (JPEG, PNG), please!"
-			/>
-		</div>
-	);
+      <ModalValidateFile
+        openModal={openModal}
+        onClose={() => setOpenModal(false)}
+        image="assets/images/file-and-folder.png"
+        title="Only image files are allowed"
+        content="Just upload type file (JPEG, PNG), please!"
+      />
+    </div>
+  );
 };
 
 export default ClanBannerBackground;
--- libs/components/src/lib/components/ClanSettings/SettingSidebar/index.tsx
+++ libs/components/src/lib/components/ClanSettings/SettingSidebar/index.tsx
@@ -2,58 +2,65 @@ import { authActions, selectCurrentClan, useAppDispatch } from '@mezon/store';
 import { LogoutModal } from 'libs/ui/src/lib/LogOutButton';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { ItemObjProps, listItemSetting } from '../ItemObj';
+import { ItemObjProps, sideBarListItem } from '../ItemObj';
 import SettingItem from '../SettingItem';
 
 type SettingSidebarProps = {
-	onClickItem?: (settingItem: ItemObjProps) => void;
-	handleMenu: (value: boolean) => void;
-	currentSetting: string;
-	setIsShowDeletePopup: () => void;
+  onClickItem?: (settingItem: ItemObjProps) => void;
+  handleMenu: (value: boolean) => void;
+  currentSetting: string;
+  setIsShowDeletePopup: () => void;
 };
 
 const SettingSidebar = ({ onClickItem, handleMenu, currentSetting, setIsShowDeletePopup }: SettingSidebarProps) => {
-	const [selectedButton, setSelectedButton] = useState<string | null>(currentSetting);
-	const currentClan = useSelector(selectCurrentClan);
+  const [selectedButton, setSelectedButton] = useState<string | null>(currentSetting);
+  const currentClan = useSelector(selectCurrentClan);
 
-	const [openModal, setOpenModal] = useState<boolean>(false);
-	const dispatch = useAppDispatch();
-	const handleLogOut = () => {
-		dispatch(authActions.logOut());
-	};
-	const handleCloseModal = () => {
-		setOpenModal(false);
-		setSelectedButton('');
-	};
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const handleLogOut = () => {
+    dispatch(authActions.logOut());
+  };
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setSelectedButton('');
+  };
 
-	const handleClickButtonSidebar = (settingItem: ItemObjProps) => {
-		onClickItem?.(settingItem);
-		setSelectedButton(settingItem.id);
-	};
+  const handleClickButtonSidebar = (settingItem: ItemObjProps) => {
+    onClickItem?.(settingItem);
+    setSelectedButton(settingItem.id);
+  };
 
-	return (
-		<div className="flex flex-row flex-1 justify-end">
-			<div className="w-[220px] py-[60px] pl-5 pr-[6px]">
-				<p className="text-[#84ADFF] pl-[10px] pb-[6px] font-bold text-sm tracking-wider uppercase">{currentClan?.clan_name}</p>
-				{listItemSetting.map((setting) => (
-					<SettingItem
-						key={setting.id}
-						name={setting.name}
-						active={selectedButton === setting.id}
-						onClick={() => handleClickButtonSidebar(setting)}
-						handleMenu={handleMenu}
-					/>
-				))}
-				<button
-					className={`dark:text-textPrimary text-buttonProfile w-full py-1 px-[10px] mb-1 text-[16px] font-medium rounded text-left dark:hover:bg-bgHover hover:bg-bgModifierHoverLight`}
-					onClick={setIsShowDeletePopup}
-				>
-					Delete clan
-				</button>
-				{openModal && <LogoutModal handleLogOut={handleLogOut} onClose={handleCloseModal} />}
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-row flex-1 justify-end">
+      <div className="w-[220px] py-[60px] pl-5 pr-[6px]">
+        <p className="text-[#84ADFF] pl-[10px] pb-[6px] font-bold text-sm tracking-wider uppercase truncate">{currentClan?.clan_name}</p>
+        {sideBarListItem.map((item) => (
+          <div className='mt-[20px] relative before:absolute before:content-[""] before:left-[10px] before:w-[calc(100%_-_10px)] before:h-[1px] before:bg-black before:top-[-10px]'>
+            {item.title && <p className='select-none font-bold px-[10px] text-sm uppercase dark:text-textPrimary text-buttonProfile'>{item.title}</p>}
+            {
+              item.listItem.map((setting) => (
+                <SettingItem
+                  key={setting.id}
+                  name={setting.name}
+                  active={selectedButton === setting.id}
+                  onClick={() => handleClickButtonSidebar(setting)}
+                  handleMenu={handleMenu}
+                />
+              ))
+            }
+          </div>
+        ))}
+        <button
+          className={`text-red-500 text-buttonProfile w-full py-1 px-[10px] mb-1 text-[16px] font-medium rounded text-left dark:hover:bg-bgHover hover:bg-bgModifierHoverLight`}
+          onClick={setIsShowDeletePopup}
+        >
+          Delete clan
+        </button>
+        {openModal && <LogoutModal handleLogOut={handleLogOut} onClose={handleCloseModal} />}
+      </div>
+    </div>
+  );
 };
 
 export default SettingSidebar;
