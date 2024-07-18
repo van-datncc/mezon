de33a4aa571f87864b32376b6516b3d06950cd8d
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-07-18 11:25:11 +0700

    fix - width

--- libs/components/src/lib/components/MessageWithUser/MessageReply/MarkUpOnReply.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageReply/MarkUpOnReply.tsx
@@ -1,13 +1,17 @@
+import { selectIsUseProfileDM } from '@mezon/store';
 import { memo } from 'react';
+import { useSelector } from 'react-redux';
 import HashTagMentionById from './HashTagMentionById';
 
 interface IMarkUpOnReply {
 	mention: any[];
 	onClickToMove: (e: React.MouseEvent<HTMLSpanElement>) => void;
 	posMention?: boolean;
+	parentWidth?: number;
 }
 
-const MarkUpOnReply = ({ mention, onClickToMove, posMention }: IMarkUpOnReply) => {
+const MarkUpOnReply = ({ mention, onClickToMove, posMention, parentWidth }: IMarkUpOnReply) => {
+	const isUseProfileDM = useSelector(selectIsUseProfileDM);
 	const processedMessage = mention.reduce((acc: any, item, index) => {
 		let markUpId = '';
 
@@ -26,10 +30,17 @@ const MarkUpOnReply = ({ mention, onClickToMove, posMention }: IMarkUpOnReply) =
 		return acc;
 	}, []);
 
+	const spanStyle = {
+		whiteSpace: 'nowrap',
+		overflow: 'hidden',
+		textOverflow: 'ellipsis',
+	};
+
 	return (
 		<span
 			onClick={onClickToMove}
 			className={`${posMention ? ' whitespace-normal' : 'inline whitespace-nowrap overflow-x-hidden'} dark:hover:text-white dark:text-[#A8BAB8] text-[#818388] hover:text-[#060607] cursor-pointer  noselect`}
+			style={isUseProfileDM ? { ...spanStyle, width: `${parentWidth && parentWidth - 600}px` } : spanStyle}
 		>
 			{processedMessage}
 		</span>
