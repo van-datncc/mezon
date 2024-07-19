2a0f1b237d46e454a3889e58922c45a8d0c274f9
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-07-19 14:16:16 +0700

    fix format

--- libs/components/src/lib/components/ClanSettings/ClanSettingOverview/ClanLogoName/index.tsx
+++ libs/components/src/lib/components/ClanSettings/ClanSettingOverview/ClanLogoName/index.tsx
@@ -8,163 +8,163 @@ import { useSelector } from 'react-redux';
 import ModalValidateFile from '../../../ModalValidateFile';
 
 type ClanLogoNameProps = {
-  hasChanges: boolean;
-  onUpload: (url: string) => void;
-  onGetClanName: (clanName: string) => void;
-  onHasChanges: (hasChanges: boolean) => void;
+	hasChanges: boolean;
+	onUpload: (url: string) => void;
+	onGetClanName: (clanName: string) => void;
+	onHasChanges: (hasChanges: boolean) => void;
 };
 
 const ClanLogoName = ({ hasChanges, onUpload, onGetClanName, onHasChanges }: ClanLogoNameProps) => {
-  const { sessionRef, clientRef } = useMezon();
-  const currentClan = useSelector(selectCurrentClan);
+	const { sessionRef, clientRef } = useMezon();
+	const currentClan = useSelector(selectCurrentClan);
 
-  const currentClanId = useSelector(selectCurrentClanId) || '';
-  const currentChannelId = useSelector(selectCurrentChannelId) || '';
+	const currentClanId = useSelector(selectCurrentClanId) || '';
+	const currentChannelId = useSelector(selectCurrentChannelId) || '';
 
-  const [urlLogo, setUrlLogo] = useState<string | undefined>(currentClan?.logo ?? '');
-  const [clanName, setClanName] = useState<string | undefined>(currentClan?.clan_name ?? '');
-  const [checkValidate, setCheckValidate] = useState(!ValidateSpecialCharacters().test(currentClan?.clan_name || ''));
-  const [openModal, setOpenModal] = useState<boolean>(false);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+	const [urlLogo, setUrlLogo] = useState<string | undefined>(currentClan?.logo ?? '');
+	const [clanName, setClanName] = useState<string | undefined>(currentClan?.clan_name ?? '');
+	const [checkValidate, setCheckValidate] = useState(!ValidateSpecialCharacters().test(currentClan?.clan_name || ''));
+	const [openModal, setOpenModal] = useState<boolean>(false);
+	const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleFile = (e: any) => {
-    const file = e?.target?.files[0];
-    const session = sessionRef.current;
-    const client = clientRef.current;
+	const handleFile = (e: any) => {
+		const file = e?.target?.files[0];
+		const session = sessionRef.current;
+		const client = clientRef.current;
 
-    if (!file) return;
+		if (!file) return;
 
-    if (!client || !session) {
-      throw new Error('Client or file is not initialized');
-    }
+		if (!client || !session) {
+			throw new Error('Client or file is not initialized');
+		}
 
-    if (!fileTypeImage.includes(file.type)) {
-      setOpenModal(true);
-      e.target.value = null;
-      return;
-    }
+		if (!fileTypeImage.includes(file.type)) {
+			setOpenModal(true);
+			e.target.value = null;
+			return;
+		}
 
-    handleUploadFile(client, session, currentClanId, currentChannelId, file?.name, file).then((attachment: any) => {
-      setUrlLogo(attachment.url ?? '');
-      onUpload(attachment.url ?? '');
-    });
-  };
+		handleUploadFile(client, session, currentClanId, currentChannelId, file?.name, file).then((attachment: any) => {
+			setUrlLogo(attachment.url ?? '');
+			onUpload(attachment.url ?? '');
+		});
+	};
 
-  const handleChangeClanName = (clanName: string) => {
-    setClanName(clanName);
-    onGetClanName(clanName);
-    const regex = ValidateSpecialCharacters();
+	const handleChangeClanName = (clanName: string) => {
+		setClanName(clanName);
+		onGetClanName(clanName);
+		const regex = ValidateSpecialCharacters();
 
-    if (clanName.length === 0 || clanName.length === 64 || !regex.test(clanName)) {
-      setCheckValidate(true);
-    } else {
-      setCheckValidate(false);
-    }
-  };
+		if (clanName.length === 0 || clanName.length === 64 || !regex.test(clanName)) {
+			setCheckValidate(true);
+		} else {
+			setCheckValidate(false);
+		}
+	};
 
-  const handleOpenFile = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
+	const handleOpenFile = () => {
+		if (fileInputRef.current) {
+			fileInputRef.current.click();
+		}
+	};
 
-  const handleCloseFile = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    if (urlLogo && fileInputRef.current) {
-      setUrlLogo('');
-      fileInputRef.current.value = '';
-    }
+	const handleCloseFile = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		if (urlLogo && fileInputRef.current) {
+			setUrlLogo('');
+			fileInputRef.current.value = '';
+		}
 
-    if (fileInputRef.current && !urlLogo) {
-      fileInputRef.current.click();
-    }
-  };
+		if (fileInputRef.current && !urlLogo) {
+			fileInputRef.current.click();
+		}
+	};
 
-  useEffect(() => {
-    if (clanName !== currentClan?.clan_name || urlLogo !== currentClan?.logo) {
-      onHasChanges(true);
-    } else {
-      onHasChanges(false);
-    }
-  }, [clanName, urlLogo, currentClan?.logo, currentClan?.clan_name]);
+	useEffect(() => {
+		if (clanName !== currentClan?.clan_name || urlLogo !== currentClan?.logo) {
+			onHasChanges(true);
+		} else {
+			onHasChanges(false);
+		}
+	}, [clanName, urlLogo, currentClan?.logo, currentClan?.clan_name]);
 
-  useEffect(() => {
-    if (!hasChanges && fileInputRef.current) {
-      setUrlLogo(currentClan?.logo ?? undefined);
-      fileInputRef.current.value = '';
-      setClanName(currentClan?.clan_name ?? '');
-    }
-  }, [hasChanges]);
+	useEffect(() => {
+		if (!hasChanges && fileInputRef.current) {
+			setUrlLogo(currentClan?.logo ?? undefined);
+			fileInputRef.current.value = '';
+			setClanName(currentClan?.clan_name ?? '');
+		}
+	}, [hasChanges]);
 
-  useEffect(() => {
-    if (clanName === currentClan?.clan_name) {
-      setCheckValidate(false);
-    }
-  }, [clanName]);
+	useEffect(() => {
+		if (clanName === currentClan?.clan_name) {
+			setCheckValidate(false);
+		}
+	}, [clanName]);
 
-  return (
-    <div className="flex sbm:flex-row flex-col gap-[10px]">
-      <div className="flex flex-row flex-1 text-textSecondary gap-x-[10px]">
-        <div className="flex flex-2 gap-x-[10px]">
-          <div className="flex flex-col">
-            <div className="relative flex items-center justify-center w-[100px] h-[100px] rounded-full shadow-lg shadow-neutral-800">
-              <label className="w-full h-full">
-                <div
-                  style={{ backgroundImage: `url(${urlLogo})` }}
-                  className={`flex items-center justify-start bg-cover bg-no-repeat bg-center w-[100px] h-[100px] bg-transparent rounded-full relative cursor-pointer overflow-hidden`}
-                >
-                  {!urlLogo && <span className={'whitespace-nowrap'}>{currentClan?.clan_name}</span>}
-                </div>
-                <input ref={fileInputRef} id="upload_logo" onChange={(e) => handleFile(e)} type="file" className="hidden" />
-              </label>
-              <button
-                onClick={handleCloseFile}
-                className="absolute top-0 right-0 w-7 h-7 rounded-full bg-[#A7A8AC] hover:bg-[#919193] flex items-center justify-center"
-              >
-                {urlLogo ? <Icons.Close /> : <Icons.ImageUploadIcon />}
-              </button>
-            </div>
-            <p className="text-[10px] mt-[10px]">Minimum Size: 128x128</p>
-          </div>
-        </div>
-        <div className="flex flex-3 flex-col ml-[10px]">
-          <p className="text-sm mb-2">We recommend an image of at least 512x512 for the clan.</p>
-          <Button
-            onClick={handleOpenFile}
-            className="h-10 text-sm w-fit mt-2 rounded bg-bgLightModeThird text-textLightTheme dark:text-textDarkTheme border dark:border-buttonProfile hover:!bg-[#9e9e9e] dark:bg-transparent dark:hover:!bg-buttonProfile focus:!ring-transparent"
-          >
-            Upload Image
-          </Button>
-        </div>
-      </div>
-      <div className="flex flex-1 flex-col">
-        <h3 className="text-xs font-bold dark:text-textSecondary text-textSecondary800 uppercase mb-2">Clan Name</h3>
-        <div className="w-full">
-          <input
-            type="text"
-            value={clanName}
-            onChange={(e) => handleChangeClanName(e.target.value)}
-            className="dark:text-[#B5BAC1] text-textLightTheme outline-none w-full h-10 p-[10px] dark:bg-bgInputDark bg-bgLightModeThird text-base rounded placeholder:text-sm"
-            placeholder="Support has arrived!"
-            maxLength={64}
-          />
-        </div>
-        {checkValidate && (
-          <p className="text-[#e44141] text-xs italic font-thin">
-            Please enter a valid channel name (max 64 characters, only words, numbers, _ or -).
-          </p>
-        )}
-      </div>
+	return (
+		<div className="flex sbm:flex-row flex-col gap-[10px]">
+			<div className="flex flex-row flex-1 text-textSecondary gap-x-[10px]">
+				<div className="flex flex-2 gap-x-[10px]">
+					<div className="flex flex-col">
+						<div className="relative flex items-center justify-center w-[100px] h-[100px] rounded-full shadow-lg shadow-neutral-800">
+							<label className="w-full h-full">
+								<div
+									style={{ backgroundImage: `url(${urlLogo})` }}
+									className={`flex items-center justify-start bg-cover bg-no-repeat bg-center w-[100px] h-[100px] bg-transparent rounded-full relative cursor-pointer overflow-hidden`}
+								>
+									{!urlLogo && <span className={'whitespace-nowrap'}>{currentClan?.clan_name}</span>}
+								</div>
+								<input ref={fileInputRef} id="upload_logo" onChange={(e) => handleFile(e)} type="file" className="hidden" />
+							</label>
+							<button
+								onClick={handleCloseFile}
+								className="absolute top-0 right-0 w-7 h-7 rounded-full bg-[#A7A8AC] hover:bg-[#919193] flex items-center justify-center"
+							>
+								{urlLogo ? <Icons.Close /> : <Icons.ImageUploadIcon />}
+							</button>
+						</div>
+						<p className="text-[10px] mt-[10px]">Minimum Size: 128x128</p>
+					</div>
+				</div>
+				<div className="flex flex-3 flex-col ml-[10px]">
+					<p className="text-sm mb-2">We recommend an image of at least 512x512 for the clan.</p>
+					<Button
+						onClick={handleOpenFile}
+						className="h-10 text-sm w-fit mt-2 rounded bg-bgLightModeThird text-textLightTheme dark:text-textDarkTheme border dark:border-buttonProfile hover:!bg-[#9e9e9e] dark:bg-transparent dark:hover:!bg-buttonProfile focus:!ring-transparent"
+					>
+						Upload Image
+					</Button>
+				</div>
+			</div>
+			<div className="flex flex-1 flex-col">
+				<h3 className="text-xs font-bold dark:text-textSecondary text-textSecondary800 uppercase mb-2">Clan Name</h3>
+				<div className="w-full">
+					<input
+						type="text"
+						value={clanName}
+						onChange={(e) => handleChangeClanName(e.target.value)}
+						className="dark:text-[#B5BAC1] text-textLightTheme outline-none w-full h-10 p-[10px] dark:bg-bgInputDark bg-bgLightModeThird text-base rounded placeholder:text-sm"
+						placeholder="Support has arrived!"
+						maxLength={64}
+					/>
+				</div>
+				{checkValidate && (
+					<p className="text-[#e44141] text-xs italic font-thin">
+						Please enter a valid channel name (max 64 characters, only words, numbers, _ or -).
+					</p>
+				)}
+			</div>
 
-      <ModalValidateFile
-        openModal={openModal}
-        onClose={() => setOpenModal(false)}
-        image="assets/images/file-and-folder.png"
-        title="Only image files are allowed"
-        content="Just upload type file (JPEG, PNG), please!"
-      />
-    </div>
-  );
+			<ModalValidateFile
+				openModal={openModal}
+				onClose={() => setOpenModal(false)}
+				image="assets/images/file-and-folder.png"
+				title="Only image files are allowed"
+				content="Just upload type file (JPEG, PNG), please!"
+			/>
+		</div>
+	);
 };
 
 export default ClanLogoName;
--- libs/components/src/lib/components/ClanSettings/ClanSettingOverview/ModalSaveChanges/index.tsx
+++ libs/components/src/lib/components/ClanSettings/ClanSettingOverview/ModalSaveChanges/index.tsx
@@ -1,34 +1,34 @@
 
 type ModalSaveChangesProps = {
-  onSave: () => void;
-  onReset: () => void;
+	onSave: () => void;
+	onReset: () => void;
 };
 
 const ModalSaveChanges = ({ onSave, onReset }: ModalSaveChangesProps) => {
-  return (
-    <div
-      className="w-fit min-w-[700px] fixed bottom-[20px] left-[50%] translate-x-[-50%] py-[10px] pl-4 pr-[10px] rounded-[5px] dark:bg-bgProfileBody bg-white dark:text-white text-colorTextLightMode border dark:border-white border-colorTextLightMode text-sm font-medium z-10"
-      style={{ boxShadow: '0 2px 10px 0 hsl(0 calc( 1 * 0%) 0% / 0.1)' }}
-    >
-      <div className="flex flex-row justify-between items-center">
-        <h3>Careful — you have unsaved changes!</h3>
-        <div className="flex flex-row justify-end gap-[10px]">
-          <button
-            onClick={onReset}
-            className="rounded px-4 py-1.5 hover:underline"
-          >
-            Reset
-          </button>
-          <button
-            onClick={onSave}
-            className="ml-auto bg-blue-600 rounded-[4px] px-4 py-1.5 text-nowrap text-white"
-          >
-            Save Changes
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="w-fit min-w-[700px] fixed bottom-[20px] left-[50%] translate-x-[-50%] py-[10px] pl-4 pr-[10px] rounded-[5px] dark:bg-bgProfileBody bg-white dark:text-white text-colorTextLightMode border dark:border-white border-colorTextLightMode text-sm font-medium z-10"
+			style={{ boxShadow: '0 2px 10px 0 hsl(0 calc( 1 * 0%) 0% / 0.1)' }}
+		>
+			<div className="flex flex-row justify-between items-center">
+				<h3>Careful — you have unsaved changes!</h3>
+				<div className="flex flex-row justify-end gap-[10px]">
+					<button
+						onClick={onReset}
+						className="rounded px-4 py-1.5 hover:underline"
+					>
+						Reset
+					</button>
+					<button
+						onClick={onSave}
+						className="ml-auto bg-blue-600 rounded-[4px] px-4 py-1.5 text-nowrap text-white"
+					>
+						Save Changes
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ModalSaveChanges;
