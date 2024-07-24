334a6e204e80e877a4a6f27d1441135ce432c758
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-07-24 15:35:23 +0700

    update

--- libs/store/src/lib/account/account.slice.ts
+++ libs/store/src/lib/account/account.slice.ts
@@ -30,13 +30,13 @@ const fetchUserProfileCached = memoize((mezon: MezonValueContext) => mezon.clien
 	},
 });
 
-export const getUserProfile = createAsyncThunk<IUserAccount, {noCache: boolean} | void>('account/user', async (arg, thunkAPI) => {
+export const getUserProfile = createAsyncThunk<IUserAccount, { noCache: boolean } | void>('account/user', async (arg, thunkAPI) => {
 	const mezon = await ensureSession(getMezonCtx(thunkAPI));
-  const noCache = arg?.noCache ?? false;
-  if(noCache) {
-    fetchUserProfileCached.clear(mezon);
-  }
-  const response = await fetchUserProfileCached(mezon);
+	const noCache = arg?.noCache ?? false;
+	if (noCache) {
+		fetchUserProfileCached.clear(mezon);
+	}
+	const response = await fetchUserProfileCached(mezon);
 	if (!response) {
 		return thunkAPI.rejectWithValue('Invalid session');
 	}
