e6a953416f8195d74d3bdcd3fa06c1199a597d89
NghiaDT <nghiadt22.work>
2024-07-26 16:35:09 +0700

    fix patch

--- apps/mobile/src/app/screens/home/HomeScreen.tsx
+++ apps/mobile/src/app/screens/home/HomeScreen.tsx
@@ -1,19 +1,17 @@
 import { ActionEmitEvent } from '@mezon/mobile-components';
 import { appActions } from '@mezon/store-mobile';
-import { useMezon } from '@mezon/transport';
 import React, { useEffect, useRef } from 'react';
 import { DeviceEventEmitter, Dimensions, Keyboard } from 'react-native';
 import { DrawerLayout, DrawerState } from 'react-native-gesture-handler';
 import { useDispatch } from 'react-redux';
+import { useCheckUpdatedVersion } from '../../hooks/useCheckUpdatedVersion';
 import LeftDrawerContent from './homedrawer/DrawerContent';
 import HomeDefault from './homedrawer/HomeDefault';
 
 const HomeScreen = React.memo((props: any) => {
 	const homeDrawerRef = useRef<DrawerLayout>(null);
 	const dispatch = useDispatch();
-	const { sessionRef } = useMezon();
-
-	// useCheckUpdatedVersion();
+	useCheckUpdatedVersion();
 
 	useEffect(() => {
 		dispatch(appActions.setHiddenBottomTabMobile(true));
