f47d5d6cf3354c399f6f4297499a1a0d22a55cb3
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-08-02 15:08:35 +0700

    show sticker by clanID

--- libs/components/src/lib/components/GifsStickersEmojis/stickers.tsx
+++ libs/components/src/lib/components/GifsStickersEmojis/stickers.tsx
@@ -1,9 +1,9 @@
 import { useChatSending, useGifsStickersEmoji } from '@mezon/core';
-import { selectCurrentClan, selectListStickerByClanID, selectModeResponsive, useAppSelector } from "@mezon/store";
+import { selectAllStickerSuggestion, selectCurrentClan, selectModeResponsive, useAppSelector } from "@mezon/store";
 import { Icons } from "@mezon/ui";
 import { IMessageSendPayload, ModeResponsive, SubPanelName } from '@mezon/utils';
 import { ApiMessageAttachment, ApiMessageMention, ApiMessageRef } from 'mezon-js/api.gen';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { mockCategoryLogo, mockStickers } from "./StickerMockData";
 
 type ChannelMessageBoxProps = {
@@ -11,6 +11,16 @@ type ChannelMessageBoxProps = {
   mode: number;
 };
 
+interface ICategorizedStickerProps {
+  stickerList: any[];
+  categoryName: string;
+  onClickSticker: (stickerUrl: string) => void;
+}
+
+interface IStickerPanelProps {
+  stickerList: any[];
+  onClickSticker: (stickerUrl: string) => void;
+}
 function stickersquare({ channelId, mode }: ChannelMessageBoxProps) {
   const { sendMessage } = useChatSending({ channelId, mode });
 
@@ -26,10 +36,9 @@ function stickersquare({ channelId, mode }: ChannelMessageBoxProps) {
     [sendMessage],
   );
 
-  const clanStickers = useAppSelector(selectListStickerByClanID);
+  const clanStickers = useAppSelector(selectAllStickerSuggestion);
   const currentClan = useAppSelector(selectCurrentClan);
   const modeResponsive = useAppSelector(selectModeResponsive);
-
   const categoryLogo = [
     ...(modeResponsive === ModeResponsive.MODE_CLAN ? [{ id: 0, url: currentClan?.logo, type: 'custom' }] : []),
     ...mockCategoryLogo
@@ -46,7 +55,6 @@ function stickersquare({ channelId, mode }: ChannelMessageBoxProps) {
 
   const { setSubPanelActive } = useGifsStickersEmoji();
   const [selectedType, setSelectedType] = useState('');
-  const [selectImage, setSelectImage] = useState<any>(stickers);
   const categoryRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -54,11 +62,6 @@ function stickersquare({ channelId, mode }: ChannelMessageBoxProps) {
     handleSend({ t: '' }, [], [{ url: imageUrl, height: 40, width: 40, filetype: 'image/gif' }], []);
     setSubPanelActive(SubPanelName.NONE);
   };
-
-  const handleClickAvt = (type: string) => {
-    setSelectedType(type);
-  };
-
   const scrollToCategory = (event: React.MouseEvent, categoryName: string) => {
     event.stopPropagation();
     if (categoryName !== selectedType) {
@@ -75,24 +78,20 @@ function stickersquare({ channelId, mode }: ChannelMessageBoxProps) {
     }
   };
 
-  useEffect(() => {
-    const filteredstickers = selectedType ? stickers.filter((image) => image.type === selectedType) : stickers;
-    setSelectImage(filteredstickers);
-  }, [selectedType]);
+
 
   return (
     <div className="flex h-full pr-2 w-full md:w-[500px]">
       <div className="w-[10%] max-sm:gap-x-1
-				flex flex-col max-sm:flex-row max-sm:justify-end gap-y-1
-				max-sm:w-full dark:bg-[#1E1F22] bg-bgLightModeSecond pt-1
-				px-1 md:items-start h-[25rem] pb-1 rounded-lg">
+        flex flex-col max-sm:flex-row max-sm:justify-end gap-y-1
+        max-sm:w-full dark:bg-[#1E1F22] bg-bgLightModeSecond pt-1
+        px-1 md:items-start h-[25rem] pb-1 rounded-lg">
         {categoryLogo.map((avt) => (
           <button key={avt.id} onClick={(e) => scrollToCategory(e, avt.type)}>
             <img
               src={avt.url}
               alt={`avt ${avt.id}`}
-              className={`w-full h-auto cursor-pointer dark:hover:bg-bgDisable hover:bg-bgLightModeButton ${avt.type === selectedType ? 'bg-bgDisable' : ''} hover:rounded-lg justify-center items-center border border-bgHoverMember rounded-lg aspect-square`}
-              onClick={() => handleClickAvt(avt.type)}
+              className={`w-full h-full object-cover aspect-square cursor-pointer dark:hover:bg-bgDisable hover:bg-bgLightModeButton ${avt.type === selectedType ? 'bg-bgDisable' : ''} hover:rounded-lg justify-center items-center border border-bgHoverMember rounded-lg aspect-square`}
               role="button"
             />
           </button>
@@ -108,14 +107,8 @@ function stickersquare({ channelId, mode }: ChannelMessageBoxProps) {
     </div>
   );
 }
-
 export default stickersquare;
 
-interface ICategorizedStickerProps {
-  stickerList: any[];
-  categoryName: string;
-  onClickSticker: (stickerUrl: string) => void;
-}
 
 const CategorizedStickers: React.FC<ICategorizedStickerProps> = ({ stickerList, categoryName, onClickSticker }) => {
   const stickersListByCategoryName = stickerList.filter(sticker => sticker.type === categoryName);
@@ -130,11 +123,10 @@ const CategorizedStickers: React.FC<ICategorizedStickerProps> = ({ stickerList, 
     <div>
       <button
         onClick={handleToggleButton}
-        className="w-full flex flex-row justify-start items-center pl-1 mb-1 mt-0 py-1 sticky top-[-0.5rem] dark:bg-[#2B2D31] bg-bgLightModeSecond z-10 dark:text-white text-black max-h-full"
+        className="w-full flex flex-row justify-start items-center pl-1 mb-1 mt-0 py-1 gap-[2px] sticky top-[-0.5rem] dark:bg-[#2B2D31] bg-bgLightModeSecond z-10 dark:text-white text-black max-h-full"
       >
         <p className='uppercase'>{categoryName !== 'custom' ? categoryName : currentClan?.clan_name}</p>
-        <span className={isShowStickerList ? ' rotate-90' : ''}>
-          &nbsp;
+        <span className={`${isShowStickerList ? ' rotate-90' : ''}`}>
           <Icons.ArrowRight />
         </span>
       </button>
@@ -143,26 +135,27 @@ const CategorizedStickers: React.FC<ICategorizedStickerProps> = ({ stickerList, 
   )
 }
 
-interface IStickerPanelProps {
-  stickerList: any[];
-  onClickSticker: (stickerUrl: string) => void;
-}
 
 const StickerPanel: React.FC<IStickerPanelProps> = ({ stickerList, onClickSticker }) => {
   return (
-    <div className="w-auto pb-2">
-      <div className="grid grid-cols-3 gap-4 max-h-[400px] overflow-y-scroll hide-scrollbar">
-        {stickerList.map((sticker: any) => (
-          <img
-            key={sticker.id}
-            src={sticker.url}
-            alt='sticker'
-            className="w-full h-auto cursor-pointer dark:hover:bg-bgDisable hover:bg-bgLightModeButton hover:rounded-lg border border-bgHoverMember rounded-lg"
-            onClick={() => onClickSticker(sticker.url)}
-            role="button"
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      {
+        stickerList.length > 0 &&
+        <div className="w-auto pb-2">
+          <div className="grid grid-cols-3 gap-4 max-h-[400px] overflow-y-scroll hide-scrollbar">
+            {stickerList.map((sticker: any) => (
+              <img
+                key={sticker.id}
+                src={sticker.url}
+                alt='sticker'
+                className="w-full h-full aspect-square object-cover cursor-pointer dark:hover:bg-bgDisable hover:bg-bgLightModeButton hover:rounded-lg border border-bgHoverMember rounded-lg"
+                onClick={() => onClickSticker(sticker.url)}
+                role="button"
+              />
+            ))}
+          </div>
+        </div>
+      }
+    </>
   )
 }
