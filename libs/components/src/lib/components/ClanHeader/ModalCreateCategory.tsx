705a77c9809b90363e5862e3cf84d4c62987249d
hmquannnnn <minhquanhoang040103@gmail.com>
2024-08-04 21:48:39 +0700

    fix: ui bug

--- libs/components/src/lib/components/ClanHeader/ModalCreateCategory.tsx
+++ libs/components/src/lib/components/ClanHeader/ModalCreateCategory.tsx
@@ -2,7 +2,7 @@ import { InputField } from '@mezon/ui';
 import { ValidateSpecialCharacters } from '@mezon/utils';
 import { Modal } from 'flowbite-react';
 import { useState } from 'react';
-import * as Icons from '../../../../../ui/src/lib/Icons';
+import { Icons } from '@mezon/ui';
 
 type ModalCreateCategoryProps = {
 	openCreateCate: boolean;
@@ -42,13 +42,13 @@ const ModalCreateCategory = ({ openCreateCate, onClose, onCreateCategory }: Moda
 			className="bg-[#111111] dark:text-contentPrimary text-black bg-opacity-80"
 			size="lg"
 		>
-			<div className="dark:bg-[#1E1E1E] bg-bgLightMode flex items-center justify-between px-6 pt-4 border-solid border-borderDefault rounded-tl-[5px] rounded-tr-[5px]">
+			<div className="dark:bg-bgPrimary bg-bgLightModeSecond flex items-center justify-between px-6 pt-4 border-solid border-borderDefault rounded-tl-[5px] rounded-tr-[5px]">
 				<div className="text-[19px] font-bold uppercase">Create Category</div>
 				<button className="flex items-center justify-center opacity-50" onClick={onClose}>
 					<span className="text-4xl dark:hover:text-white hover:text-black">×</span>
 				</button>
 			</div>
-			<Modal.Body className="dark:bg-[#1E1E1E] bg-bgLightMode px-6 py-4">
+			<Modal.Body className="dark:bg-bgPrimary bg-bgLightModeSecond px-6 py-4">
 				<div className="flex flex-col">
 					<span className="font-[600] text-sm ">What is category's name?</span>
 					<InputField
@@ -87,7 +87,7 @@ const ModalCreateCategory = ({ openCreateCate, onClose, onCreateCategory }: Moda
         <p className={'dark:text-textSecondary text-textSecondary800 text-sm'}>By making a category private, only select members and roles will be able to view this category. Synced channels in this  category will automatically match to this setting</p>
 			</Modal.Body>
 			<div className=" text-white font-semibold text-sm flex dark:bg-bgTertiary bg-bgLightMode justify-end flex-row items-center gap-4 py-4 px-6 rounded-bl-[5px] rounded-br-[5px]">
-				<button onClick={onClose}>Cancel</button>
+				<button onClick={onClose} className={"dark:text-textSecondary text-textSecondary800"}>Cancel</button>
 				<button
 					className={`px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 bg-primary ${checkValidate ? 'opacity-50 cursor-not-allowed' : ''}`}
 					onClick={handleCreateCate}
