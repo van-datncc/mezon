ad20888186d828ab2307d107c5342e95e2edd4d4
hmquannnnn <minhquanhoang040103@gmail.com>
2024-08-05 15:52:00 +0700

    refactor code

--- libs/components/src/lib/components/GifsStickersEmojis/index.tsx
+++ libs/components/src/lib/components/GifsStickersEmojis/index.tsx
@@ -1,12 +1,12 @@
-import {useAppParams, useEscapeKey, useGifsStickersEmoji} from '@mezon/core';
-import {selectCurrentChannel} from '@mezon/store';
-import {EmojiPlaces, SubPanelName} from '@mezon/utils';
-import {ChannelStreamMode, ChannelType} from 'mezon-js';
-import {useEffect, useRef, useState} from 'react';
-import {useSelector} from 'react-redux';
+import { useAppParams, useEscapeKey, useGifsStickersEmoji } from '@mezon/core';
+import { selectCurrentChannel } from '@mezon/store';
+import { EmojiPlaces, SubPanelName } from '@mezon/utils';
+import { ChannelStreamMode, ChannelType } from 'mezon-js';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import EmojiPickerComp from '../EmojiPicker';
 import TenorGifCategories from './gifs/TenorGifCategories';
-import {InputSearch} from './inputSearch';
+import { InputSearch } from './inputSearch';
 import ImageSquare from './stickers';
 import ClanSetting from "../ClanSettings";
 import {ItemSetting} from "../ClanSettings/ItemObj";
@@ -17,30 +17,30 @@ export type GifStickerEmojiPopupOptions = {
 	mode?: number;
 };
 
-const GifStickerEmojiPopup = ({messageEmojiId, emojiAction, mode}: GifStickerEmojiPopupOptions) => {
-	const currentChannel = useSelector (selectCurrentChannel);
-	const {type} = useAppParams ();
-	const [mod, setMod] = useState (0);
-	const {subPanelActive, setSubPanelActive} = useGifsStickersEmoji ();
-	const {setValueInputSearch} = useGifsStickersEmoji ();
-	const [isShowSetting, setIsShowSetting] = useState (false)
+const GifStickerEmojiPopup = ({ messageEmojiId, emojiAction, mode }: GifStickerEmojiPopupOptions) => {
+	const currentChannel = useSelector(selectCurrentChannel);
+	const { type } = useAppParams();
+	const [mod, setMod] = useState(0);
+	const { subPanelActive, setSubPanelActive } = useGifsStickersEmoji();
+	const { setValueInputSearch } = useGifsStickersEmoji();
+	const [isShowSetting, setIsShowSetting] = useState (false);
 	
-	useEffect (() => {
-		if (Number (type) === ChannelType.CHANNEL_TYPE_GROUP) {
-			setMod (ChannelStreamMode.STREAM_MODE_GROUP);
-		} else if (Number (type) === ChannelType.CHANNEL_TYPE_DM) {
-			setMod (ChannelStreamMode.STREAM_MODE_DM);
+	useEffect(() => {
+		if (Number(type) === ChannelType.CHANNEL_TYPE_GROUP) {
+			setMod(ChannelStreamMode.STREAM_MODE_GROUP);
+		} else if (Number(type) === ChannelType.CHANNEL_TYPE_DM) {
+			setMod(ChannelStreamMode.STREAM_MODE_DM);
 		} else {
-			setMod (ChannelStreamMode.STREAM_MODE_CHANNEL);
+			setMod(ChannelStreamMode.STREAM_MODE_CHANNEL);
 		}
 	}, [type]);
 	const handleTabClick = (tab: SubPanelName) => {
-		setValueInputSearch ('');
-		setSubPanelActive (tab);
+		setValueInputSearch('');
+		setSubPanelActive(tab);
 	};
 	
-	useEscapeKey (() => setSubPanelActive (SubPanelName.NONE));
-	const emojiRefParentDiv = useRef<HTMLDivElement> (null);
+	useEscapeKey(() => setSubPanelActive(SubPanelName.NONE));
+	const emojiRefParentDiv = useRef<HTMLDivElement>(null);
 	
 	const handleCloseSetting = () => {
 		setSubPanelActive (SubPanelName.NONE);
@@ -61,7 +61,7 @@ const GifStickerEmojiPopup = ({messageEmojiId, emojiAction, mode}: GifStickerEmo
 	return (
 		<>
 			<div
-				onClick={(e) => e.stopPropagation ()}
+				onClick={(e) => e.stopPropagation()}
 				className={`w-[370px] max-sm:w-full max-sm:pt-0 max-sm:rounded-none max-sm:mt-[-0.5rem]
 			sbm:w-[500px] h-fit rounded-lg dark:bg-bgSecondary bg-bgLightMode shadow shadow-neutral-900 z-30
 			 ${emojiAction === EmojiPlaces.EMOJI_REACTION || emojiAction === EmojiPlaces.EMOJI_REACTION_BOTTOM ? 'min-h-[400px]' : 'min-h-[500px]'}`}
@@ -71,25 +71,25 @@ const GifStickerEmojiPopup = ({messageEmojiId, emojiAction, mode}: GifStickerEmo
 						<div className="flex justify-start flex-row mt-3 border-b border-blue-500 pb-1 pt-1 max-sm:justify-evenly">
 							<button
 								className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.GIFS ? ' font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-								onClick={() => handleTabClick (SubPanelName.GIFS)}
+								onClick={() => handleTabClick(SubPanelName.GIFS)}
 							>
 								Gifs
 							</button>
 							<button
 								className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.STICKERS ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-								onClick={() => handleTabClick (SubPanelName.STICKERS)}
+								onClick={() => handleTabClick(SubPanelName.STICKERS)}
 							>
 								Stickers
 							</button>
 							<button
 								className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.EMOJI ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-								onClick={() => handleTabClick (SubPanelName.EMOJI)}
+								onClick={() => handleTabClick(SubPanelName.EMOJI)}
 							>
 								Emoji
 							</button>
 						</div>
 					)}
-					<InputSearch/>
+					<InputSearch />
 				</div>
 				
 				<div className="w-full min-h-[400px] text-center md:w-[500px]" ref={emojiRefParentDiv}>
@@ -106,7 +106,7 @@ const GifStickerEmojiPopup = ({messageEmojiId, emojiAction, mode}: GifStickerEmo
 					
 					{subPanelActive === SubPanelName.STICKERS && (
 						<div className="flex h-full pr-2 w-full md:w-[500px]">
-							<ImageSquare channelId={currentChannel?.id ?? ''} mode={mod}/>
+							<ImageSquare channelId={currentChannel?.id ?? ''} mode={mod} />
 						</div>
 					)}
 					{subPanelActive === SubPanelName.EMOJI && (
@@ -114,7 +114,7 @@ const GifStickerEmojiPopup = ({messageEmojiId, emojiAction, mode}: GifStickerEmo
 							<EmojiPickerComp onClickAddButton={handleOpenSetting}/>
 						</div>
 					)}
-					{isShowEmojiPicker () && (
+					{isShowEmojiPicker() && window.innerWidth <= 640 && (
 						<div className="flex h-full pr-2 w-full md:w-[500px]">
 							<EmojiPickerComp mode={mode} messageEmojiId={messageEmojiId} onClickAddButton={handleOpenSetting}/>{' '}
 						</div>
@@ -126,7 +126,6 @@ const GifStickerEmojiPopup = ({messageEmojiId, emojiAction, mode}: GifStickerEmo
 				<ClanSetting onClose={handleCloseSetting} initialSetting={ItemSetting.EMOJI}/>
 			)}
 		</>
-	
 	);
 };
 
