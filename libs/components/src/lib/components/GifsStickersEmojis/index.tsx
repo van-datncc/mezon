390402d8bddc53e86735f7d0e3e7a9fa6bb5bdc2
hmquannnnn <minhquanhoang040103@gmail.com>
2024-08-05 16:00:00 +0700

    fix: display emoji reaction

--- libs/components/src/lib/components/GifsStickersEmojis/index.tsx
+++ libs/components/src/lib/components/GifsStickersEmojis/index.tsx
@@ -59,73 +59,71 @@ const GifStickerEmojiPopup = ({ messageEmojiId, emojiAction, mode }: GifStickerE
 	}
 	
 	return (
-		<>
-			<div
-				onClick={(e) => e.stopPropagation()}
-				className={`w-[370px] max-sm:w-full max-sm:pt-0 max-sm:rounded-none max-sm:mt-[-0.5rem]
-			sbm:w-[500px] h-fit rounded-lg dark:bg-bgSecondary bg-bgLightMode shadow shadow-neutral-900 z-30
-			 ${emojiAction === EmojiPlaces.EMOJI_REACTION || emojiAction === EmojiPlaces.EMOJI_REACTION_BOTTOM ? 'min-h-[400px]' : 'min-h-[500px]'}`}
-			>
-				<div className="w-full">
-					{emojiAction !== EmojiPlaces.EMOJI_REACTION && (
-						<div className="flex justify-start flex-row mt-3 border-b border-blue-500 pb-1 pt-1 max-sm:justify-evenly">
-							<button
-								className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.GIFS ? ' font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-								onClick={() => handleTabClick(SubPanelName.GIFS)}
-							>
-								Gifs
-							</button>
-							<button
-								className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.STICKERS ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-								onClick={() => handleTabClick(SubPanelName.STICKERS)}
-							>
-								Stickers
-							</button>
-							<button
-								className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.EMOJI ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
-								onClick={() => handleTabClick(SubPanelName.EMOJI)}
-							>
-								Emoji
-							</button>
-						</div>
-					)}
-					<InputSearch />
-				</div>
+		<div
+			onClick={(e) => e.stopPropagation()}
+			className={`w-[370px] max-sm:w-full max-sm:pt-0 max-sm:rounded-none max-sm:mt-[-0.5rem]
+		sbm:w-[500px] h-fit rounded-lg dark:bg-bgSecondary bg-bgLightMode shadow shadow-neutral-900 z-30
+		 ${emojiAction === EmojiPlaces.EMOJI_REACTION || emojiAction === EmojiPlaces.EMOJI_REACTION_BOTTOM ? 'min-h-[400px]' : 'min-h-[500px]'}`}
+		>
+			<div className="w-full">
+				{emojiAction !== EmojiPlaces.EMOJI_REACTION && (
+					<div className="flex justify-start flex-row mt-3 border-b border-blue-500 pb-1 pt-1 max-sm:justify-evenly">
+						<button
+							className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.GIFS ? ' font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
+							onClick={() => handleTabClick(SubPanelName.GIFS)}
+						>
+							Gifs
+						</button>
+						<button
+							className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.STICKERS ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
+							onClick={() => handleTabClick(SubPanelName.STICKERS)}
+						>
+							Stickers
+						</button>
+						<button
+							className={` relative px-2 mx-2 rounded-md ${subPanelActive === SubPanelName.EMOJI ? 'font-semibold dark:text-white text-black' : ' dark:text-gray-300 text-colorTextLightMode dark:hover:text-white hover:text-black '}`}
+							onClick={() => handleTabClick(SubPanelName.EMOJI)}
+						>
+							Emoji
+						</button>
+					</div>
+				)}
+				<InputSearch />
+			</div>
+			
+			<div className="w-full min-h-[400px] text-center md:w-[500px]" ref={emojiRefParentDiv}>
+				{subPanelActive === SubPanelName.GIFS && (
+					<div className="flex h-full pr-1 w-full md:w-[500px]">
+						<TenorGifCategories
+							activeTab={SubPanelName.EMOJI}
+							channelId={currentChannel?.id ?? ''}
+							channelLabel={currentChannel?.channel_label ?? ''}
+							mode={mod}
+						/>
+					</div>
+				)}
 				
-				<div className="w-full min-h-[400px] text-center md:w-[500px]" ref={emojiRefParentDiv}>
-					{subPanelActive === SubPanelName.GIFS && (
-						<div className="flex h-full pr-1 w-full md:w-[500px]">
-							<TenorGifCategories
-								activeTab={SubPanelName.EMOJI}
-								channelId={currentChannel?.id ?? ''}
-								channelLabel={currentChannel?.channel_label ?? ''}
-								mode={mod}
-							/>
-						</div>
-					)}
-					
-					{subPanelActive === SubPanelName.STICKERS && (
-						<div className="flex h-full pr-2 w-full md:w-[500px]">
-							<ImageSquare channelId={currentChannel?.id ?? ''} mode={mod} />
-						</div>
-					)}
-					{subPanelActive === SubPanelName.EMOJI && (
-						<div className="flex h-full pr-2 w-full md:w-[500px]">
-							<EmojiPickerComp onClickAddButton={handleOpenSetting}/>
-						</div>
-					)}
-					{isShowEmojiPicker() && window.innerWidth <= 640 && (
-						<div className="flex h-full pr-2 w-full md:w-[500px]">
-							<EmojiPickerComp mode={mode} messageEmojiId={messageEmojiId} onClickAddButton={handleOpenSetting}/>{' '}
-						</div>
-					)}
-				</div>
+				{subPanelActive === SubPanelName.STICKERS && (
+					<div className="flex h-full pr-2 w-full md:w-[500px]">
+						<ImageSquare channelId={currentChannel?.id ?? ''} mode={mod} />
+					</div>
+				)}
+				{subPanelActive === SubPanelName.EMOJI && (
+					<div className="flex h-full pr-2 w-full md:w-[500px]">
+						<EmojiPickerComp onClickAddButton={handleOpenSetting}/>
+					</div>
+				)}
+				{isShowEmojiPicker() && (
+					<div className="flex h-full pr-2 w-full md:w-[500px]">
+						<EmojiPickerComp mode={mode} messageEmojiId={messageEmojiId} onClickAddButton={handleOpenSetting}/>{' '}
+					</div>
+				)}
 			</div>
 			
 			{isShowSetting && (
 				<ClanSetting onClose={handleCloseSetting} initialSetting={ItemSetting.EMOJI}/>
 			)}
-		</>
+		</div>
 	);
 };
 
