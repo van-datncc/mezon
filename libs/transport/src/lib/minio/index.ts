187d9674544dd9941e4351da949a7380fc360b3a
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-08-07 16:12:26 +0700

    Merge branch 'refactor-sticker-emoji-CRUD' of https://github.com/nguyentrannhan/mezon-fe into refactor-emoji-sticker

--- libs/transport/src/lib/minio/index.ts
+++ libs/transport/src/lib/minio/index.ts
@@ -16,14 +16,11 @@ export function uploadImageToMinIO(url: string, stream: Buffer, size: number) {
 	return fetch(url, { method: 'PUT', body: stream });
 }
 
-export async function handleUploadFile(
+export async function handleUploadEmoticon(
 	client: Client,
 	session: Session,
-	currentClanId: string,
-	currentChannelId: string,
 	filename: string,
 	file: File,
-	path?: string,
 ): Promise<ApiMessageAttachment> {
 	// eslint-disable-next-line no-async-promise-executor
 	return new Promise<ApiMessageAttachment>(async function (resolve, reject) {
@@ -35,47 +32,23 @@ export async function handleUploadFile(
 				fileType = `text/${fileExtension}`;
 			}
 			
-			const ms = new Date().getMinutes();			
-			filename = ms + filename;
-			filename = filename.replace(/-|\(|\)| /g, '_')
-			if (!currentClanId) {
-				currentClanId = "0";
-			}
-			let fullfilename = currentClanId + '/' + currentChannelId + '/' + session.user_id + '/' + filename;
-			if (path) {
-				fullfilename = (path + '/') + fullfilename;
-			}
-
 			const buf = await file?.arrayBuffer();
-			const data = await client.uploadAttachmentFile(session, {
-				filename: fullfilename,
-				filetype: fileType,
-				size: file.size,
-			});
-			if (!data?.url) {
-				reject(new Error('Failed to upload file. URL not available.'));
-				return;
-			}
-			const res = await uploadImageToMinIO(data.url || '', Buffer.from(buf), file.size);
-			if (res.status !== 200) {
-				throw new Error('Failed to upload file to MinIO.');
-			}
-			const url = 'https://cdn.mezon.vn/' + fullfilename;
-			resolve({
-				filename: file.name,
-				url: url,
-				filetype: fileType,
-				size: file.size,
-				width: 0,
-				height: 0,
-			});
+
+			return uploadFile(client, session, filename, fileType, file.size, Buffer.from(buf));			
 		} catch (error) {
 			reject(new Error(`${error}`));
 		}
 	});
 }
 
-export async function handleUploadFileMobile(client: Client, session: Session, fullfilename: string, file: any): Promise<ApiMessageAttachment> {
+export async function handleUploadFile(
+	client: Client,
+	session: Session,
+	currentClanId: string,
+	currentChannelId: string,
+	filename: string,
+	file: File,
+): Promise<ApiMessageAttachment> {
 	// eslint-disable-next-line no-async-promise-executor
 	return new Promise<ApiMessageAttachment>(async function (resolve, reject) {
 		try {
@@ -85,46 +58,98 @@ export async function handleUploadFileMobile(client: Client, session: Session, f
 				const fileExtension = fileNameParts[fileNameParts.length - 1].toLowerCase();
 				fileType = `text/${fileExtension}`;
 			}
-			if (file?.uri) {
+			const fullfilename = createUploadFilePath(session, currentChannelId, currentChannelId, filename);
+			const buf = await file?.arrayBuffer();
+
+			return uploadFile(client, session, fullfilename, fileType, file.size, Buffer.from(buf));			
+		} catch (error) {
+			reject(new Error(`${error}`));
+		}
+	});
+}
+
+export async function handleUploadFileMobile(
+	client: Client, 
+	session: Session,
+	currentClanId: string,
+	currentChannelId: string,
+	filename: string,
+	file: any,
+): Promise<ApiMessageAttachment> {
+	// eslint-disable-next-line no-async-promise-executor
+	return new Promise<ApiMessageAttachment>(async function (resolve, reject) {
+		try {
+			let fileType = file.type;
+			if (!fileType) {
+				const fileNameParts = file.name.split('.');
+				const fileExtension = fileNameParts[fileNameParts.length - 1].toLowerCase();
+				fileType = `text/${fileExtension}`;
+			}
+			if (file?.uri) {			
 				const arrayBuffer = BufferMobile.from(file.fileData, 'base64');
 				if (!arrayBuffer) {
 					console.log('Failed to read file data.');
 					return;
 				}
-				const data = await client.uploadAttachmentFile(session, {
-					filename: fullfilename,
-					filetype: fileType,
-					size: file.size,
-				});
-				if (!data?.url) {
-					console.log('Failed to upload file. URL not available.');
-					return;
-				}
-				const buffer = BufferMobile.from(arrayBuffer);
-
-				const res = await fetch(data.url, {
-					method: 'PUT',
-					headers: {
-						'Content-Type': fileType,
-						'Content-Length': file?.size?.toString() || '1000',
-					},
-					body: buffer,
-				});
-				if (res.status !== 200) {
-					throw new Error('Failed to upload file to MinIO.');
-				}
-				const url = 'https://cdn.mezon.vn/' + fullfilename;
-				resolve({
-					filename: file.name,
-					url: url,
-					filetype: fileType,
-					size: file.size,
-					width: 0,
-					height: 0,
-				});
+				const fullfilename = createUploadFilePath(session, currentChannelId, currentChannelId, filename);
+				return uploadFile(client, session, fullfilename, fileType, file.size, arrayBuffer);				
 			}
 		} catch (error) {
-			console.log('handleUploadFileMobile Error: ', error);
+			reject(new Error(`${error}`));
+		}
+	});
+}
+
+export function createUploadFilePath(
+	session: Session,
+	currentClanId: string,
+	currentChannelId: string,
+	filename: string,
+): string {
+	const ms = new Date().getMinutes();			
+	filename = ms + filename;
+	filename = filename.replace(/-|\(|\)| /g, '_')
+	if (!currentClanId) {
+		currentClanId = "0";
+	}
+	return currentClanId + '/' + currentChannelId + '/' + session.user_id + '/' + filename;
+}
+
+export async function uploadFile(
+	client: Client,
+	session: Session,
+	filename: string,
+	type: string,
+	size: number,
+	buf: Buffer
+
+): Promise<ApiMessageAttachment> {
+	// eslint-disable-next-line no-async-promise-executor
+	return new Promise<ApiMessageAttachment>(async function (resolve, reject) {
+		try {			
+			const data = await client.uploadAttachmentFile(session, {
+				filename: filename,
+				filetype: type,
+				size: size,
+			});
+			if (!data?.url) {
+				reject(new Error('Failed to upload file. URL not available.'));
+				return;
+			}
+			const res = await uploadImageToMinIO(data.url || '', buf, size);
+			if (res.status !== 200) {
+				throw new Error('Failed to upload file to MinIO.');
+			}
+			const url = 'https://cdn.mezon.vn/' + filename;
+			resolve({
+				filename: filename,
+				url: url,
+				filetype: type,
+				size: size,
+				width: 0,
+				height: 0,
+			});
+		} catch (error) {
 			reject(new Error(`${error}`));
 		}
 	});
