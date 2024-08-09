89cefa65347c8921399f48cd44e1623e60be110f
thai.phamquoc <110405911+Cut666@users.noreply.github.com>
2024-08-09 10:17:20 +0700

    realtime user

--- libs/store/src/lib/clanProfile/clanProfile.slice.ts
+++ libs/store/src/lib/clanProfile/clanProfile.slice.ts
@@ -10,7 +10,7 @@ export interface UserClanProfileEntity extends IClanProfile {
 }
 
 export const mapUserClanProfileToEntity = (userClanProfileRes: ApiClanProfile) => {
-	const id = userClanProfileRes.clan_id as string;
+	const id = `${userClanProfileRes.clan_id}${userClanProfileRes.user_id}`;
 	return { ...userClanProfileRes, id };
 };
 
