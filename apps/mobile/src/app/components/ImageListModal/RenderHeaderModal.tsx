bbcf1d42a7686f661ec39afdc5138f2b42fb4c7f
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-08-12 09:24:18 +0700

    update: import

--- apps/mobile/src/app/components/ImageListModal/RenderHeaderModal.tsx
+++ apps/mobile/src/app/components/ImageListModal/RenderHeaderModal.tsx
@@ -1,8 +1,8 @@
 import { useTheme } from '@mezon/mobile-ui';
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { CloseSmallIcon } from '../../../../../../libs/mobile-components/src/lib/icons2';
 import { style } from './styles';
+import { Icons } from '@mezon/mobile-components';
 
 interface IRenderFooterModalProps {
 	onClose?: any;
@@ -14,7 +14,7 @@ export const RenderHeaderModal = React.memo((props: IRenderFooterModalProps) => 
 
 	return (
 		<TouchableOpacity activeOpacity={0.8} onPress={props.onClose} style={styles.btnCloseHeader}>
-			<CloseSmallIcon width={30} height={30} color={'white'} />
+			<Icons.CloseSmallIcon color={'white'} height={30} width={30} />
 		</TouchableOpacity>
 	);
 });
