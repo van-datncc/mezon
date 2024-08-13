92a7489e7d558220574e628b8ffe38a3bbe2c7bd
Nhan Nguyen <nguyentran@ncc.asia>
2024-08-13 17:34:45 +0700

    fix build

--- libs/components/src/lib/components/ClanSettings/SettingEmoji/SettingEmojiList/index.tsx
+++ libs/components/src/lib/components/ClanSettings/SettingEmoji/SettingEmojiList/index.tsx
@@ -1,38 +1,29 @@
-import { ApiClanEmojiListResponse } from "mezon-js/api.gen";
-import SettingEmojiItem from "../SettingEmojiItem";
-
+import { ClanEmoji } from 'mezon-js';
+import SettingEmojiItem from '../SettingEmojiItem';
 
 type SettingEmojiListProps = {
-  title: string;
-  emojiList: ApiClanEmojiListResponse[]
+	title: string;
+	emojiList: ClanEmoji[];
 };
 
 const SettingEmojiList = ({ title, emojiList }: SettingEmojiListProps) => {
-
-
-  return (
-    <div className={'flex flex-col gap-3 dark:text-textDarkTheme text-textLightTheme pb-[60px]'}>
-      <h2 className="text-base font-bold">
-        {title} - {250 - emojiList.length} slots available
-      </h2>
-      <div className={'flex items-center flex-row w-full dark:text-textSecondary text-textSecondary800'}>
-        <p className={'w-14 text-xs font-bold '}>
-          IMAGE
-        </p>
-        <p className={'flex-1 text-xs font-bold'}>
-          NAME
-        </p>
-        <p className={'flex-1 flex text-xs font-bold'}>
-          UPLOADED BY
-        </p>
-      </div>
-      <div className={'flex flex-col w-full'}>
-        {emojiList.map((emoji) => (
-          <SettingEmojiItem emoji={emoji} key={emoji.id} />
-        ))}
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={'flex flex-col gap-3 dark:text-textDarkTheme text-textLightTheme pb-[60px]'}>
+			<h2 className="text-base font-bold">
+				{title} - {250 - emojiList.length} slots available
+			</h2>
+			<div className={'flex items-center flex-row w-full dark:text-textSecondary text-textSecondary800'}>
+				<p className={'w-14 text-xs font-bold '}>IMAGE</p>
+				<p className={'flex-1 text-xs font-bold'}>NAME</p>
+				<p className={'flex-1 flex text-xs font-bold'}>UPLOADED BY</p>
+			</div>
+			<div className={'flex flex-col w-full'}>
+				{emojiList.map((emoji) => (
+					<SettingEmojiItem emoji={emoji} key={emoji.id} />
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default SettingEmojiList;
