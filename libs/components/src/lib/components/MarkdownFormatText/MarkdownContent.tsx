012520db685e495147e0b0ccc88f450cc936c3c0
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-08-13 09:42:23 +0700

    render image fr link - done

--- libs/components/src/lib/components/MarkdownFormatText/MarkdownContent.tsx
+++ libs/components/src/lib/components/MarkdownFormatText/MarkdownContent.tsx
@@ -1,10 +1,12 @@
 import { selectTheme } from '@mezon/store';
+import { handleUrlInput, isValidUrl } from '@mezon/transport';
+import { ETypeLinkMedia } from '@mezon/utils';
 import clx from 'classnames';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
 import { useSelector } from 'react-redux';
 import remarkGfm from 'remark-gfm';
-import { PreClass } from '../../components';
+import { MessageImage, PreClass } from '../../components';
 
 type MarkdownContentOpt = {
 	content?: string;
@@ -14,6 +16,22 @@ type MarkdownContentOpt = {
 
 export const MarkdownContent: React.FC<MarkdownContentOpt> = ({ content, isSingleLine, isTokenClickAble }) => {
 	const appearanceTheme = useSelector(selectTheme);
+
+	const [isImage, setIsImage] = useState<boolean>(false);
+	const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+	useEffect(() => {
+		if (content && isValidUrl(content)) {
+			handleUrlInput(content).then((result) => {
+				if (result.filetype && result.filetype.startsWith(ETypeLinkMedia.IMAGE_PREFIX)) {
+					setTimeout(() => {
+						setIsImage(true);
+						setImageUrl(content);
+					}, 1000);
+				}
+			});
+		}
+	}, [content]);
 
 	const onClickLink = useCallback(
 		(url: string) => {
@@ -34,29 +52,33 @@ export const MarkdownContent: React.FC<MarkdownContentOpt> = ({ content, isSingl
 	return (
 		<article style={{ letterSpacing: '-0.01rem' }} className={classes}>
 			<div className="lineText contents dark:text-white text-colorTextLightMode">
-				<Markdown
-					children={content}
-					remarkPlugins={[remarkGfm]}
-					components={{
-						pre: PreClass,
-						p: 'span',
-						a: (props) => (
-							<span
-								onClick={() => onClickLink(props.href ?? '')}
-								rel="noopener noreferrer"
-								style={{
-									color: 'rgb(59,130,246)',
-									cursor: isSingleLine || !isTokenClickAble ? 'text' : 'pointer',
-									wordBreak: 'break-word',
-									textDecoration: isSingleLine || !isTokenClickAble ? 'none' : 'underline',
-								}}
-								className="tagLink"
-							>
-								{props.children}
-							</span>
-						),
-					}}
-				/>
+				{isImage && imageUrl ? (
+					<MessageImage attachmentData={{ url: imageUrl }} />
+				) : (
+					<Markdown
+						children={content}
+						remarkPlugins={[remarkGfm]}
+						components={{
+							pre: PreClass,
+							p: 'span',
+							a: (props) => (
+								<span
+									onClick={() => onClickLink(props.href ?? '')}
+									rel="noopener noreferrer"
+									style={{
+										color: 'rgb(59,130,246)',
+										cursor: isSingleLine || !isTokenClickAble ? 'text' : 'pointer',
+										wordBreak: 'break-word',
+										textDecoration: isSingleLine || !isTokenClickAble ? 'none' : 'underline',
+									}}
+									className="tagLink"
+								>
+									{props.children}
+								</span>
+							),
+						}}
+					/>
+				)}
 			</div>
 		</article>
 	);
