2849972552ca91e06adf80a3d834fcc41a678991
thuy1901 <thuy.nguyenthithu1@ncc.asia>
2024-08-14 15:31:00 +0700

    UI channel setting permission, typing user

--- libs/components/src/lib/components/ChannelSetting/Component/PermissionsChannel/PermissionManage/ListPermission.tsx
+++ libs/components/src/lib/components/ChannelSetting/Component/PermissionsChannel/PermissionManage/ListPermission.tsx
@@ -1,5 +1,6 @@
 import { selectAllPermissionsDefault } from "@mezon/store";
 import { Icons } from "@mezon/ui";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const ListPermission = () => {
@@ -27,19 +28,43 @@ type ItemPermissionProps = {
     title?: string;
 }
 
+enum TypeChoose  {
+    Remove= 1,
+    Or=2,
+    Tick=3,
+}
+
 const ItemPermission = (props: ItemPermissionProps) => {
     const {id, title} = props;
+    const [choose, setChoose] = useState(TypeChoose.Or);
+    const handleSelect = (option: number) => {
+        setChoose(option);
+    };
+
+    const className = {
+        wrapperClass: 'h-[26px] flex rounded-md overflow-hidden border dark:border-gray-700 border-gray-300',
+        buttonClass: 'w-8 flex justify-center items-center border dark:border-gray-700 border-gray-300',
+    }
     return(
         <div className="flex justify-between items-center">
-            <p>{title}</p>
-            <div className="h-[26px] flex rounded-md overflow-hidden border dark:border-gray-700">
-                <button className="w-8 flex justify-center items-center">
+            <p className="font-semibold text-base">{title}</p>
+            <div className={className.wrapperClass}>
+                <button 
+                    className={`${className.buttonClass} ${choose === TypeChoose.Remove ? 'bg-colorDanger' : ''}`}
+                    onClick={() => handleSelect(TypeChoose.Remove)}
+                >
                     <Icons.Close defaultSize='size-4'/>
                 </button>
-                <button className="w-8 flex justify-center items-center">
+                <button 
+                    onClick={() => handleSelect(TypeChoose.Or)}
+                    className={`${className.buttonClass} ${choose === TypeChoose.Or ? 'bg-bgModifierHover' : ''}`}
+                >
                     <Icons.IconOr defaultSize='size-4'/>
                 </button>
-                <button className="w-8 flex justify-center items-center">
+                <button 
+                    onClick={() => handleSelect(TypeChoose.Tick)}
+                    className={`${className.buttonClass} ${choose === TypeChoose.Tick ? 'bg-colorSuccess' : ''}`}
+                >
                     <Icons.IconTick defaultSize='size-4'/>
                 </button>
             </div>
--- libs/components/src/lib/components/MemberProfile/index.tsx
+++ libs/components/src/lib/components/MemberProfile/index.tsx
@@ -1,9 +1,9 @@
 import { AvatarImage, Icons, ShortUserProfile } from '@mezon/components';
 import { useChannelMembersActions, useOnClickOutside } from '@mezon/core';
-import { ChannelMembersEntity, selectAllAccount, selectCurrentClan, selectCurrentClanId } from '@mezon/store';
+import { ChannelMembersEntity, selectAllAccount, selectCurrentClan, selectCurrentClanId, selectTypingUserIds } from '@mezon/store';
 import { MemberProfileType, MouseButton } from '@mezon/utils';
 import { ChannelStreamMode, ChannelType } from 'mezon-js';
-import { useMemo, useRef, useState } from 'react';
+import { memo, useMemo, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { OfflineStatus, OnlineStatus } from '../../../../../ui/src/lib/Icons';
 import { Coords } from '../ChannelLink';
@@ -152,6 +152,8 @@ function MemberProfile({
 
 	const isMemberChannel = useMemo(() => positionType === MemberProfileType.MEMBER_LIST, [positionType]);
 
+	const isListDm = useMemo(() => positionType === MemberProfileType.DM_LIST, [positionType]);
+
 	const isAnonymous = useMemo(() => (isFooter ? userProfile?.user?.id : user?.user?.id) === process.env.NX_CHAT_APP_ANNONYMOUS_USER_ID, []);
 
 	const userName = useMemo(() => isFooter ? userProfile?.user?.username || '' : name || '', []);
@@ -184,11 +186,14 @@ function MemberProfile({
 						isAnonymous={isAnonymous}
 					/>
 					{!isHideIconStatus && avatar !== 'assets/images/avatar-group.png' ? (
-						<span
-							className={`absolute bottom-[0px] right-[-4px] inline-flex items-center justify-center gap-1 p-[3px] text-sm text-white dark:bg-bgSecondary bg-bgLightMode rounded-full`}
-						>
-							{status ? <OnlineStatus /> : <OfflineStatus />}
-						</span>
+						<StatusUser 
+							isListDm={isListDm}
+							isMemberChannel={isMemberChannel}
+							isMemberDMGroup={isMemberDMGroup}
+							status={status}
+							directMessageValue={directMessageValue}
+							userId={user?.user?.id}
+						/>
 					) : (
 						<></>
 					)}
@@ -298,3 +303,41 @@ function MemberProfile({
 }
 
 export default MemberProfile;
+
+type StatusUserProps = {
+	status?: boolean;
+	isMemberDMGroup: boolean;
+	isMemberChannel: boolean;
+	isListDm: boolean;
+	directMessageValue?: directMessageValueProps;
+	userId?: string;
+}
+
+const StatusUser = memo((props: StatusUserProps) =>{
+	const {status, isMemberChannel, isMemberDMGroup, isListDm, directMessageValue, userId =''} = props;
+	const typingUserIds = useSelector(selectTypingUserIds);
+
+	const checkTypingUser = useMemo(() => {
+		switch (true) {
+			case isMemberDMGroup:
+				return typingUserIds?.includes(userId);
+			case isMemberChannel:
+				return typingUserIds?.includes(userId);
+
+			case isListDm:
+				return Number(directMessageValue?.type) === ChannelType.CHANNEL_TYPE_DM && 
+					typingUserIds?.includes(directMessageValue?.userId[0] || '');
+
+			default:
+				return false;
+		}
+	}, [isMemberDMGroup, isMemberChannel, isListDm, typingUserIds]);
+
+	return(
+		<span
+			className={`absolute bottom-[0px] inline-flex items-center justify-center gap-1 p-[3px] text-sm text-white dark:bg-bgSecondary bg-bgLightMode ${(status && checkTypingUser) ? 'rounded-lg -right-3' : 'rounded-full right-[-4px]'}`}
+		>
+			{status ? (checkTypingUser ? <Icons.IconLoadingTyping defaultSize='bg-colorSuccess w-5 rounded-lg'/> : <OnlineStatus />) : <OfflineStatus />}
+		</span>
+	);
+})
