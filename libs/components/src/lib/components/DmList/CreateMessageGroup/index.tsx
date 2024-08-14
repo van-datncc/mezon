401e4bccec07b0ca2d6d5af6fd6bbbbc6a6a5c26
anbuihoang <an.buihoang@ncc.asia>
2024-08-14 17:56:04 +0700

    update ui - select friend - search friend in modal create group DM chat

--- libs/components/src/lib/components/DmList/CreateMessageGroup/index.tsx
+++ libs/components/src/lib/components/DmList/CreateMessageGroup/index.tsx
@@ -1,13 +1,13 @@
 import { useAppNavigation, useFriends } from '@mezon/core';
-import { FriendsEntity, directActions, selectAllFriends, useAppDispatch } from '@mezon/store';
-import { InputField } from '@mezon/ui';
+import { FriendsEntity, IFriend, directActions, selectAllFriends, useAppDispatch } from '@mezon/store';
+import { Icons, InputField } from '@mezon/ui';
 import { ChannelType } from 'mezon-js';
 import { ApiCreateChannelDescRequest } from 'mezon-js/api.gen';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { AvatarImage } from '../../AvatarImage/AvatarImage';
 import EmptySearchFriends from './EmptySearchFriends';
-import ListFriends from './ListFriends';
 
 type CreateMessageGroupProps = {
 	isOpen: boolean;
@@ -188,16 +188,41 @@ const CreateMessageGroup = ({ onClose }: CreateMessageGroupProps) => {
 				</div>
 				{listFriends.length > 0 ? (
 					<div ref={boxRef} className="w-full h-[190px] overflow-y-auto overflow-x-hidden thread-scroll">
-						{
-							<ListFriends
-								ref={boxRef}
-								listFriends={listFriends}
-								idActive={idActive}
-								selectedFriends={selectedFriends}
-								setIdActive={setIdActive}
-								handleCheckboxChange={handleCheckboxChange}
-							/>
-						}
+						{listFriends.map((friend: IFriend, index) => (
+							<div
+								key={friend.id}
+								onMouseEnter={() => setIdActive(friend.id ?? '')}
+								onMouseLeave={() => setIdActive(friend.id ?? '')}
+								className={`${idActive === friend.id ? 'dark:bg-bgModifierHover bg-bgLightModeThird' : ''} flex items-center h-10 px-2 ml-3 mr-2 py-[8px] rounded-[6px] cursor-pointer`}
+							>
+								<label className="flex flex-row items-center justify-between w-full gap-2 py-[3px] cursor-pointer">
+									<div className="flex flex-row items-center gap-2">
+										<AvatarImage
+											alt={''}
+											userName={friend.user?.username}
+											src={friend.user?.avatar_url}
+											className="size-8"
+											classNameText="text-[9px] min-w-5 min-h-5 pt-[3px]"
+										/>
+										<span className={`text-base font-medium dark:text-white text-textLightTheme one-line`}>
+											{friend.user?.display_name}
+										</span>
+										<span className="dark:text-colorNeutral text-colorTextLightMode font-medium">{friend.user?.username}</span>
+									</div>
+									<div className="relative flex flex-row justify-center">
+										<input
+											id={`checkbox-item-${index}`}
+											type="checkbox"
+											value={friend.id}
+											checked={selectedFriends.includes(friend?.id || '')}
+											onChange={handleCheckboxChange}
+											className="peer appearance-none forced-colors:appearance-auto relative w-4 h-4 border dark:border-textPrimary border-gray-600 rounded-md focus:outline-none"
+										/>
+										<Icons.Check className="absolute invisible peer-checked:visible forced-colors:hidden w-4 h-4" />
+									</div>
+								</label>
+							</div>
+						))}
 					</div>
 				) : (
 					<EmptySearchFriends />
