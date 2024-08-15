da346d70065396196ca5374b4c4f113b643bd7c0
Tungnqs <thanhlamtainguyen@gmail.com>
2024-08-15 17:02:43 +0700

    implement fetch applications in admin panel

--- libs/store/src/lib/application/applications.slice.ts
+++ libs/store/src/lib/application/applications.slice.ts
@@ -1,7 +1,8 @@
 import { LoadingStatus } from '@mezon/utils';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import memoizee from 'memoizee';
 import { ApiAddAppRequest, ApiAppList } from 'mezon-js/api.gen';
-import { ensureSession, getMezonCtx } from '../helpers';
+import { MezonValueContext, ensureSession, getMezonCtx } from '../helpers';
 
 export const ADMIN_APPLICATIONS = 'adminApplication';
 
@@ -21,28 +22,55 @@ export const applicationInitialState: IApplicationState = {
 	},
 };
 
-export const createApplication = createAsyncThunk(
-    'adminApplication/createApplication',
-    async(data: {request: ApiAddAppRequest}, thunkAPI)=>{
-        try{
-            const mezon = await ensureSession(getMezonCtx(thunkAPI));
-            const response = await mezon.client.addApp(mezon.session, data.request);
-            console.log(response);
-        }
-        catch(err){
-            console.log(err);
-            return thunkAPI.rejectWithValue({err});
-        }
-    }
-)
+const FETCH_CACHED_TIME = 3 * 60 * 1000;
+
+export interface IFetchAppsArg {
+	noCache: boolean;
+}
+
+const fetchApplicationsCached = memoizee((mezon: MezonValueContext) => mezon.client.ListApp(mezon.session), {
+	promise: true,
+	maxAge: FETCH_CACHED_TIME,
+	normalizer: (args) => {
+		return args[0].session.username as string;
+	},
+});
+
+export const fetchApplications = createAsyncThunk('adminApplication/fetchApplications', async ({ noCache }: IFetchAppsArg, thunkAPI) => {
+	try {
+		const mezon = await ensureSession(getMezonCtx(thunkAPI));
+		if (noCache) {
+			fetchApplicationsCached.clear(mezon);
+		}
+		const response = await fetchApplicationsCached(mezon);
+		return response;
+	} catch (err) {
+		console.log(err);
+		return thunkAPI.rejectWithValue({});
+	}
+});
+
+export const createApplication = createAsyncThunk('adminApplication/createApplication', async (data: { request: ApiAddAppRequest }, thunkAPI) => {
+	try {
+		const mezon = await ensureSession(getMezonCtx(thunkAPI));
+		const response = await mezon.client.addApp(mezon.session, data.request);
+		console.log(response);
+	} catch (err) {
+		console.log(err);
+		return thunkAPI.rejectWithValue({ err });
+	}
+});
 
 export const adminApplicationSlice = createSlice({
 	name: ADMIN_APPLICATIONS,
 	initialState: applicationInitialState,
 	reducers: {},
-	// extraReducers(builder){
-	//     builder.addCase()
-	// }
+	extraReducers(builder) {
+		builder.addCase(fetchApplications.fulfilled, (state, action) => {
+			state.allApps = action.payload;
+			state.loadingStatus = 'loaded';
+		});
+	},
 });
 
 export const adminApplicationReducer = adminApplicationSlice.reducer;
