5f7ef0ca8f8fcc320f0c7f527527143476c03526
thuy1901 <thuy.nguyenthithu1@ncc.asia>
2024-08-15 17:03:25 +0700

    update logic join channel when click link

--- libs/components/src/lib/components/MarkdownFormatText/MarkdownContent.tsx
+++ libs/components/src/lib/components/MarkdownFormatText/MarkdownContent.tsx
@@ -1,7 +1,6 @@
-import { useAppNavigation, useInvite } from '@mezon/core';
-import { selectTheme, useAppDispatch } from '@mezon/store';
+import { useAppNavigation } from '@mezon/core';
+import { selectTheme } from '@mezon/store';
 import clx from 'classnames';
-import { ApiInviteUserRes } from 'mezon-js/api.gen';
 import { memo, useCallback } from 'react';
 import Markdown from 'react-markdown';
 import { useSelector } from 'react-redux';
@@ -15,33 +14,27 @@ type MarkdownContentOpt = {
 	isRenderImage: boolean;
 };
 
-const navigateToChannel = async (url: string, navigate: any, toChannelPage: any, dispatch: any, inviteUser: any) => {
+const navigateToChannel = async (url: string, navigate: any) => {
 	const regex = /\/invite\/(\d+)/;
 	const match = url.match(regex);
 	if (match) {
 		const [_, inviteId] = match;
 		if(inviteId){
-			inviteUser(inviteId).then((res: ApiInviteUserRes) => {
-				if (res.channel_id && res.clan_id) {
-					navigate(`/chat/clans/${res.clan_id}/channels/${res.channel_id}`);
-				}
-			});
+			navigate("/invite/" + inviteId);
 		}
 	}
 };
 
 export const MarkdownContent: React.FC<MarkdownContentOpt> = ({ content, isJumMessageEnabled, isTokenClickAble, isRenderImage }) => {
 	const appearanceTheme = useSelector(selectTheme);
-	const { navigate, toChannelPage } = useAppNavigation();
-	const dispatch = useAppDispatch();
-	const { inviteUser } = useInvite();
+	const { navigate } = useAppNavigation();
 	const origin = window.location.origin + "/invite/";
 
 	const onClickLink = useCallback(
 		(url: string) => {
 			if (!isJumMessageEnabled || isTokenClickAble) {
 				if (url.startsWith(origin)) {
-					navigateToChannel(url, navigate, toChannelPage, dispatch, inviteUser);
+					navigateToChannel(url, navigate);
 				} else {
 					window.open(url, '_blank');
 				}
