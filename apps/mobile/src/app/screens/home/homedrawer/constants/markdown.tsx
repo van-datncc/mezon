5db5b5eb0958b212eabdbc62f43421053ca296cb
hoang.tranlehuy <hoang.tranlehuy@ncc.asia>
2024-08-19 11:15:59 +0700

    fix link message reply mobile

--- apps/mobile/src/app/screens/home/homedrawer/constants/markdown.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/constants/markdown.tsx
@@ -106,7 +106,7 @@ export const markdownStyles = (colors: Attributes) =>
 		link: {
 			color: colors.textLink,
 			textDecorationLine: 'none',
-			lineHeight: size.s_20,
+			lineHeight: size.s_17,
 		},
 		iconEmojiInMessage: {
 			width: size.s_20,
