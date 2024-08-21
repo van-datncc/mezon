5a3858f6127a5b0e6983bf32f92354a25ddbbe52
thuy1901 <thuy.nguyenthithu1@ncc.asia>
2024-08-21 08:52:06 +0700

    fix conflict

--- libs/components/src/lib/components/MemberList/MemberItem.tsx
+++ libs/components/src/lib/components/MemberList/MemberItem.tsx
@@ -13,7 +13,7 @@ export type MemberItemProps = {
 	name?: string;
 };
 
-function MemberItem({ user, listProfile, isOffline, positionType, dataMemberCreate, directMessageId }: MemberItemProps) {
+function MemberItem({ user, listProfile, isOffline, positionType, dataMemberCreate, directMessageId, name }: MemberItemProps) {
 	const userStatus = useMemberStatus(user.user?.id || '');
 	const userCustomStatus = useMemberCustomStatus(user.user?.id || '');
 
