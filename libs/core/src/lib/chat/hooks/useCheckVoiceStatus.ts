35cad16c07c7d452a3d140bbeac44e4ba2e87a19
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-08-23 14:25:25 +0700

    fix message line

--- libs/core/src/lib/chat/hooks/useCheckVoiceStatus.ts
+++ libs/core/src/lib/chat/hooks/useCheckVoiceStatus.ts
@@ -1,16 +1,16 @@
-import { selectNumberMemberVoiceChannel } from '@mezon/store-mobile';
+import { selectNumberMemberVoiceChannel } from '@mezon/store';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 export const useCheckVoiceStatus = (channelId: string) => {
-    const numberMembersVoice = useSelector(selectNumberMemberVoiceChannel(channelId));
+	const numberMembersVoice = useSelector(selectNumberMemberVoiceChannel(channelId));
 
-    const checkVoiceStatus = useMemo(() => {
-        if (!!channelId && !!numberMembersVoice) {
-            return numberMembersVoice >= 2;
-        }
-        return false;
-    }, [numberMembersVoice, channelId]);
+	const checkVoiceStatus = useMemo(() => {
+		if (!!channelId && !!numberMembersVoice) {
+			return numberMembersVoice >= 2;
+		}
+		return false;
+	}, [numberMembersVoice, channelId]);
 
-    return checkVoiceStatus;
+	return checkVoiceStatus;
 };
