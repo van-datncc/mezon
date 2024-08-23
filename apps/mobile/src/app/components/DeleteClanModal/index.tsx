58124ada3c5e702813cc11161cdaf49e23333b2f
VyNCC <vy.huynhngoc@ncc.asia>
2024-08-23 16:23:57 +0700

    update ui for confirm popup clan detection

--- apps/mobile/src/app/components/DeleteClanModal/index.tsx
+++ apps/mobile/src/app/components/DeleteClanModal/index.tsx
@@ -1,11 +1,10 @@
 import { useBottomSheetModal } from '@gorhom/bottom-sheet';
 import { STORAGE_CHANNEL_CURRENT_CACHE, STORAGE_CLAN_ID, remove, save, setDefaultChannelLoader } from '@mezon/mobile-components';
-import { useTheme } from '@mezon/mobile-ui';
 import { channelsActions, clansActions, getStoreAsync, selectAllClans, selectCurrentClan, useAppDispatch } from '@mezon/store-mobile';
 import { useNavigation } from '@react-navigation/native';
-import { useCallback, useState } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, TextInput } from 'react-native';
+import { Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { APP_SCREEN } from '../../navigation/ScreenTypes';
 import { MezonConfirm } from '../../temp-ui';
@@ -18,61 +17,42 @@ const DeleteClanModal = ({ isVisibleModal, visibleChange }: { isVisibleModal: bo
 	const dispatch = useAppDispatch();
 	const clans = useSelector(selectAllClans);
 	const navigation = useNavigation<any>();
-	const { themeValue } = useTheme();
 
-	const [inputValue, setInputValue] = useState('');
-	const [inputValueIsMatchClanName, setInputValueIsMatchClanName] = useState(true);
+	const currentClanName = useMemo(() => {
+		return currentClan?.clan_name
+	}, [currentClan?.clan_name])
 
 	const onConfirm = async () => {
-		if (inputValue === currentClan?.clan_name) {
-			const store = await getStoreAsync();
-			navigation.navigate(APP_SCREEN.HOME);
-			dismiss();
-			visibleChange(false);
-			await dispatch(clansActions.deleteClan({ clanId: currentClan?.clan_id || '' }));
-			await remove(STORAGE_CHANNEL_CURRENT_CACHE);
-			store.dispatch(clansActions.joinClan({ clanId: clans?.[0]?.clan_id }));
-			save(STORAGE_CLAN_ID, clans?.[0]?.clan_id);
-			store.dispatch(clansActions.changeCurrentClan({ clanId: clans[0]?.clan_id }));
-			const respChannel = await store.dispatch(channelsActions.fetchChannels({ clanId: clans[0]?.clan_id, noCache: true }));
-			await setDefaultChannelLoader(respChannel.payload, clans[0]?.clan_id);
-		} else {
-			setInputValueIsMatchClanName(false)
-		}
+		const store = await getStoreAsync();
+		navigation.navigate(APP_SCREEN.HOME);
+		dismiss();
+		await dispatch(clansActions.deleteClan({ clanId: currentClan?.clan_id || '' }));
+		await remove(STORAGE_CHANNEL_CURRENT_CACHE);
+		store.dispatch(clansActions.joinClan({ clanId: clans?.[0]?.clan_id }));
+		save(STORAGE_CLAN_ID, clans?.[0]?.clan_id);
+		store.dispatch(clansActions.changeCurrentClan({ clanId: clans[0]?.clan_id }));
+		const respChannel = await store.dispatch(channelsActions.fetchChannels({ clanId: clans[0]?.clan_id, noCache: true }));
+		await setDefaultChannelLoader(respChannel.payload, clans[0]?.clan_id);
 	};
-
-	const handleClanNameChange =(text: string) => {
-		setInputValue(text)
-	}
-
-	const handleCancel = useCallback(() => {
-		visibleChange(false);
-	}, [visibleChange]);
 
 	return (
 		<MezonConfirm
 			visible={isVisibleModal}
-			onCancel={handleCancel}
+			onVisibleChange={visibleChange}
 			confirmText={t('deleteClanModal.confirm')}
 			title={t('deleteClanModal.title')}
 			children={(
-				<>
-					<Text style={styles.contentText}>
-						{t('deleteClanModal.description', { currentClan: currentClan?.clan_name })}
-					</Text>
-					<Text style={styles.label}>
-						{t('deleteClanModal.inputClanName')}
-					</Text>
-					<TextInput 
-						style={[styles.input, {backgroundColor: themeValue.bgInputPrimary}]}
-						value={inputValue}
-						onChangeText={handleClanNameChange}
-						autoFocus
-					/>
-					{!inputValueIsMatchClanName && 
-						<Text style={styles.inputError}>{t('deleteClanModal.inputError')}</Text>
+				<Text style={styles.contentText}>
+					{t('deleteClanModal.description', { currentClan: currentClanName })
+						.split(currentClanName)
+						.reduce((acc, part, index) => {
+							if (index === 0) {
+								return [part];
+							}
+							return [...acc, <Text key={index} style={{ fontWeight: 'bold' }}>{currentClanName}</Text>, part];
+						}, [])
 					}
-				</>
+				</Text>
 			)}
 			onConfirm={onConfirm}
 		/>
