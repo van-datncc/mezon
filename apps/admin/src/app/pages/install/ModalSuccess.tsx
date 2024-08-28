f9e920f5e43f518c2c82de2136897e40fc7ec32a
thuy1901 <thuy.nguyenthithu1@ncc.asia>
2024-08-28 11:41:44 +0700

    light mode popup add bot

--- apps/admin/src/app/pages/install/ModalSuccess.tsx
+++ apps/admin/src/app/pages/install/ModalSuccess.tsx
@@ -1,5 +1,8 @@
 import { useAppNavigation } from '@mezon/core';
+import { selectTheme } from '@mezon/store';
 import { Icons } from '@mezon/ui';
+import { ThemeApp } from '@mezon/utils';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { TypeSelectClan } from './ModalAddBot';
 
@@ -16,11 +19,14 @@ const ModalSuccess = (props: ModalSuccessProps) => {
 	const handleNavigate = () => {
 		navigate(toClanPage(clan?.clanId || ''));
 	};
+
+	const appearanceTheme = useSelector(selectTheme);
+
 	return (
-		<div className="rounded bg-bgProfileBody max-w-[440px] w-full pt-4 flex flex-col items-center p-6 gap-y-5">
-			<Icons.PicSuccessModal />
-			<p className="text-base text-colorWhiteSecond">Success!</p>
-			<p className="text-sm text-colorWhiteSecond">
+		<div className="rounded dark:bg-bgProfileBody bg-bgLightMode max-w-[440px] w-full pt-4 flex flex-col items-center p-6 gap-y-5">
+			<Icons.PicSuccessModal defaultFill={appearanceTheme === ThemeApp.Light ? '#fff' : '#000'} />
+			<p className="text-base dark:text-colorWhiteSecond text-colorTextLightMode">Success!</p>
+			<p className="text-sm dark:text-colorWhiteSecond text-colorTextLightMode">
 				<strong>{name}</strong>
 				&nbsp;has been authorised and added
 				{isModalTry ? (
@@ -32,11 +38,11 @@ const ModalSuccess = (props: ModalSuccessProps) => {
 				)}
 			</p>
 			{!isModalTry && (
-				<button className="px-4 py-2 rounded bg-primary w-fit text-sm font-medium" onClick={handleNavigate}>
+				<button className="px-4 py-2 rounded bg-primary w-fit text-sm font-medium text-white" onClick={handleNavigate}>
 					Go to <strong>{clan?.clanName}</strong>
 				</button>
 			)}
-			<p className="text-xs text-contentTertiary">You may now close this window or tab.</p>
+			<p className="text-xs dark:text-contentTertiary text-black">You may now close this window or tab.</p>
 		</div>
 	);
 };
