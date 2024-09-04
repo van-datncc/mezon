e8e94739ea4e1fed75541e34469beed9a9e1369b
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2024-09-04 17:27:22 +0700

    updated - 1

--- libs/core/src/lib/chat/contexts/ChatContext.tsx
+++ libs/core/src/lib/chat/contexts/ChatContext.tsx
@@ -114,6 +114,7 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 
 	const onchannelmessage = useCallback(
 		async (message: ChannelMessage) => {
+			console.log('message: ', message);
 			const senderId = message.sender_id;
 			const timestamp = Date.now() / 1000;
 			const mess = mapMessageChannelToEntity(message);
