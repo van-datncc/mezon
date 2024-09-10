569bcd97a9afff75e5b921c59e52b9590b2f1e8d
devGangz <phongnn@devcamp.edu.vn>
2024-09-10 20:02:00 +0700

    Merge branch 'develop' of github.com:nccasia/mezon-fe into mention-list-thread

--- libs/core/src/lib/chat/hooks/useChannelMembers.ts
+++ libs/core/src/lib/chat/hooks/useChannelMembers.ts
@@ -1,12 +1,15 @@
-import { selectAllChannelMembers, selectMentionUsers, useAppSelector } from '@mezon/store';
+import { selectAllChannelMembers, selectChannelById, useAppSelector } from '@mezon/store';
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 
 export type useChannelMembersOptions = {
 	channelId?: string | null;
 };
 
 export function useChannelMembers({ channelId }: useChannelMembersOptions = {}) {
-	const membersOfParent = useAppSelector((state) => selectMentionUsers(state, channelId as string));
+	const getChannelByChannelId = useSelector(selectChannelById(channelId ?? ''));
+
+	const membersOfParent = useAppSelector((state) => selectAllChannelMembers(state, getChannelByChannelId.parrent_id as string));
 	const membersOfChild = useAppSelector((state) => selectAllChannelMembers(state, channelId as string));
 
 	return useMemo(
