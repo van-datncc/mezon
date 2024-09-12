af0770ce1b64c3ec94ca9cd48009cf4e565c138c
Long Thanh <longma350@gmail.com>
2024-09-12 17:57:21 +0700

    update system mesage

--- libs/components/src/lib/components/ClanSettings/ClanSettingOverview/index.tsx
+++ libs/components/src/lib/components/ClanSettings/ClanSettingOverview/index.tsx
@@ -1,12 +1,14 @@
 import { useClans } from '@mezon/core';
-import { selectCurrentClan } from '@mezon/store';
+import { createSystemMessage, fetchSystemMessageByClanId, selectCurrentClan, updateSystemMessage, useAppDispatch } from '@mezon/store';
+import { unwrapResult } from '@reduxjs/toolkit';
 import { ApiUpdateClanDescRequest } from 'mezon-js';
-import { useState } from 'react';
+import { ApiSystemMessage, ApiSystemMessageRequest, MezonUpdateSystemMessageBody } from 'mezon-js/api.gen';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import ClanBannerBackground from './ClanBannerBackground';
 import ClanLogoName from './ClanLogoName';
 import ModalSaveChanges from './ModalSaveChanges';
-import SystemMessagesManagement from "./SystemMessagesManagement";
+import SystemMessagesManagement from './SystemMessagesManagement';
 
 const ClanSettingOverview = () => {
 	const { updateClan } = useClans();
@@ -17,8 +19,31 @@ const ClanSettingOverview = () => {
 		clan_id: currentClan?.clan_id ?? '',
 		clan_name: currentClan?.clan_name ?? '',
 		creator_id: currentClan?.creator_id ?? '',
-		logo: currentClan?.logo ?? '',
+		logo: currentClan?.logo ?? ''
 	});
+
+	const [systemMessage, setSystemMessage] = useState<ApiSystemMessage | null>(null);
+	const [createSystemMessageRequest, setCreateSystemMessageRequest] = useState<ApiSystemMessageRequest | null>(null);
+	const [updateSystemMessageRequest, setUpdateSystemMessageRequest] = useState<MezonUpdateSystemMessageBody>({
+		channel_id: systemMessage?.channel_id ?? '',
+		welcome_random: systemMessage?.welcome_random ?? '',
+		welcome_sticker: systemMessage?.welcome_sticker ?? '',
+		boost_message: systemMessage?.boost_message ?? '',
+		setup_tips: systemMessage?.setup_tips ?? ''
+	});
+
+	const dispatch = useAppDispatch();
+
+	const fetchSystemMessage = async () => {
+		if (!currentClan?.clan_id) return;
+		const resultAction = await dispatch(fetchSystemMessageByClanId(currentClan?.clan_id));
+		const message = unwrapResult(resultAction);
+		setSystemMessage(message);
+	};
+
+	useEffect(() => {
+		fetchSystemMessage();
+	}, [currentClan]);
 
 	const handleUploadBackground = (urlImage: string) => {
 		setClanRequest({ ...clanRequest, banner: urlImage });
@@ -32,8 +57,45 @@ const ClanSettingOverview = () => {
 		setClanRequest({ ...clanRequest, clan_name: clanName ?? '' });
 	};
 
+	const handleCreateSystemMessageRequest = (createSystemMessageRequest: ApiSystemMessageRequest) => {
+		setCreateSystemMessageRequest(createSystemMessageRequest);
+	};
+
+	const handleChangeChannelId = (channelId: string) => {
+		setUpdateSystemMessageRequest({ ...updateSystemMessageRequest, channel_id: channelId ?? '' });
+	};
+
+	const handleChangeWelcomeRandom = (welcomeRandom: string) => {
+		setUpdateSystemMessageRequest({ ...updateSystemMessageRequest, welcome_random: welcomeRandom ?? '' });
+	};
+
+	const handleChangeWelcomeSticker = (welcomeSticker: string) => {
+		setUpdateSystemMessageRequest({ ...updateSystemMessageRequest, welcome_sticker: welcomeSticker ?? '' });
+	};
+
+	const handleChangeBoostMessage = (boostMessage: string) => {
+		setUpdateSystemMessageRequest({ ...updateSystemMessageRequest, boost_message: boostMessage ?? '' });
+	};
+
+	const handleChangeSetupTips = (setupTips: string) => {
+		setUpdateSystemMessageRequest({ ...updateSystemMessageRequest, setup_tips: setupTips ?? '' });
+	};
+
 	const handleSave = async () => {
 		await updateClan(clanRequest);
+		await updateSystemMessages();
+	};
+
+	const updateSystemMessages = async () => {
+		if (systemMessage && Object.keys(systemMessage).length > 0 && currentClan?.clan_id && updateSystemMessageRequest) {
+			const request = {
+				clanId: currentClan.clan_id,
+				newMessage: updateSystemMessageRequest
+			};
+			await dispatch(updateSystemMessage(request));
+		} else if (createSystemMessageRequest) {
+			await dispatch(createSystemMessage(createSystemMessageRequest));
+		}
 	};
 
 	const handleReset = () => {
@@ -53,7 +115,20 @@ const ClanSettingOverview = () => {
 				onUpload={handleUploadBackground}
 				onHasChanges={(hasChanges) => setHasChanges(hasChanges)}
 			/>
-			<SystemMessagesManagement/>
+			{systemMessage && (
+				<SystemMessagesManagement
+					hasChanges={hasChanges}
+					systemMessage={systemMessage}
+					onGetCreateSystemMessageRequest={handleCreateSystemMessageRequest}
+					onGetChannelId={handleChangeChannelId}
+					onGetWelcomeRandom={handleChangeWelcomeRandom}
+					onGetWelcomeSticker={handleChangeWelcomeSticker}
+					onGetBoostMessage={handleChangeBoostMessage}
+					onGetSetupTips={handleChangeSetupTips}
+					onHasChanges={(hasChanges) => setHasChanges(hasChanges)}
+				/>
+			)}
+
 			{hasChanges && <ModalSaveChanges onSave={handleSave} onReset={handleReset} />}
 		</div>
 	);
