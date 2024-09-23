38b0cab5b867ab6e8ed3dc7da6525a94c49cd83f
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-09-23 10:15:54 +0700

    update: channel voice

--- apps/mobile/src/app/screens/home/homedrawer/components/ChannelList/ChannelListUserVoice/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ChannelList/ChannelListUserVoice/index.tsx
@@ -1,22 +1,22 @@
-import { selectCurrentChannelId } from '@mezon/store';
-import { IChannelMember } from '@mezon/utils';
+import { selectVoiceChannelMembersByChannelId } from '@mezon/store';
 import React, { memo } from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import UserVoiceItem from '../ChannelListUserVoiceItem';
 
 interface IUserListVoiceChannelProps {
-	userListVoice: IChannelMember[];
+	channelId: string;
 }
 
-export default memo(function ChannelListUserVoice({ userListVoice }: IUserListVoiceChannelProps) {
-	const currentChannelId = useSelector(selectCurrentChannelId);
+export default memo(function ChannelListUserVoice({ channelId }: IUserListVoiceChannelProps) {
+	const voiceChannelMember = useSelector(selectVoiceChannelMembersByChannelId(channelId));
+	console.log('log  => voiceChannelMember', voiceChannelMember);
 
 	return (
 		<View>
-			{userListVoice?.length
-				? userListVoice?.map((userVoice, index) => (
-						<UserVoiceItem key={`${index}_voice_item_${userVoice?.participant}`} userVoice={userVoice} channelID={currentChannelId} />
+			{voiceChannelMember?.length
+				? voiceChannelMember?.map((userVoice, index) => (
+						<UserVoiceItem key={`${index}_voice_item_${userVoice?.participant}`} userVoice={userVoice} />
 					))
 				: null}
 		</View>
