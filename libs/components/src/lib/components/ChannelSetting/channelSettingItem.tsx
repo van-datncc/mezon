bb9c5c009892f176bdecb3c399f06b3e24544ff2
hmquannnnn <minhquanhoang040103@gmail.com>
2024-09-24 14:44:05 +0700

    fix: channel permission

--- libs/components/src/lib/components/ChannelSetting/channelSettingItem.tsx
+++ libs/components/src/lib/components/ChannelSetting/channelSettingItem.tsx
@@ -21,7 +21,7 @@ const ChannelSettingItem = (props: ChannelSettingItemProps) => {
 	const [showModal, setShowModal] = useState(false);
 	const [hasAdminPermission, { isClanOwner }] = useClanRestriction([EPermission.administrator]);
 	const [hasManageClanPermission] = useClanRestriction([EPermission.manageClan]);
-	const [hasManageChannelPermission] = useClanRestriction([EPermission.manageClan]);
+	const [hasManageChannelPermission] = useClanRestriction([EPermission.manageChannel]);
 	const canEditChannelPermissions = isClanOwner || hasAdminPermission || hasManageClanPermission || hasManageChannelPermission;
 	const isThread = useMemo(() => {
 		return channel.parrent_id !== '0';
