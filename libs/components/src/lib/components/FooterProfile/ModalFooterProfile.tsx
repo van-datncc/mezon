dac8e2b935a2fc9549a3ea7932403ee6fc88d804
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-09-25 14:47:33 +0700

    fix modal layout z-index

--- libs/components/src/lib/components/FooterProfile/ModalFooterProfile.tsx
+++ libs/components/src/lib/components/FooterProfile/ModalFooterProfile.tsx
@@ -12,7 +12,7 @@ const ModalFooterProfile = ({ userId, avatar, name, isDM, userStatusProfile }: M
 	return (
 		<div
 			onClick={(e) => e.stopPropagation()}
-			className={`fixed sbm:left-[50px] left-5 bottom-[70px] dark:bg-black bg-gray-200 mt-[10px] w-[340px] max-w-[89vw] rounded-lg flex flex-col z-10 opacity-100 shadow-md shadow-bgTertiary-500/40`}
+			className={`fixed sbm:left-[50px] left-5 bottom-[70px] dark:bg-black bg-gray-200 mt-[10px] w-[340px] max-w-[89vw] rounded-lg flex flex-col z-20 opacity-100 shadow-md shadow-bgTertiary-500/40`}
 		>
 			<ModalUserProfile userID={userId} isFooterProfile avatar={avatar} name={name} isDM={isDM} userStatusProfile={userStatusProfile} />
 		</div>
