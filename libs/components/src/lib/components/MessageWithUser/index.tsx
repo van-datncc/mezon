157dc8a6d08a5017342740c45b2690da97594007
hmquannnnn <minhquanhoang040103@gmail.com>
2024-09-26 17:36:10 +0700

    fix: not show system short user

--- libs/components/src/lib/components/MessageWithUser/index.tsx
+++ libs/components/src/lib/components/MessageWithUser/index.tsx
@@ -212,7 +212,7 @@ function MessageWithUser({
 					</div>
 				</HoverStateWrapper>
 			)}
-			{isShowPanelChannel && allowDisplayShortProfile && (
+			{isShowPanelChannel && senderId !== '0' && allowDisplayShortProfile && (
 				<div
 					className={`fixed z-50 max-[480px]:!left-16 max-[700px]:!left-9 dark:bg-black bg-gray-200 w-[300px] max-w-[89vw] rounded-lg flex flex-col  duration-300 ease-in-out`}
 					style={{
