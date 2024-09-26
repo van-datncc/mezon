2779ef9a4c7e4769e49940746dc7cfe4d756ee9f
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-09-26 16:24:55 +0700

    update

--- apps/mobile/src/app/components/Event/EventItem/index.tsx
+++ apps/mobile/src/app/components/Event/EventItem/index.tsx
@@ -26,7 +26,8 @@ export default function EventItem({ event, onPress, showActions = true, start }:
 	const eventStatus = useMemo(() => {
 		if (event?.status) {
 			return event.status;
-		} else if (start) {
+		}
+		if (start) {
 			const currentTime = Date.now();
 			const startTimeLocal = new Date(start);
 			const startTimeUTC = startTimeLocal.getTime() + startTimeLocal.getTimezoneOffset() * 60000;
--- libs/components/src/lib/components/ChannelList/EventChannelModal/ModalCreate/itemEventManagement.tsx
+++ libs/components/src/lib/components/ChannelList/EventChannelModal/ModalCreate/itemEventManagement.tsx
@@ -69,9 +69,7 @@ const ItemEventManagement = (props: ItemEventManagementProps) => {
 	const eventStatus = useMemo(() => {
 		if (event?.status) {
 			return event.status;
-		}
-
-		if (start) {
+		} else if (start) {
 			const currentTime = Date.now();
 			const startTimeLocal = new Date(start);
 			const startTimeUTC = startTimeLocal.getTime() + startTimeLocal.getTimezoneOffset() * 60000;
