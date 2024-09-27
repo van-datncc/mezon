51d900bb879a0dcabab22e4b570aa138439b1af4
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-09-27 13:33:27 +0700

    Mobile app: Notifications > UI Error in light mode and jump doesn't reach the correct position

--- apps/mobile/src/app/screens/Notifications/NotificationIndividualItem/NotificationIndividualItem.styles.ts
+++ apps/mobile/src/app/screens/Notifications/NotificationIndividualItem/NotificationIndividualItem.styles.ts
@@ -1,56 +1,40 @@
-import { Colors, size } from '@mezon/mobile-ui';
+import { Attributes, size } from '@mezon/mobile-ui';
 import { StyleSheet } from 'react-native';
-export const styles = StyleSheet.create({
-  notifyContainer: {
-    paddingHorizontal: size.s_10,
-    marginBottom: size.s_10,
-  },
-  notifyHeader: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-    gap: 10,
-  },
-  notifyContent: {
-    flex: 1,
-    marginLeft: size.s_6,
-  },
-  notifyHeaderTitle: {
-    color: Colors.textGray,
-    fontSize: size.medium,
-    fontWeight: '400',
-    marginBottom: 5,
-  },
-  notifyDuration: {
-    color: Colors.textGray,
-  },
-  boxImage: {
-    width: size.s_40,
-    height: size.s_40,
-    borderRadius: 50,
-    marginBottom: 10,
-    fontSize: size.medium,
-  },
-  image: {
-    width: '90%',
-    height: '90%',
-    borderRadius: 50,
-  },
-  boxImageChar: {
-    width: size.s_40,
-    height: size.s_40,
-    borderRadius: size.s_40,
-    backgroundColor: Colors.bgDarkCharcoal,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
-  },
-  contentMessage: {
-    marginBottom: 10,
-    borderLeftColor: Colors.borderNeutralDisable,
-    borderLeftWidth: 2,
-    paddingLeft: 8,
-    fontSize: size.medium,
-  },
-});
+export const style = (colors: Attributes) =>
+	StyleSheet.create({
+		notifyContainer: {
+			paddingHorizontal: size.s_10,
+			marginBottom: size.s_10
+		},
+		notifyHeader: {
+			flexDirection: 'row',
+			alignItems: 'flex-start',
+			justifyContent: 'space-between',
+			gap: 10
+		},
+		notifyContent: {
+			flex: 1,
+			marginLeft: size.s_6
+		},
+		notifyHeaderTitle: {
+			color: colors.textStrong,
+			fontSize: size.medium,
+			fontWeight: '400',
+			marginBottom: 5
+		},
+		notifyDuration: {
+			color: colors?.textStrong
+		},
+		boxImage: {
+			width: size.s_40,
+			height: size.s_40,
+			borderRadius: 50,
+			marginBottom: 10,
+			fontSize: size.medium
+		},
+		image: {
+			width: '90%',
+			height: '90%',
+			borderRadius: 50
+		}
+	});
