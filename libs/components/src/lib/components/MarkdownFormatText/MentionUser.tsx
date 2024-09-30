287462599a87d2c918c72fcf3a72b002875cf48d
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-09-30 14:52:04 +0700

    add animation flyIn

--- libs/components/src/lib/components/MarkdownFormatText/MentionUser.tsx
+++ libs/components/src/lib/components/MarkdownFormatText/MentionUser.tsx
@@ -160,7 +160,7 @@ const UserProfilePopup = ({ userID, channelId, mode, isDm, positionShortUser, on
 	return (
 		<div
 			tabIndex={-1}
-			className={`fixed z-50 max-[480px]:!left-16 max-[700px]:!left-9 dark:bg-black bg-gray-200 w-[300px] max-w-[89vw] rounded-lg flex flex-col  duration-300 ease-in-out`}
+			className={`fixed z-50 max-[480px]:!left-16 max-[700px]:!left-9 dark:bg-black bg-gray-200 w-[300px] max-w-[89vw] rounded-lg flex flex-col  duration-300 ease-in-out animate-flyIn`}
 			style={{
 				top: `${positionShortUser?.top}px`,
 				left: `${positionShortUser?.left}px`
