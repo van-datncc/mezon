3f63756f73dc325e95cafac438b5c4d8216a3f42
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-10-05 19:30:42 +0700

    update: import

--- apps/mobile/src/app/screens/settings/index.tsx
+++ apps/mobile/src/app/screens/settings/index.tsx
@@ -12,9 +12,8 @@ import { authActions, channelsActions, clansActions, getStoreAsync, messagesActi
 import React, { useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Alert, ScrollView, View } from 'react-native';
+import { IMezonMenuItemProps, IMezonMenuSectionProps, MezonMenu, MezonSearch, reserve } from '../../componentUI';
 import { APP_SCREEN } from '../../navigation/ScreenTypes';
-import { IMezonMenuItemProps, IMezonMenuSectionProps, MezonMenu, reserve } from '../../componentUI';
-import MezonSearch from '../../componentUI/MezonSearch';
 import { style } from './styles';
 
 export const Settings = ({ navigation }: { navigation: any }) => {
