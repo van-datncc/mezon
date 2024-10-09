c80a43a24c74d38ecc0f929c96ac3445ea8445c5
anh.trantruongNCC~ <sigaldphp@gmail.com>
2024-10-09 11:39:11 +0700

    fix sort pin message

--- libs/components/src/lib/components/ChannelTopbar/TopBarComponents/PinnedMessages/ListPinMessage.tsx
+++ libs/components/src/lib/components/ChannelTopbar/TopBarComponents/PinnedMessages/ListPinMessage.tsx
@@ -38,31 +38,28 @@ const ListPinMessage = ({
 				<EmptyPinMess />
 			) : (
 				<div className="flex flex-col items-center justify-center space-y-2 py-2">
-					{listPinMessages
-						.slice()
-						.reverse()
-						.map((pinMessage) => {
-							// Parse content if it's a JSON string
-							let contentString = pinMessage.content;
-							if (typeof contentString === 'string') {
-								try {
-									const contentObject = JSON.parse(contentString);
-									contentString = contentObject.t;
-								} catch (e) {
-									console.error('Failed to parse content JSON:', e);
-								}
+					{listPinMessages.slice().map((pinMessage) => {
+						// Parse content if it's a JSON string
+						let contentString = pinMessage.content;
+						if (typeof contentString === 'string') {
+							try {
+								const contentObject = JSON.parse(contentString);
+								contentString = contentObject.t;
+							} catch (e) {
+								console.error('Failed to parse content JSON:', e);
 							}
+						}
 
-							return (
-								<ItemPinMessage
-									pinMessage={pinMessage}
-									contentString={contentString}
-									handleUnPinMessage={handleUnPinMessage}
-									key={pinMessage.id}
-									onClose={onClose}
-								/>
-							);
-						})}
+						return (
+							<ItemPinMessage
+								pinMessage={pinMessage}
+								contentString={contentString}
+								handleUnPinMessage={handleUnPinMessage}
+								key={pinMessage.id}
+								onClose={onClose}
+							/>
+						);
+					})}
 				</div>
 			)}
 		</div>
