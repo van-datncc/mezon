760e7f6b1ab1cb618e9c655b6df1642a75970695
Hiephanu <doanvanhiep230903@gmail.com>
2024-10-15 14:27:28 +0700

    update event fix

--- libs/components/src/lib/components/ChannelList/EventChannelModal/timeFomatEvent.ts
+++ libs/components/src/lib/components/ChannelList/EventChannelModal/timeFomatEvent.ts
@@ -116,3 +116,15 @@ export const getTimeFomatDay = () => {
 	const minutes = date.getMinutes().toString().padStart(2, '0');
 	return `${hours}:${minutes}`;
 };
+
+export const formatTimeStringToHourFormat = (timeString: string) => {
+	const date = new Date(timeString);
+	const hours = date.getUTCHours().toString().padStart(2, '0');
+	const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+
+	return `${hours}:${minutes}`;
+};
+
+export const formatToLocalDateString = (timeString: string) => {
+	return timeString.slice(0, -1);
+};
