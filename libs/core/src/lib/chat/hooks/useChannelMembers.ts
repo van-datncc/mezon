ec07f8493eee89aa3d98bd2d76ee89de1e0f6ca0
devGangz <phongnn@devcamp.edu.vn>
2024-10-19 20:46:10 +0700

    saved

--- libs/core/src/lib/chat/hooks/useChannelMembers.ts
+++ libs/core/src/lib/chat/hooks/useChannelMembers.ts
@@ -3,13 +3,11 @@ import {
 	ChannelsEntity,
 	channelUsersActions,
 	selectAllChannelMembers,
-	selectAllRolesClan,
 	selectChannelById,
 	ThreadsEntity,
 	useAppDispatch,
 	useAppSelector
 } from '@mezon/store';
-import { IMentionOnMessage, uniqueUsers } from '@mezon/utils';
 import { ChannelStreamMode } from 'mezon-js';
 import { useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
@@ -23,7 +21,6 @@ export function useChannelMembers({ channelId, mode }: useChannelMembersOptions)
 	const channel = useSelector(selectChannelById(channelId ?? ''));
 	const membersOfChild = useAppSelector((state) => (channelId ? selectAllChannelMembers(state, channelId as string) : null));
 	const membersOfParent = useAppSelector((state) => (channel?.parrent_id ? selectAllChannelMembers(state, channel.parrent_id as string) : null));
-	const rolesClan = useSelector(selectAllRolesClan);
 
 	const dispatch = useAppDispatch();
 
@@ -52,20 +49,14 @@ export function useChannelMembers({ channelId, mode }: useChannelMembersOptions)
 	};
 
 	const addMemberToThread = useCallback(
-		async (currentChannel: ChannelsEntity | null, mentions: IMentionOnMessage[]) => {
-			if (currentChannel?.parrent_id === '0' || currentChannel?.parrent_id === '') return;
-			const userIds = uniqueUsers(mentions, membersOfChild, rolesClan);
-			const existingUserIds = userIds.filter((userId) => membersOfParent?.some((member) => member.id === userId));
-			if (existingUserIds.length > 0) {
-				await updateChannelUsers(currentChannel, existingUserIds, currentChannel?.clan_id as string);
-			}
+		async (currentChannel: ChannelsEntity | null, notExistingUserIds: string[]) => {
+			await updateChannelUsers(currentChannel, notExistingUserIds, currentChannel?.clan_id as string);
 		},
 		[dispatch, membersOfChild]
 	);
 
 	const joinningToThread = useCallback(
 		async (targetThread: ThreadsEntity | null, user: string[]) => {
-			if (targetThread?.parrent_id === '0' || targetThread?.parrent_id === '') return;
 			await updateChannelUsers(targetThread as ChannelsEntity, user, targetThread?.clan_id as string);
 		},
 		[dispatch]
