ef434b86b402541258157d7066b3b2e37edcfe1c
thuyngoc <ngoc.maithuy@ncc.asia>
2024-10-23 11:14:14 +0700

    fix/ui-home-page: change image and add link for social page of mezon

--- apps/chat/src/app/pages/homepage/mezonpage/sidebar/index.tsx
+++ apps/chat/src/app/pages/homepage/mezonpage/sidebar/index.tsx
@@ -105,13 +105,13 @@ export const SideBarMezon = memo((props: SideBarProps) => {
 			<div ref={footerSideBarRef} className="container w-full py-[48px] absolute bottom-0">
 				<div className="flex flex-col gap-[24px] max-lg:px-[32px]">
 					<div className="flex items-center gap-[24px]">
-						<a href="#link" className="cursor-pointer">
+						<a href="https://www.linkedin.com/company/nccplus-vietnam" target="_blank" rel="noreferrer" className="cursor-pointer">
 							<Image src={`assets/instagram.svg`} alt={'linkedIn'} />
 						</a>
-						<a href="#facebook" className="cursor-pointer">
+						<a href="https://www.facebook.com/profile.php?id=61558081847939" target="_blank" rel="noreferrer" className="cursor-pointer">
 							<Image src={`assets/facebook.svg`} alt={'facebook'} />
 						</a>
-						<a href="#github" className="cursor-pointer">
+						<a href="https://github.com/nccasia/mezon-fe" target="_blank" rel="noreferrer" className="cursor-pointer">
 							<Image src={`assets/twitter.svg`} alt={'twitter'} />
 						</a>
 					</div>
