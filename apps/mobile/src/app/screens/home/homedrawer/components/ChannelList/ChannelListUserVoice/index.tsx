e92db2db59d4221e4a395e3b3091b2562fb5f8d7
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-10-28 19:15:09 +0700

    update user list channel stream

--- apps/mobile/src/app/screens/home/homedrawer/components/ChannelList/ChannelListUserVoice/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ChannelList/ChannelListUserVoice/index.tsx
@@ -1,5 +1,5 @@
 import { size } from '@mezon/mobile-ui';
-import { selectVoiceChannelMembersByChannelId } from '@mezon/store-mobile';
+import { selectStreamMembersByChannelId, selectVoiceChannelMembersByChannelId } from '@mezon/store-mobile';
 import React, { memo } from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
@@ -12,6 +12,7 @@ interface IUserListVoiceChannelProps {
 
 export default memo(function ChannelListUserVoice({ channelId, isCategoryExpanded }: IUserListVoiceChannelProps) {
 	const voiceChannelMember = useSelector(selectVoiceChannelMembersByChannelId(channelId));
+	const streamChannelMembers = useSelector(selectStreamMembersByChannelId(channelId));
 
 	return (
 		<View style={[!isCategoryExpanded && { flexDirection: 'row', marginLeft: size.s_30 }]}>
@@ -23,6 +24,17 @@ export default memo(function ChannelListUserVoice({ channelId, isCategoryExpande
 							userVoice={userVoice}
 							isCategoryExpanded={isCategoryExpanded}
 							totalMembers={voiceChannelMember?.length}
+						/>
+					))
+				: null}
+			{streamChannelMembers?.length
+				? streamChannelMembers?.map((userStream, index) => (
+						<UserVoiceItem
+							key={`${index}_stream_item_${userStream?.participant}`}
+							index={index}
+							userVoice={userStream}
+							isCategoryExpanded={isCategoryExpanded}
+							totalMembers={streamChannelMembers?.length}
 						/>
 					))
 				: null}
