e66c0dd26b13a19ad067de2fded7ed0f67379ba7
hoang.tranlehuy <hoang.tranlehuy@ncc.asia>
2024-10-29 15:43:05 +0700

    update login and register ui tablet

--- apps/mobile/src/app/screens/auth/Login/index.tsx
+++ apps/mobile/src/app/screens/auth/Login/index.tsx
@@ -1,21 +1,24 @@
 import { appleAuth } from '@invertase/react-native-apple-authentication';
 import { useAuth } from '@mezon/core';
-import { Block, Colors, size, useTheme } from '@mezon/mobile-ui';
+import { IS_TABLET } from '@mezon/mobile-components';
+import { baseColor, Block, Colors, size, useTheme } from '@mezon/mobile-ui';
 import { RootState } from '@mezon/store-mobile';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { useNavigation } from '@react-navigation/native';
 import { Formik } from 'formik';
 import React, { useEffect } from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import { useSelector } from 'react-redux';
 import * as Yup from 'yup';
-import LoadingModal from '../../../components/LoadingModal/LoadingModal';
 import Button from '../../../components/auth/Button';
 import { FooterAuth } from '../../../components/auth/FooterAuth';
 import { LoginSocial } from '../../../components/auth/LoginSocial';
 import { TextInputUser } from '../../../components/auth/TextInput';
+import LoadingModal from '../../../components/LoadingModal/LoadingModal';
+import useTabletLandscape from '../../../hooks/useTabletLandscape';
 import { APP_SCREEN } from '../../../navigation/ScreenTypes';
 import { style } from './styles';
 const LoginSchema = Yup.object().shape({
@@ -31,7 +34,8 @@ const WEB_CLIENT_ID = '285548761692-l9bdt00br2jg1fgh4c23dlb9rvkvqqs0.apps.google
 const IOS_CLIENT_ID = '285548761692-3k9ubkdhl8bbvbal78j9v2905kjhg3tj.apps.googleusercontent.com';
 
 const LoginScreen = () => {
-	const styles = style(useTheme().themeValue);
+	const isTabletLandscape = useTabletLandscape();
+	const styles = style(useTheme().themeValue, isTabletLandscape);
 	const navigation = useNavigation();
 	const isLoading = useSelector((state: RootState) => state.auth.loadingStatus);
 	const { loginByGoogle, loginByApple, loginEmail } = useAuth();
@@ -107,65 +111,78 @@ const LoginScreen = () => {
 
 	return (
 		<SafeAreaView style={styles.supperContainer}>
-			<KeyboardAvoidingView style={styles.container}>
-				{/* header */}
-				<View style={styles.headerContainer}>
-					<Text style={styles.headerTitle}>WELCOME BACK</Text>
-					<Text style={styles.headerContent}>So glad to meet you again!</Text>
-				</View>
-				{/* body */}
-				<View style={styles.googleButton}>
-					<LoginSocial onGoogleButtonPress={onGoogleButtonPress} onAppleButtonPress={onAppleButtonPress} />
-					<Block marginTop={size.s_30} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'} alignSelf={'center'}>
-						<Block width={'35%'} height={1} backgroundColor={Colors.gray48} />
-						<Text style={styles.orText}>Or</Text>
-						<Block width={'35%'} height={1} backgroundColor={Colors.gray48} />
-					</Block>
-				</View>
-				<ScrollView style={{ flex: 1 }}>
-					<Formik
-						initialValues={{
-							email: '',
-							password: ''
-						}}
-						validationSchema={LoginSchema}
-						onSubmit={handleSubmit}
-					>
-						{({ errors, touched, values, handleSubmit, handleChange, setFieldTouched, isValid }) => (
-							<>
-								{/* email */}
-								<TextInputUser
-									label="Email or phone"
-									value={values.email}
-									onChangeText={handleChange('email')}
-									placeholder="Email or phone"
-									onBlur={() => setFieldTouched('email')}
-									touched={touched.email}
-									error={errors.email}
-									isPass={false}
-								/>
+			<LinearGradient
+				start={{ x: 0, y: 1 }}
+				end={{ x: 1, y: 0 }}
+				colors={[baseColor.white, Colors.bgViolet, Colors.textLink]}
+				style={styles.gradient}
+			>
+				<KeyboardAvoidingView style={styles.container}>
+					{/* header */}
+					<View style={styles.headerContainer}>
+						<Text style={styles.headerTitle}>WELCOME BACK</Text>
+						<Text style={styles.headerContent}>So glad to meet you again!</Text>
+					</View>
+					{/* body */}
+					<View style={styles.googleButton}>
+						<LoginSocial onGoogleButtonPress={onGoogleButtonPress} onAppleButtonPress={onAppleButtonPress} />
+						<Block
+							marginTop={IS_TABLET ? size.s_20 : size.s_30}
+							flexDirection={'row'}
+							alignItems={'center'}
+							justifyContent={'space-between'}
+							alignSelf={'center'}
+						>
+							<Block width={'35%'} height={1} backgroundColor={Colors.gray48} />
+							<Text style={styles.orText}>Or</Text>
+							<Block width={'35%'} height={1} backgroundColor={Colors.gray48} />
+						</Block>
+					</View>
+					<ScrollView style={{ flex: 1 }}>
+						<Formik
+							initialValues={{
+								email: '',
+								password: ''
+							}}
+							validationSchema={LoginSchema}
+							onSubmit={handleSubmit}
+						>
+							{({ errors, touched, values, handleSubmit, handleChange, setFieldTouched, isValid }) => (
+								<>
+									{/* email */}
+									<TextInputUser
+										label="Email or phone"
+										value={values.email}
+										onChangeText={handleChange('email')}
+										placeholder="Email or phone"
+										onBlur={() => setFieldTouched('email')}
+										touched={touched.email}
+										error={errors.email}
+										isPass={false}
+									/>
 
-								{/* password */}
-								<TextInputUser
-									label="Password"
-									value={values.password}
-									onChangeText={handleChange('password')}
-									placeholder="Password"
-									onBlur={() => setFieldTouched('password')}
-									touched={touched.password}
-									error={errors.password}
-									isPass={true}
-								/>
-								{/* button  */}
-								<Button disabled={!isValid} onPress={handleSubmit} isValid={isValid} title={'Sign in'} />
-							</>
-						)}
-					</Formik>
-				</ScrollView>
+									{/* password */}
+									<TextInputUser
+										label="Password"
+										value={values.password}
+										onChangeText={handleChange('password')}
+										placeholder="Password"
+										onBlur={() => setFieldTouched('password')}
+										touched={touched.password}
+										error={errors.password}
+										isPass={true}
+									/>
+									{/* button  */}
+									<Button disabled={!isValid} onPress={handleSubmit} isValid={isValid} title={'Sign in'} />
+								</>
+							)}
+						</Formik>
+					</ScrollView>
 
-				<FooterAuth content={'Need an account?'} onPress={() => navigation.navigate(APP_SCREEN.REGISTER as never)} title={'Register'} />
-				<LoadingModal isVisible={isLoading === 'loading'} />
-			</KeyboardAvoidingView>
+					<FooterAuth content={'Need an account?'} onPress={() => navigation.navigate(APP_SCREEN.REGISTER as never)} title={'Register'} />
+					<LoadingModal isVisible={isLoading === 'loading'} />
+				</KeyboardAvoidingView>
+			</LinearGradient>
 		</SafeAreaView>
 	);
 };
--- apps/mobile/src/app/screens/auth/Register/index.tsx
+++ apps/mobile/src/app/screens/auth/Register/index.tsx
@@ -1,13 +1,15 @@
-import { Colors, useTheme } from '@mezon/mobile-ui';
+import { baseColor, Colors, useTheme } from '@mezon/mobile-ui';
 import { useNavigation } from '@react-navigation/native';
 import { Formik } from 'formik';
 import React from 'react';
 import { Alert, KeyboardAvoidingView, ScrollView, Text, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as Yup from 'yup';
 import Button from '../../../components/auth/Button';
 import { FooterAuth } from '../../../components/auth/FooterAuth';
 import { TextInputUser } from '../../../components/auth/TextInput';
+import useTabletLandscape from '../../../hooks/useTabletLandscape';
 import { APP_SCREEN } from '../../../navigation/ScreenTypes';
 import { style } from './styles';
 
@@ -26,98 +28,106 @@ const RegisterSchema = Yup.object().shape({
 		.required('Please enter your mobile number')
 });
 const RegisterScreen = () => {
-	const styles = style(useTheme().themeValue);
+	const isTabletLandscape = useTabletLandscape();
+	const styles = style(useTheme().themeValue, isTabletLandscape);
 	const navigation = useNavigation();
 
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: Colors.secondary }}>
-			<KeyboardAvoidingView style={styles.container}>
-				{/* header */}
-				<View style={styles.headerContainer}>
-					<Text style={styles.headerTitle}>Sign up</Text>
-					<Text style={styles.headerContent}>So glad to meet you again!</Text>
-				</View>
-				{/* body */}
-				<ScrollView>
-					<Formik
-						initialValues={{
-							FullName: '',
-							email: '',
-							password: '',
-							confirmPassword: '',
-							mobile: ''
-						}}
-						validationSchema={RegisterSchema}
-						onSubmit={(values) => {
-							Alert.alert(JSON.stringify(values));
-						}}
-					>
-						{({ errors, touched, values, handleSubmit, handleChange, setFieldTouched, isValid }) => (
-							<>
-								{/*Full Name */}
-								<TextInputUser
-									label="Full name"
-									value={values.FullName}
-									onChangeText={handleChange('FullName')}
-									placeholder="Full name"
-									onBlur={() => setFieldTouched('FullName')}
-									touched={touched.FullName}
-									error={errors.FullName}
-									isPass={false}
-								/>
-								{/* email */}
-								<TextInputUser
-									label="Email or phone"
-									value={values.email}
-									onChangeText={handleChange('email')}
-									placeholder="Email or phone"
-									onBlur={() => setFieldTouched('email')}
-									touched={touched.email}
-									error={errors.email}
-									isPass={false}
-								/>
+			<LinearGradient
+				start={{ x: 0, y: 1 }}
+				end={{ x: 1, y: 0 }}
+				colors={[baseColor.white, Colors.bgViolet, Colors.textLink]}
+				style={styles.gradient}
+			>
+				<KeyboardAvoidingView style={styles.container}>
+					{/* header */}
+					<View style={styles.headerContainer}>
+						<Text style={styles.headerTitle}>Sign up</Text>
+						<Text style={styles.headerContent}>So glad to meet you again!</Text>
+					</View>
+					{/* body */}
+					<ScrollView>
+						<Formik
+							initialValues={{
+								FullName: '',
+								email: '',
+								password: '',
+								confirmPassword: '',
+								mobile: ''
+							}}
+							validationSchema={RegisterSchema}
+							onSubmit={(values) => {
+								Alert.alert(JSON.stringify(values));
+							}}
+						>
+							{({ errors, touched, values, handleSubmit, handleChange, setFieldTouched, isValid }) => (
+								<>
+									{/*Full Name */}
+									<TextInputUser
+										label="Full name"
+										value={values.FullName}
+										onChangeText={handleChange('FullName')}
+										placeholder="Full name"
+										onBlur={() => setFieldTouched('FullName')}
+										touched={touched.FullName}
+										error={errors.FullName}
+										isPass={false}
+									/>
+									{/* email */}
+									<TextInputUser
+										label="Email or phone"
+										value={values.email}
+										onChangeText={handleChange('email')}
+										placeholder="Email or phone"
+										onBlur={() => setFieldTouched('email')}
+										touched={touched.email}
+										error={errors.email}
+										isPass={false}
+									/>
 
-								{/* password */}
-								<TextInputUser
-									label="Password"
-									value={values.password}
-									onChangeText={handleChange('password')}
-									placeholder="Password"
-									onBlur={() => setFieldTouched('password')}
-									touched={touched.password}
-									error={errors.password}
-									isPass={true}
-								/>
-								{/* confirm password  */}
-								<TextInputUser
-									label="Confirm password"
-									value={values.confirmPassword}
-									onChangeText={handleChange('confirmPassword')}
-									placeholder="Confirm Password"
-									onBlur={() => setFieldTouched('confirmPassword')}
-									touched={touched.confirmPassword}
-									error={errors.confirmPassword}
-									isPass={true}
-								/>
-								{/* mobile */}
-								<TextInputUser
-									label="Phone"
-									value={values.mobile}
-									onChangeText={handleChange('mobile')}
-									placeholder="Phone"
-									onBlur={() => setFieldTouched('mobile')}
-									touched={touched.mobile}
-									error={errors.mobile}
-									isPass={false}
-								/>
-								{/* button  */}
-								<Button disabled={!isValid} onPress={handleSubmit} isValid={isValid} title={'Sign up'} />
-							</>
-						)}
-					</Formik>
-				</ScrollView>
-				<FooterAuth content={'Have an account!'} onPress={() => navigation.navigate(APP_SCREEN.LOGIN as never)} title={'Login'} />
-			</KeyboardAvoidingView>
+									{/* password */}
+									<TextInputUser
+										label="Password"
+										value={values.password}
+										onChangeText={handleChange('password')}
+										placeholder="Password"
+										onBlur={() => setFieldTouched('password')}
+										touched={touched.password}
+										error={errors.password}
+										isPass={true}
+									/>
+									{/* confirm password  */}
+									<TextInputUser
+										label="Confirm password"
+										value={values.confirmPassword}
+										onChangeText={handleChange('confirmPassword')}
+										placeholder="Confirm Password"
+										onBlur={() => setFieldTouched('confirmPassword')}
+										touched={touched.confirmPassword}
+										error={errors.confirmPassword}
+										isPass={true}
+									/>
+									{/* mobile */}
+									<TextInputUser
+										label="Phone"
+										value={values.mobile}
+										onChangeText={handleChange('mobile')}
+										placeholder="Phone"
+										onBlur={() => setFieldTouched('mobile')}
+										touched={touched.mobile}
+										error={errors.mobile}
+										isPass={false}
+									/>
+									{/* button  */}
+									<Button disabled={!isValid} onPress={handleSubmit} isValid={isValid} title={'Sign up'} />
+								</>
+							)}
+						</Formik>
+					</ScrollView>
+					<FooterAuth content={'Have an account!'} onPress={() => navigation.navigate(APP_SCREEN.LOGIN as never)} title={'Login'} />
+				</KeyboardAvoidingView>
+			</LinearGradient>
 		</SafeAreaView>
 	);
 };
