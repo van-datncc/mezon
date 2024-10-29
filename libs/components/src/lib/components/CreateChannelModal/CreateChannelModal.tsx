aa9df476baf3cdf4ccd76ec940b119903a97b26b
hmquannnnn <minhquanhoang040103@gmail.com>
2024-10-29 17:53:42 +0700

    fix: create channel modal height

--- libs/components/src/lib/components/CreateChannelModal/CreateChannelModal.tsx
+++ libs/components/src/lib/components/CreateChannelModal/CreateChannelModal.tsx
@@ -155,7 +155,7 @@ export const CreateNewChannelModal = () => {
 				className="w-[100vw] h-[100vh] overflow-hidden fixed top-0 left-0 z-50 bg-black bg-opacity-80 flex flex-row justify-center items-center"
 			>
 				<div
-					className={`z-60 w-full h-full sm:w-4/5 ${isAppChannel ? 'sm:max-h-[730px]' : 'sm:max-h-[600px]'}  md:w-[684px] dark:bg-bgPrimary bg-bgLightModeSecond rounded-2xl flex-col justify-start  items-start gap-3 inline-flex relative shadow-lg`}
+					className={`z-60 w-full h-full sm:w-4/5 ${isAppChannel ? 'sm:max-h-[730px]' : 'sm:max-h-[630px]'}  md:w-[684px] dark:bg-bgPrimary bg-bgLightModeSecond rounded-2xl flex-col justify-start  items-start gap-3 inline-flex relative shadow-lg`}
 				>
 					<div className="self-stretch md:h-96 flex-col justify-start items-start flex">
 						<div className="self-stretch md:h-96 px-5 pt-8 flex-col justify-start items-start gap-3 flex">
