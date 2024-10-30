362bb13d85dc4a684861f76e4fcea75c293fefac
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2024-10-30 17:01:43 +0700

    feat: config codepush

--- apps/mobile/src/app/navigation/index.tsx
+++ apps/mobile/src/app/navigation/index.tsx
@@ -1,10 +1,14 @@
 import i18n from '@mezon/translations';
 import { CreateMezonClientOptions, MezonContextProvider } from '@mezon/transport';
 import * as Sentry from '@sentry/react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import codePush from 'react-native-code-push';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { enableScreens } from 'react-native-screens';
 import 'react-native-svg';
+import VersionInfo from 'react-native-version-info';
+import MezonUpdateVersionModal from '../componentUI/MezonUpdateVersionModal';
 import RootNavigation from './RootNavigator';
 
 const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
@@ -20,6 +24,13 @@ Sentry.init({
 	]
 });
 
+const codePushOptions = {
+	checkFrequency: codePush.CheckFrequency.MANUAL,
+	installMode: codePush.InstallMode.IMMEDIATE,
+	mandatoryInstallMode: codePush.InstallMode.IMMEDIATE
+};
+
+enableScreens(false);
 const mezon: CreateMezonClientOptions = {
 	host: process.env.NX_CHAT_APP_API_HOST as string,
 	key: process.env.NX_CHAT_APP_API_KEY as string,
@@ -28,8 +39,22 @@ const mezon: CreateMezonClientOptions = {
 };
 
 const App = () => {
+	const [isShowUpdateModal, setIsShowUpdateModal] = React.useState<boolean>(false);
+
+	useEffect(() => {
+		checkForUpdate();
+	}, []);
+
+	const checkForUpdate = async () => {
+		const update = await codePush.checkForUpdate(process.env.NX_CODE_PUSH_KEY_MOBILE as string);
+		if (VersionInfo.appVersion === update?.appVersion) {
+			setIsShowUpdateModal(true);
+		}
+	};
+
 	return (
 		<SafeAreaProvider>
+			<MezonUpdateVersionModal visible={isShowUpdateModal} onClose={() => setIsShowUpdateModal(false)} />
 			<I18nextProvider i18n={i18n}>
 				<MezonContextProvider mezon={mezon} connect={true} isFromMobile={true}>
 					<RootNavigation />
@@ -39,4 +64,4 @@ const App = () => {
 	);
 };
 
-export default App;
+export default codePush(codePushOptions)(App);
