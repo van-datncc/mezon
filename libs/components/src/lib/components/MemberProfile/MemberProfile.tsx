9a62db23aaac6b4e76763f8bb5e16c1225cdd4f4
thanh.levan <thanh.levan@ncc.asia>
2024-11-06 18:54:36 +0700

    update activity user

--- libs/components/src/lib/components/MemberProfile/MemberProfile.tsx
+++ libs/components/src/lib/components/MemberProfile/MemberProfile.tsx
@@ -109,6 +109,7 @@ export function MemberProfile({
 
 	const activityNames: { [key: string]: string } = {
 		[ActivitiesName.CODE]: 'Visual Studio Code',
+		[ActivitiesName.VISUAL_STUDIO_CODE]: 'Visual Studio Code',
 		[ActivitiesName.SPOTIFY]: 'Listening to Spotify',
 		[ActivitiesName.LOL]: 'League of Legends'
 	};
