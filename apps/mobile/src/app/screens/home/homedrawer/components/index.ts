8a57a210a098a3739e41b8d6f914174ed08f9d7b
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2024-11-11 19:44:22 +0700

    Mobile: feat embed message mobile (#4805)
    * feat embed message mobile
    
    * feat embed message mobile validate field

--- apps/mobile/src/app/screens/home/homedrawer/components/index.ts
+++ apps/mobile/src/app/screens/home/homedrawer/components/index.ts
@@ -1,6 +1,7 @@
 export * from './AttachmentPicker';
 export * from './AttachmentPreview';
 export * from './BottomKeyboardPicker';
+export * from './EmbedMessage';
 export * from './EmojiPicker';
 export * from './InviteToChannel';
 export * from './MessageItemBS';
