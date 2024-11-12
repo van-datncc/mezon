adb4092155527d80296f006754bef09f66f73f0d
Tungnqs <thanhlamtainguyen@gmail.com>
2024-11-12 15:37:07 +0700

    implement questions page in on boarding setting

--- libs/ui/src/lib/Icons/icons.tsx
+++ libs/ui/src/lib/Icons/icons.tsx
@@ -5784,3 +5784,30 @@ export function HashIcon(props: React.HTMLAttributes<SVGElement>) {
 		</svg>
 	);
 }
+
+export function LongArrowRight(props: React.HTMLAttributes<SVGElement>) {
+	return (
+		<svg aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="" {...props}>
+			<path
+				fill="currentColor"
+				d="M20.7 12.7a1 1 0 0 0 0-1.4l-5-5a1 1 0 1 0-1.4 1.4l3.29 3.3H4a1 1 0 1 0 0 2h13.59l-3.3 3.3a1 1 0 0 0 1.42 1.4l5-5Z"
+				className=""
+			></path>
+		</svg>
+	);
+}
+
+export function CirclePlusFill(props: React.HTMLAttributes<SVGElement>) {
+	return (
+		<svg className="plusIcon_df2e1e" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
+			<circle cx="12" cy="12" r="10" fill="transparent" className=""></circle>
+			<path
+				fill="currentColor"
+				fill-rule="evenodd"
+				d="M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22Zm0-17a1 1 0 0 1 1 1v4h4a1 1 0 1 1 0 2h-4v4a1 1 0 1 1-2 0v-4H7a1 1 0 1 1 0-2h4V7a1 1 0 0 1 1-1Z"
+				clip-rule="evenodd"
+				className=""
+			></path>
+		</svg>
+	);
+}
