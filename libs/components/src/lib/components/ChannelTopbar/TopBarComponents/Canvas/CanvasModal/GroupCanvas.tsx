9246296fde165a1ea307a7444eff413166eceadf
thai.phamquoc <110405911+Cut666@users.noreply.github.com>
2024-11-18 18:15:04 +0700

    fix

--- libs/components/src/lib/components/ChannelTopbar/TopBarComponents/Canvas/CanvasModal/GroupCanvas.tsx
+++ libs/components/src/lib/components/ChannelTopbar/TopBarComponents/Canvas/CanvasModal/GroupCanvas.tsx
@@ -54,30 +54,6 @@ const GroupCanvas = ({ canvasId, channelId, clanId, onClose, creatorIdChannel }:
 		}
 	};
 
-	const unsecuredCopyToClipboard = (text: string) => {
-		const textArea = document.createElement('textarea');
-		textArea.value = text;
-		document.body.appendChild(textArea);
-		textArea.focus();
-		textArea.select();
-		try {
-			document.execCommand('copy');
-		} catch (err) {
-			console.error('Unable to copy to clipboard', err);
-		}
-		document.body.removeChild(textArea);
-	};
-
-	const handleCopyToClipboard = (content: string) => {
-		if (window.isSecureContext && navigator.clipboard) {
-			navigator.clipboard.writeText(content);
-			setIsCopied(true);
-		} else {
-			unsecuredCopyToClipboard(content);
-			setIsCopied(true);
-		}
-	};
-
 	return (
 		<div className="w-full flex gap-2 relative">
 			<div
