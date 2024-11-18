0eff33c4bb354d5906bdbdf4e3cab2d4cefd1411
thai.phamquoc <110405911+Cut666@users.noreply.github.com>
2024-11-18 18:00:43 +0700

    fix cmt

--- libs/components/src/lib/components/ChannelTopbar/TopBarComponents/Canvas/CanvasModal/GroupCanvas.tsx
+++ libs/components/src/lib/components/ChannelTopbar/TopBarComponents/Canvas/CanvasModal/GroupCanvas.tsx
@@ -1,7 +1,6 @@
 import { useAuth } from '@mezon/core';
 import { appActions, canvasActions, canvasAPIActions, selectCanvasEntityById, selectIdCanvas, useAppDispatch } from '@mezon/store';
 import { Icons } from '@mezon/ui';
-import isElectron from 'is-electron';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -92,10 +91,7 @@ const GroupCanvas = ({ canvasId, channelId, clanId, onClose, creatorIdChannel }:
 			</div>
 			<button
 				onClick={() =>
-					handleCopyToClipboard(
-						(isElectron() ? process.env.NX_CHAT_APP_REDIRECT_URI : window.location.origin) +
-							`/chat/clans/${clanId}/channels/${channelId}/canvas/${canvasId}`
-					)
+					handleCopyToClipboard(process.env.NX_CHAT_APP_REDIRECT_URI + `/chat/clans/${clanId}/channels/${channelId}/canvas/${canvasId}`)
 				}
 				className={`absolute top-0 dark:border-black dark:shadow-[#000000] bg-white dark:bg-transparent shadow-emoji_item-delete font-bold w-6 h-6 flex items-center justify-center rounded-full ${isCopied ? 'text-red-600' : 'text-white'} ${!isDisableDelCanvas ? 'right-[35px]' : 'right-[5px]'}`}
 				style={{ top: '5px' }}
