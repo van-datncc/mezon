0dcb23446194c4473c37d4821401ae94a3e6833b
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2024-12-04 18:33:06 +0700

    feat update field component embed mobile

---
+++ apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/EmbedRadioItem/styles.ts
@@ -0,0 +1,23 @@
+import { Attributes, size } from '@mezon/mobile-ui';
+import { StyleSheet } from 'react-native';
+
+export const style = (colors: Attributes) =>
+	StyleSheet.create({
+		name: {
+			color: colors.white,
+			fontWeight: 'bold',
+			fontSize: size.medium,
+			marginTop: size.s_10
+		},
+		value: {
+			color: colors.text,
+			fontSize: size.s_13,
+			marginTop: size.s_6,
+			width: '80%'
+		},
+		option: {
+			justifyContent: 'space-between',
+			flexDirection: 'row',
+			alignItems: 'center'
+		}
+	});
--- apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/index.tsx
@@ -1,21 +1,31 @@
 import { useTheme } from '@mezon/mobile-ui';
-import { memo, useMemo } from 'react';
+import { embedActions, useAppDispatch } from '@mezon/store-mobile';
+import { EMessageComponentType, IMessageRatioOption, InputComponent, SelectComponent } from '@mezon/utils';
+import debounce from 'lodash.debounce';
+import { memo, useMemo, useState } from 'react';
 import { Text, View } from 'react-native';
+import { MezonInput, MezonSelect } from '../../../../../../componentUI';
+import { EmbedRadioButton } from './EmbedRadioItem';
 import { style } from './styles';
 
 interface Field {
 	name: string;
 	value: string;
 	inline?: boolean;
+	options?: IMessageRatioOption[];
+	inputs?: SelectComponent | InputComponent;
 }
 
 interface EmbedFieldsProps {
+	message_id: string;
 	fields: Field[];
 }
 
-export const EmbedFields = memo(({ fields }: EmbedFieldsProps) => {
+export const EmbedFields = memo(({ message_id, fields }: EmbedFieldsProps) => {
 	const { themeValue } = useTheme();
 	const styles = style(themeValue);
+	const [checked, setChecked] = useState<number[]>([]);
+	const dispatch = useAppDispatch();
 	const groupedFields = useMemo(() => {
 		return fields.reduce<Field[][]>((acc, field) => {
 			if (!field.inline) {
@@ -31,6 +41,50 @@ export const EmbedFields = memo(({ fields }: EmbedFieldsProps) => {
 			return acc;
 		}, []);
 	}, [fields]);
+
+	const handleCheckRadioButton = (index: number, option: IMessageRatioOption) => {
+		if (!option?.name) {
+			setChecked([index]);
+			handleRadioValue(option);
+			return;
+		}
+		if (checked.includes(index)) {
+			setChecked(checked.filter((check) => check !== index));
+			return;
+		}
+		setChecked([...checked, index]);
+		handleRadioValue(option);
+	};
+
+	const handleRadioValue = (option: IMessageRatioOption) => {
+		dispatch(
+			embedActions.addEmbedValueOptions({
+				message_id: message_id,
+				data: {
+					id: option?.id,
+					value: option?.value
+				}
+			})
+		);
+	};
+
+	const handleChangeDataInput = (value: string, id?: string) => {
+		dispatch(
+			embedActions.addEmbedValueInput({
+				message_id: message_id,
+				data: {
+					id: id,
+					value: value
+				},
+				multiple: true
+			})
+		);
+	};
+
+	const handleChangeText = debounce((text: string, id: string) => {
+		handleChangeDataInput(text, id);
+	}, 300);
+
 	return (
 		<View>
 			{!!groupedFields?.length &&
@@ -39,8 +93,37 @@ export const EmbedFields = memo(({ fields }: EmbedFieldsProps) => {
 						{!!field.length &&
 							field.map((fieldItem, fieldIndex) => (
 								<View key={`field${index}-${fieldIndex}`}>
-									<Text style={styles.name}>{fieldItem.name}:</Text>
-									<Text style={styles.value}>{fieldItem.value}</Text>
+									{!!fieldItem?.name && (
+										<View>
+											<Text style={styles.name}>{fieldItem?.name}:</Text>
+											<Text style={styles.value}>{fieldItem?.value}</Text>
+										</View>
+									)}
+									{!!fieldItem?.options?.length &&
+										fieldItem?.options?.map((optionItem, optionIndex) => (
+											<EmbedRadioButton
+												key={`Embed_field_option_${optionItem}_${optionIndex}`}
+												option={optionItem}
+												checked={checked?.includes(optionIndex)}
+												onCheck={() => handleCheckRadioButton(optionIndex, optionItem)}
+											/>
+										))}
+									{!!fieldItem?.inputs && (
+										<View>
+											{fieldItem?.inputs?.type === EMessageComponentType.INPUT ? (
+												<MezonInput
+													placeHolder={fieldItem?.inputs?.component?.placeholder}
+													onTextChange={(text) => handleChangeText(text, fieldItem?.inputs?.id)}
+												/>
+											) : (
+												<MezonSelect
+													data={fieldItem?.inputs?.component?.options.map((item) => {
+														return { title: item?.label, value: item?.value };
+													})}
+												/>
+											)}
+										</View>
+									)}
 								</View>
 							))}
 					</View>
