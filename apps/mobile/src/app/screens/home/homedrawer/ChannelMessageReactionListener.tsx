05ee7885ef42fea9032d440d53a6c11afcdd30ac
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-12-06 17:00:35 +0700

    Sometimes reacting fails when in the background

--- apps/mobile/src/app/screens/home/homedrawer/ChannelMessageReactionListener.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/ChannelMessageReactionListener.tsx
@@ -1,8 +1,9 @@
-import { useChatReaction } from '@mezon/core';
+import { ChatContext, useChatReaction } from '@mezon/core';
 import { ActionEmitEvent } from '@mezon/mobile-components';
 import { selectCurrentChannel } from '@mezon/store-mobile';
+import { useMezon } from '@mezon/transport';
 import { isPublicChannel } from '@mezon/utils';
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import { DeviceEventEmitter, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { IReactionMessageProps } from './components';
@@ -10,10 +11,14 @@ import { IReactionMessageProps } from './components';
 const ChannelMessageReactionListener = React.memo(() => {
 	const { reactionMessageDispatch } = useChatReaction({ isMobile: true });
 	const currentChannel = useSelector(selectCurrentChannel);
-
+	const { socketRef } = useMezon();
+	const { handleReconnect } = useContext(ChatContext);
 	const onReactionMessage = useCallback(
 		async (data: IReactionMessageProps) => {
-			await reactionMessageDispatch(
+			if (!socketRef?.current?.isOpen()) {
+				handleReconnect('');
+			}
+			reactionMessageDispatch(
 				data?.id ?? '',
 				data?.messageId ?? '',
 				data?.emojiId ?? '',
@@ -24,7 +29,7 @@ const ChannelMessageReactionListener = React.memo(() => {
 				isPublicChannel(currentChannel)
 			);
 		},
-		[currentChannel, reactionMessageDispatch]
+		[currentChannel, reactionMessageDispatch, socketRef, handleReconnect]
 	);
 
 	useEffect(() => {
