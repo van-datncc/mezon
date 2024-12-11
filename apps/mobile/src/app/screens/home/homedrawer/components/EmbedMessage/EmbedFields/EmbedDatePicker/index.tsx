465bdccea549de9c52887fae02761674a38de751
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2024-12-11 16:46:00 +0700

    fix date picker payload

--- apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/EmbedDatePicker/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/EmbedDatePicker/index.tsx
@@ -1,3 +1,4 @@
+import { useBottomSheetModal } from '@gorhom/bottom-sheet';
 import { embedActions, useAppDispatch } from '@mezon/store-mobile';
 import { IMessageDatePicker } from '@mezon/utils';
 import moment from 'moment';
@@ -12,8 +13,10 @@ type EmbedInputProps = {
 
 export const EmbedDatePicker = memo(({ input, buttonId, messageId }: EmbedInputProps) => {
 	const dispatch = useAppDispatch();
+	const { dismiss } = useBottomSheetModal();
 
 	const handleDatePicked = (value) => {
+		dismiss();
 		const formattedDate = moment(value).format('YYYY-MM-DD');
 		dispatch(
 			embedActions.addEmbedValue({
