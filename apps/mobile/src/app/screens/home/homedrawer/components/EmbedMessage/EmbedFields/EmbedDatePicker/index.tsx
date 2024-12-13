801e2861462300120d5a0197d0ea475347150281
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2024-12-13 14:31:37 +0700

    feat custom direct message logo mobile

--- apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/EmbedDatePicker/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/EmbedMessage/EmbedFields/EmbedDatePicker/index.tsx
@@ -1,8 +1,9 @@
 import { useBottomSheetModal } from '@gorhom/bottom-sheet';
+import { size, useTheme } from '@mezon/mobile-ui';
 import { embedActions, useAppDispatch } from '@mezon/store-mobile';
 import { IMessageDatePicker } from '@mezon/utils';
 import moment from 'moment';
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { MezonDateTimePicker } from '../../../../../../../../app/componentUI';
 
 type EmbedInputProps = {
@@ -12,11 +13,18 @@ type EmbedInputProps = {
 };
 
 export const EmbedDatePicker = memo(({ input, buttonId, messageId }: EmbedInputProps) => {
+	const { themeValue } = useTheme();
 	const dispatch = useAppDispatch();
 	const { dismiss } = useBottomSheetModal();
+	const [date, setDate] = useState<Date>();
 
-	const handleDatePicked = (value) => {
+	useEffect(() => {
+		handleDatePicked(new Date());
+	}, []);
+
+	const handleDatePicked = (value: Date) => {
 		dismiss();
+		setDate(value);
 		const formattedDate = moment(value).format('YYYY-MM-DD');
 		dispatch(
 			embedActions.addEmbedValue({
@@ -29,5 +37,11 @@ export const EmbedDatePicker = memo(({ input, buttonId, messageId }: EmbedInputP
 		);
 	};
 
-	return <MezonDateTimePicker onChange={(value) => handleDatePicked(value)} />;
+	return (
+		<MezonDateTimePicker
+			value={date}
+			onChange={(value) => handleDatePicked(value)}
+			containerStyle={{ borderWidth: 1, borderColor: themeValue.border, borderRadius: size.s_12, paddingVertical: size.s_12 }}
+		/>
+	);
 });
