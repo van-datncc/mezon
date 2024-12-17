f2b51ea479e2438c4c1b0f7c32da99267169910c
loi-huynhphuc <loi.huynhphuc@ncc.asia>
2024-12-17 18:41:58 +0700

    Mobile App: loadmore data tab notification and recheck render data notification tab

--- apps/mobile/src/app/screens/Notifications/NotificationItem/index.tsx
+++ apps/mobile/src/app/screens/Notifications/NotificationItem/index.tsx
@@ -1,21 +1,38 @@
 import { Block, size, useTheme } from '@mezon/mobile-ui';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { NotificationCode } from '@mezon/utils';
 import NotificationIndividualItem from '../NotificationIndividualItem';
 import NotificationMentionItem from '../NotificationMentionItem';
+import NotificationWebhookClan from '../NotificationWebhookClan/NotificationWebhookClan';
 import { NotifyProps } from '../types';
 
 const NotificationItem = React.memo(({ notify, onLongPressNotify, onPressNotify }: NotifyProps) => {
 	const { themeValue } = useTheme();
+
+	const isNotificationIndividual = useMemo(
+		() =>
+			notify?.code !== NotificationCode.USER_REPLIED &&
+			notify?.code !== NotificationCode.USER_MENTIONED &&
+			notify?.code !== NotificationCode.NOTIFICATION_CLAN,
+		[notify]
+	);
+
+	const isNotificationMentionItem = useMemo(
+		() => notify?.code === NotificationCode.USER_REPLIED || notify?.code === NotificationCode.USER_MENTIONED,
+		[notify]
+	);
+
+	const isNotificationWebhookClan = useMemo(() => notify?.code === NotificationCode.NOTIFICATION_CLAN, [notify]);
 	return (
 		<Block borderBottomWidth={size.s_2} borderBottomColor={themeValue.secondaryLight} paddingTop={size.s_6}>
-			{notify?.code !== NotificationCode.USER_MENTIONED && notify?.code !== NotificationCode.USER_REPLIED ? (
+			{isNotificationIndividual ? (
 				<NotificationIndividualItem onPressNotify={onPressNotify} notify={notify} onLongPressNotify={onLongPressNotify} />
 			) : null}
-			{notify?.code === NotificationCode.USER_MENTIONED || notify?.code === NotificationCode.USER_REPLIED ? (
+			{isNotificationMentionItem ? (
 				<NotificationMentionItem onPressNotify={onPressNotify} notify={notify} onLongPressNotify={onLongPressNotify} />
 			) : null}
+			{isNotificationWebhookClan && <NotificationWebhookClan notify={notify}></NotificationWebhookClan>}
 		</Block>
 	);
 });
--- apps/mobile/src/app/screens/Notifications/NotificationMentionItem/index.tsx
+++ apps/mobile/src/app/screens/Notifications/NotificationMentionItem/index.tsx
@@ -1,19 +1,18 @@
-import { AVATAR_DEFAULT_URL } from '@mezon/mobile-components';
+import { useGetPriorityNameFromUserClan } from '@mezon/core';
 import { useColorsRoleById, useTheme } from '@mezon/mobile-ui';
-import { selectChannelById, selectClanById, selectMemberClanByUserId2, useAppSelector } from '@mezon/store-mobile';
-import { createImgproxyUrl, getNameForPrioritize } from '@mezon/utils';
+import { selectChannelById, selectClanById, useAppSelector } from '@mezon/store-mobile';
+import { getNameForPrioritize } from '@mezon/utils';
 import { safeJSONParse } from 'mezon-js';
 import React, { useMemo } from 'react';
 import { Text, View } from 'react-native';
-import FastImage from 'react-native-fast-image';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { MezonAvatar } from '../../../componentUI';
 import { useMessageParser } from '../../../hooks/useMessageParser';
-import { useMessageSender } from '../../../hooks/useMessageSender';
 import MessageNotification from '../MessageNotification';
 import { ENotifyBsToShow, NotifyProps } from '../types';
 import { style } from './NotificationMentionItem.styles';
 
-function parseObject(obj: any) {
+export function parseObject(obj: any) {
 	let attachments;
 	let mentions;
 	let reactions;
@@ -59,12 +58,10 @@ function parseObject(obj: any) {
 const NotificationMentionItem = React.memo(({ notify, onLongPressNotify, onPressNotify }: NotifyProps) => {
 	const { themeValue } = useTheme();
 	const styles = style(themeValue);
-	const user = useAppSelector((state) => selectMemberClanByUserId2(state, notify?.sender_id || ''));
-	const { avatarImg } = useMessageSender(user as any);
 	const channelInfo = useAppSelector((state) => selectChannelById(state, notify?.content?.channel_id || ''));
 	const data = parseObject(notify?.content);
 	const clan = useAppSelector(selectClanById(notify?.content?.clan_id as string));
-
+	const { priorityAvatar } = useGetPriorityNameFromUserClan(notify?.sender_id);
 	const { messageTimeDifference } = useMessageParser(data);
 	const colorsUsername = useColorsRoleById(notify?.sender_id)?.highestPermissionRoleColor;
 	const subjectText = useMemo(() => {
@@ -89,14 +86,10 @@ const NotificationMentionItem = React.memo(({ notify, onLongPressNotify, onPress
 			<View style={styles.notifyContainer}>
 				<View style={styles.notifyHeader}>
 					<View style={styles.boxImage}>
-						<FastImage
-							source={{
-								uri: avatarImg
-									? createImgproxyUrl(avatarImg ?? '', { width: 100, height: 100, resizeType: 'fit' })
-									: AVATAR_DEFAULT_URL
-							}}
-							style={styles.image}
-						/>
+						<MezonAvatar
+							avatarUrl={priorityAvatar ? priorityAvatar : notify?.content?.avatar}
+							username={notify?.content?.userName}
+						></MezonAvatar>
 					</View>
 					<View style={styles.notifyContent}>
 						<Text numberOfLines={2} style={styles.notifyHeaderTitle}>
