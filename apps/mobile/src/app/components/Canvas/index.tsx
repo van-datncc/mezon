0501c13462618c7a163bc3e7cbcaff29db9735f8
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2025-01-03 10:49:10 +0700

    fix canvas list and add search option mobile

--- apps/mobile/src/app/components/Canvas/index.tsx
+++ apps/mobile/src/app/components/Canvas/index.tsx
@@ -1,11 +1,13 @@
 import { useTheme } from '@mezon/mobile-ui';
 import { canvasAPIActions, selectCanvasIdsByChannelId, useAppDispatch, useAppSelector } from '@mezon/store-mobile';
+import { normalizeString } from '@mezon/utils';
 import { useNavigation } from '@react-navigation/native';
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useMemo, useState } from 'react';
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { APP_SCREEN } from '../../navigation/ScreenTypes';
 import CanvasItem from './CanvasItem';
+import CanvasSearch from './CanvasSearch';
 import { style } from './styles';
 
 const Canvas = memo(({ channelId, clanId }: { channelId: string; clanId: string }) => {
@@ -13,6 +15,7 @@ const Canvas = memo(({ channelId, clanId }: { channelId: string; clanId: string 
 	const styles = style(themeValue);
 	const navigation = useNavigation<any>();
 	const dispatch = useAppDispatch();
+	const [searchText, setSearchText] = useState('');
 
 	useEffect(() => {
 		fetchCanvas();
@@ -30,21 +33,31 @@ const Canvas = memo(({ channelId, clanId }: { channelId: string; clanId: string 
 
 	const canvases = useAppSelector((state) => selectCanvasIdsByChannelId(state, channelId));
 
+	const filterCanvas = useMemo(() => {
+		return canvases.filter((canvas) =>
+			normalizeString(canvas?.title ? canvas?.title?.replace(/\n/g, ' ') : 'Untitled').includes(normalizeString(searchText))
+		);
+	}, [canvases, searchText]);
+
+	const handleSearchChange = (text) => {
+		setSearchText(text);
+	};
+
 	return (
 		<ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer} showsVerticalScrollIndicator={false}>
+			<CanvasSearch onSearchTextChange={handleSearchChange} />
 			<View style={styles.container}>
-				{canvases?.map((item) => (
+				{filterCanvas?.map((item, index) => (
 					<CanvasItem
-						key={`canvas${item}`}
-						channelId={channelId}
-						canvasId={item}
+						key={`canvas_${index}_${item?.id}`}
+						canvas={item}
 						onPressItem={() => {
 							navigation.navigate(APP_SCREEN.MENU_CHANNEL.STACK, {
 								screen: APP_SCREEN.MENU_CHANNEL.CANVAS,
 								params: {
 									channelId: channelId,
 									clanId: clanId,
-									canvasId: item
+									canvasId: item?.id
 								}
 							});
 						}}
