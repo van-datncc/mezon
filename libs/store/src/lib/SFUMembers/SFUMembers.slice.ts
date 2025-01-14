beb477aacc30e08582e2490dbdd794abb7fac142
nam.trinhhoai <kiaran.hoang.thatera@gmail.com>
2025-01-14 09:44:55 +0700

    rename sfu

--- libs/store/src/lib/SFUMembers/SFUMembers.slice.ts
+++ libs/store/src/lib/SFUMembers/SFUMembers.slice.ts
@@ -2,53 +2,53 @@ import { captureSentryError } from '@mezon/logger';
 import { IChannelMember, LoadingStatus } from '@mezon/utils';
 import { EntityState, PayloadAction, createAsyncThunk, createEntityAdapter, createSelector, createSlice } from '@reduxjs/toolkit';
 import { ChannelType } from 'mezon-js';
-import { ApiPTTChannelUser } from 'mezon-js/api.gen';
+import { ApiSFUChannelUser } from 'mezon-js/api.gen';
 import { ensureSession, getMezonCtx } from '../helpers';
 
-export const PTT_USERS_FEATURES_KEY = 'pushToTalkUsers';
+export const SFU_USERS_FEATURES_KEY = 'sfuUsers';
 
-export interface IPttUsersEntity extends ApiPTTChannelUser {
+export interface ISFUUsersEntity extends ApiSFUChannelUser {
 	id: string;
 }
 
-export interface IPttUsersState extends EntityState<IPttUsersEntity, string> {
+export interface ISFUUsersState extends EntityState<ISFUUsersEntity, string> {
 	loadingStatus: LoadingStatus;
 	error?: string | null;
 	usersInPTT: IChannelMember[];
 }
 
 export const pttUsersAdapter = createEntityAdapter({
-	selectId: (user: IPttUsersEntity) => user.id
+	selectId: (user: ISFUUsersEntity) => user.id
 });
 
-const initialState: IPttUsersState = pttUsersAdapter.getInitialState({
+const initialState: ISFUUsersState = pttUsersAdapter.getInitialState({
 	loadingStatus: 'not loaded',
 	error: null,
 	usersInPTT: []
 });
 
-export interface IFetchPttMembersInChannelRequest {
+export interface IFetchSFUMembersInChannelRequest {
 	clanId: string;
 	channelId: string;
 	channelType: ChannelType;
 }
 
-export const getAllPttMembersInChannel = createAsyncThunk(
-	'pushToTalk/getAllPttMembersInChannel',
-	async ({ clanId, channelId, channelType }: IFetchPttMembersInChannelRequest, thunkAPI) => {
+export const getAllSfuMembersInChannel = createAsyncThunk(
+	'sfu/getAllSfuMembersInChannel',
+	async ({ clanId, channelId, channelType }: IFetchSFUMembersInChannelRequest, thunkAPI) => {
 		try {
 			const mezon = await ensureSession(getMezonCtx(thunkAPI));
 			const response = await mezon.client.listPTTChannelUsers(mezon.session, clanId, channelId, channelType);
 			return response.ptt_channel_users;
 		} catch (err) {
-			captureSentryError(err, 'pushToTalk/getAllPttMembersInChannel');
+			captureSentryError(err, 'sfu/getAllSfuMembersInChannel');
 			return thunkAPI.rejectWithValue(err);
 		}
 	}
 );
 
-export const pushToTalkMembersSlice = createSlice({
-	name: PTT_USERS_FEATURES_KEY,
+export const sfuMembersSlice = createSlice({
+	name: SFU_USERS_FEATURES_KEY,
 	initialState: initialState,
 	reducers: {
 		add: pttUsersAdapter.addOne,
@@ -57,14 +57,14 @@ export const pushToTalkMembersSlice = createSlice({
 	},
 	extraReducers: (builder) => {
 		builder
-			.addCase(getAllPttMembersInChannel.pending, (state: IPttUsersState) => {
+			.addCase(getAllSfuMembersInChannel.pending, (state: ISFUUsersState) => {
 				state.loadingStatus = 'loading';
 			})
-			.addCase(getAllPttMembersInChannel.fulfilled, (state: IPttUsersState, action: PayloadAction<any>) => {
+			.addCase(getAllSfuMembersInChannel.fulfilled, (state: ISFUUsersState, action: PayloadAction<any>) => {
 				pttUsersAdapter.setAll(state, action.payload);
 				state.loadingStatus = 'loaded';
 			})
-			.addCase(getAllPttMembersInChannel.rejected, (state: IPttUsersState, action) => {
+			.addCase(getAllSfuMembersInChannel.rejected, (state: ISFUUsersState, action) => {
 				state.loadingStatus = 'error';
 				state.error = action.error.message;
 			});
@@ -72,17 +72,17 @@ export const pushToTalkMembersSlice = createSlice({
 });
 
 export const pttMembersActions = {
-	...pushToTalkMembersSlice.actions
+	...sfuMembersSlice.actions
 };
 
 const { selectEntities } = pttUsersAdapter.getSelectors();
-export const pushToTalkMembersReducer = pushToTalkMembersSlice.reducer;
-export const getPttMembersState = (rootState: { [PTT_USERS_FEATURES_KEY]: IPttUsersState }): IPttUsersState => rootState[PTT_USERS_FEATURES_KEY];
+export const pushToTalkMembersReducer = sfuMembersSlice.reducer;
+export const getPttMembersState = (rootState: { [SFU_USERS_FEATURES_KEY]: ISFUUsersState }): ISFUUsersState => rootState[SFU_USERS_FEATURES_KEY];
 
-export const selectPttMembersEntities = createSelector(getPttMembersState, selectEntities);
+export const selectSfuMembersEntities = createSelector(getPttMembersState, selectEntities);
 
-export const selectPttMembersByChannelId = (channelId: string) =>
-	createSelector(selectPttMembersEntities, (entities) => {
-		const inPttMembers = Object.values(entities);
-		return inPttMembers.filter((user) => user && user.channel_id === channelId);
+export const selectSfuMembersByChannelId = (channelId: string) =>
+	createSelector(selectSfuMembersEntities, (entities) => {
+		const inSfuMembers = Object.values(entities);
+		return inSfuMembers.filter((user) => user && user.channel_id === channelId);
 	});
