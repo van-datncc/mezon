6272ac3c4481ef8f1c6c343ef10e1ce16c9de8c0
nam.trinhhoai <kiaran.hoang.thatera@gmail.com>
2025-01-14 10:51:26 +0700

    add start local stream

--- apps/chat/src/app/pages/channel/ChannelApp.tsx
+++ apps/chat/src/app/pages/channel/ChannelApp.tsx
@@ -12,10 +12,10 @@ export function ChannelApps({
 	miniAppRef: RefObject<HTMLIFrameElement>;
 	miniAppDataHash: string;
 }) {
-	const { setClanId, setChannelId } = useWebRTC();
+	const { setClanId, setChannelId, startLocalStream } = useWebRTC();
 	setClanId(appChannel.clan_id || '');
 	setChannelId(appChannel.channel_id || '');
-
+	startLocalStream();
 	return appChannel?.url ? (
 		<iframe ref={miniAppRef} title={appChannel?.url} src={appChannel?.url + `#${miniAppDataHash}`} className={'w-full h-full'}></iframe>
 	) : (
