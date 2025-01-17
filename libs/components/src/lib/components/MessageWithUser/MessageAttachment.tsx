6d0302123c84127aa7796961d7e1fc72528267c3
sadohpv <sigaldphp@gmail.com>
2025-01-17 16:07:12 +0700

    clean code image layout

--- libs/components/src/lib/components/MessageWithUser/MessageAttachment.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageAttachment.tsx
@@ -114,74 +114,35 @@ const designLayout = (
 	const listImageSize: { width: number; height: number }[] = [];
 
 	if (images.length >= 2) {
-		for (let i = 0; i < images.length; i += 2) {
-			if (images[i + 1] && images[i]?.height && images[i + 1]?.height) {
-				const heightPicOne = images[i].height || 0;
-				const heightPicTwo = images[i + 1].height || 0;
-
-				let sameHeight = 0;
-				if (heightPicOne > heightPicTwo) {
-					sameHeight = heightPicTwo + Math.round((heightPicOne - heightPicTwo));
-				} else {
-					sameHeight = heightPicOne + Math.round((heightPicTwo - heightPicOne));
-				}
-
-				const widthPicOneNew = ((images[i].width || 0) * sameHeight) / (images[i].height || 1);
-
-				const widthPicTwoNew = ((images[i + 1].width || 0) * sameHeight) / (images[i + 1].height || 1);
-
-				const percent = (widthPicOneNew + widthPicTwoNew) / WIDTH_ALBUM_WITH_SPACE;
-
-				listImageSize[i] = {
-					width: Math.round(widthPicOneNew / percent),
-					height: Math.round(sameHeight / percent)
-				};
-				listImageSize[i + 1] = {
-					width: Math.round(widthPicTwoNew / percent),
-					height: Math.round(sameHeight / percent)
-				};
-			} else if (images[i + 1]) {
-				const width = MAX_WIDTH_ALBUM_IMAGE;
-				listImageSize[i] = {
-					width: WIDTH_ALBUM_WITH_SPACE / NUMBER_IMAGE_ON_ROW,
-					height: 150
-				};
-			} else {
-				const width = MAX_WIDTH_ALBUM_IMAGE;
-				listImageSize[i] = {
-					width: width,
-					height: Math.round((width * (images[i].height || 1)) / (images[i].width || 1))
-				};
-			}
-		}
-	} else if (images.length == 1) {
-		if (!images[0]?.height || !images[0]?.width) {
-			listImageSize[0] = {
-				height: 150,
-				width: images[0].width || 0
-			};
-			return listImageSize;
-		} else {
-			const aspectRatio = images[0]?.width / images[0]?.height;
-			let heightAlonePic = images[0]?.height;
-			let widthAlonePic = images[0]?.width;
-			if (heightAlonePic >= 275) {
-				heightAlonePic = 275;
-				widthAlonePic = heightAlonePic * aspectRatio;
-			}
-
-			if (widthAlonePic >= 550) {
-				widthAlonePic = 550;
-				heightAlonePic = widthAlonePic / aspectRatio;
-			}
-	
-			listImageSize[0] = {
-				height: Math.round(heightAlonePic),
-				width: Math.round(widthAlonePic)
-			};
-			return listImageSize;
-		}
+		return designAlbumLayout(images);
 	}
+
+	if (!images[0]?.height || !images[0]?.width) {
+		listImageSize[0] = {
+			height: 150,
+			width: images[0].width || 0
+		};
+		return listImageSize;
+	}
+	
+	const aspectRatio = images[0].width / images[0].height;
+	let heightAlonePic = images[0].height;
+	let widthAlonePic = images[0].width;
+
+	if (heightAlonePic >= 275) {
+		heightAlonePic = 275;
+		widthAlonePic = heightAlonePic * aspectRatio;
+	}
+
+	if (widthAlonePic >= 550) {
+		widthAlonePic = 550;
+		heightAlonePic = widthAlonePic / aspectRatio;
+	}
+
+	listImageSize[0] = {
+		height: Math.round(heightAlonePic),
+		width: Math.round(widthAlonePic)
+	};
 
 	return listImageSize;
 };
@@ -220,3 +181,44 @@ const ImageAlbum = ({
 };
 
 export default memo(MessageAttachment);
+
+
+
+const designAlbumLayout = (images: (ApiMessageAttachment & { create_time?: string; })[]): { width: number; height: number }[] => {
+	const listImageSize: { width: number; height: number }[] = [];
+
+	for (let i = 0; i < images.length; i += 2) {
+		if (images[i + 1] && images[i]?.height && images[i + 1]?.height) {
+			const heightPicOne = images[i].height || 0;
+			const heightPicTwo = images[i + 1].height || 0;
+
+			const sameHeight = Math.max(heightPicOne, heightPicTwo);
+
+			const widthPicOneNew = (images[i].width || 0) * sameHeight / (images[i].height || 1);
+			const widthPicTwoNew = (images[i + 1].width || 0) * sameHeight / (images[i + 1].height || 1);
+
+			const percent = (widthPicOneNew + widthPicTwoNew) / WIDTH_ALBUM_WITH_SPACE;
+
+			listImageSize[i] = {
+				width: Math.round(widthPicOneNew / percent),
+				height: Math.round(sameHeight / percent)
+			};
+			listImageSize[i + 1] = {
+				width: Math.round(widthPicTwoNew / percent),
+				height: Math.round(sameHeight / percent)
+			};
+		} else if (images[i + 1]) {
+			listImageSize[i] = {
+				width: WIDTH_ALBUM_WITH_SPACE / NUMBER_IMAGE_ON_ROW,
+				height: 150
+			};
+		} else {
+			const width = MAX_WIDTH_ALBUM_IMAGE;
+			listImageSize[i] = {
+				width: width,
+				height: Math.round((width * (images[i].height || 1)) / (images[i].width || 1))
+			};
+		}
+	}
+	return listImageSize;
+};
