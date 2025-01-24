094c4fa302873e300be94b1930f47d63fff26585
Tungnqs <thanhlamtainguyen@gmail.com>
2025-01-24 11:23:19 +0700

    update dependency

--- libs/components/src/lib/components/MessageWithUser/MessageInput.tsx
+++ libs/components/src/lib/components/MessageWithUser/MessageInput.tsx
@@ -107,7 +107,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ messageId, channelId, mode,
 			tp: channelDraftMessage?.draftTopicId,
 			cid: channelDraftMessage?.draftContent?.cid
 		};
-	}, [channelDraftMessage]);
+	}, [channelDraftMessage?.draftContent]);
 
 	const processedMentionDraft: ApiMessageMention[] = channelDraftMessage?.draftMention;
 
