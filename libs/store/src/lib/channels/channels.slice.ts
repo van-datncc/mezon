570156348116ea061f53f59c38b0a4e0e387de80
sadohpv <sigaldphp@gmail.com>
2025-02-04 14:03:45 +0700

    remove console.log

--- libs/store/src/lib/channels/channels.slice.ts
+++ libs/store/src/lib/channels/channels.slice.ts
@@ -525,7 +525,7 @@ export const addThreadToChannels = createAsyncThunk(
 			thunkAPI.dispatch(
 				listChannelRenderAction.addThreadToListRender({
 					clanId: clanId,
-					channel: {...channelData, active : 1}
+					channel: channelData
 				})
 			);
 		}
@@ -614,12 +614,10 @@ export const fetchChannels = createAsyncThunk(
 				};
 			}
 
-      console.log('response.channeldesc: ', response.channeldesc);
 			const channels = response.channeldesc.map((channel) => ({
 				...mapChannelToEntity(channel),
 				last_seen_message: channel.last_seen_message ? channel.last_seen_message : { timestamp_seconds: 0 }
 			}));
-      console.log('channels: ', channels);
 
 			const [favorChannels, listCategory] = await Promise.all([
 				thunkAPI.dispatch(fetchListFavoriteChannel({ clanId })),
