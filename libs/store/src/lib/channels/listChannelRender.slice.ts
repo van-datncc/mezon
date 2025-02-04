804343c2f7a979fc676d1e5c82b4a9c15c673847
tranthomanh <kaitran1112@gmail.com>
2025-02-04 11:12:31 +0700

    remove the redundant code

--- libs/store/src/lib/channels/listChannelRender.slice.ts
+++ libs/store/src/lib/channels/listChannelRender.slice.ts
@@ -195,28 +195,21 @@ export const selectListChannelRenderByClanId = createSelector(
 );
 
 function sortChannels(channels: IChannel[]): IChannel[] {
-	const channelMap = new Map<string, IChannel>();
 	const sortedChannels: IChannel[] = [];
 
-	// Create a map of channels by their id
-	channels.forEach((channel) => {
-		channelMap.set(channel.id, channel);
-	});
-
-	// Use forEach to sort channels
+	// Add channel into list first
 	channels.forEach((channel) => {
 		if (!channel.parrent_id || channel.parrent_id === '0') {
 			sortedChannels.push(channel);
 			addChildren(channel, sortedChannels);
 		}
 	});
-
+	// Then add threads of a channel into list
 	function addChildren(parent: IChannel, acc: IChannel[]) {
 		channels
 			.filter((child) => child.parrent_id === parent.id)
 			.forEach((child) => {
 				acc.push(child);
-				addChildren(child, acc);
 			});
 	}
 
