4a48e6780fd48d9133ccdd92e4816f0d3b8bb340
sadohpv <sigaldphp@gmail.com>
2025-02-06 11:53:33 +0700

    fix number count

--- libs/components/src/lib/components/PanelChannel/index.tsx
+++ libs/components/src/lib/components/PanelChannel/index.tsx
@@ -146,8 +146,9 @@ const PanelChannel = ({ coords, channel, openSetting, setIsShowPanelChannel, onD
 				isPrivate: currentChannel.channel_private || 0
 			})
 		);
-
-		dispatch(clansActions.updateClanBadgeCount({ clanId: currentClan?.id || '', count: -1 }))
+    if(channel.count_mess_unread){
+      dispatch(clansActions.updateClanBadgeCount({ clanId: currentClan?.id || '', count: -channel.count_mess_unread }))
+    }
 
 		handleCloseModalConfirm();
 		navigate(`/chat/clans/${currentClan?.id}/channels/${currentChannel.parrent_id}`);
