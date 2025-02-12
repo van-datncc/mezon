1b2d443a63a2159c08b5d961e6c4c1e5d892c037
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2025-02-12 22:01:01 +0700

    optimize: load DM list and DM detail, remove warning noti permission

--- apps/mobile/src/app/screens/messages/MessagesScreen.tsx
+++ apps/mobile/src/app/screens/messages/MessagesScreen.tsx
@@ -1,118 +1,32 @@
-import { BottomSheetModal } from '@gorhom/bottom-sheet';
-import { ActionEmitEvent, Icons } from '@mezon/mobile-components';
-import { size, useTheme } from '@mezon/mobile-ui';
-import { DirectEntity, RootState, directActions, selectDirectsOpenlistOrder, useAppDispatch } from '@mezon/store-mobile';
+import { useIdleRender } from '@mezon/core';
+import { directActions, selectDirectsOpenlistOrder, useAppDispatch } from '@mezon/store-mobile';
 import { useFocusEffect } from '@react-navigation/native';
-import { FlashList } from '@shopify/flash-list';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { AppState, DeviceEventEmitter, Pressable, View } from 'react-native';
+import React, { useCallback, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { MezonBottomSheet } from '../../componentUI';
-import { APP_SCREEN } from '../../navigation/ScreenTypes';
-import UserEmptyMessage from '../home/homedrawer/UserEmptyClan/UserEmptyMessage';
-import MessageMenu from '../home/homedrawer/components/MessageMenu';
-import { DmListItem } from './DmListItem';
-import MessageHeader from './MessageHeader';
-import SearchDmList from './SearchDmList';
-import SkeletonMessageItem from './SkeletonMessageItem';
-import { style } from './styles';
+import MessagesScreenRender from './MessagesScreenRender';
 
-const MessagesScreen = ({ navigation }: { navigation: any }) => {
-	const { themeValue } = useTheme();
-	const styles = style(themeValue);
+const MessagesScreen = () => {
 	const dmGroupChatList = useSelector(selectDirectsOpenlistOrder);
-	const loadingStatus = useSelector((state: RootState) => state?.direct?.loadingStatus);
-	const bottomSheetDMMessageRef = useRef<BottomSheetModal>(null);
-	const [isDismissUI, setIsDismissUI] = useState<boolean>(false);
+	const shouldRender = useIdleRender();
+	const isFirstRender = useRef(true);
 
 	const dispatch = useAppDispatch();
 
 	useFocusEffect(
 		useCallback(() => {
+			if (isFirstRender.current) {
+				isFirstRender.current = false;
+				return;
+			}
 			dispatch(directActions.fetchDirectMessage({ noCache: true }));
 		}, [dispatch])
 	);
 
-	useEffect(() => {
-		const event = DeviceEventEmitter.addListener(ActionEmitEvent.ON_DISMISS_UI_FROM_FCM, (isDismiss: true) => {
-			setIsDismissUI(isDismiss);
-		});
-
-		return () => {
-			event.remove();
-		};
-	}, []);
-
-	useEffect(() => {
-		const appStateSubscription = AppState.addEventListener('change', handleAppStateChange);
-
-		return () => {
-			appStateSubscription.remove();
-		};
-	}, []);
-
-	const handleAppStateChange = async (state: string) => {
-		if (state === 'active') {
-			try {
-				await dispatch(directActions.fetchDirectMessage({ noCache: true }));
-			} catch (error) {
-				console.error('error messageLoaderBackground', error);
-			}
-		}
-	};
-
-	const navigateToAddFriendScreen = () => {
-		navigation.navigate(APP_SCREEN.FRIENDS.STACK, { screen: APP_SCREEN.FRIENDS.ADD_FRIEND });
-	};
-
-	const navigateToNewMessageScreen = () => {
-		navigation.navigate(APP_SCREEN.MESSAGES.STACK, { screen: APP_SCREEN.MESSAGES.NEW_MESSAGE });
-	};
-
-	const [directMessageSelected, setDirectMessageSelected] = useState<DirectEntity>(null);
-	const handleLongPress = useCallback((directMessage: DirectEntity) => {
-		bottomSheetDMMessageRef.current?.present();
-		setDirectMessageSelected(directMessage);
-	}, []);
-
-	if (isDismissUI) {
+	if (!shouldRender) {
 		return null;
 	}
 
-	return (
-		<View style={styles.container}>
-			<MessageHeader />
-			<SearchDmList />
-			{loadingStatus === 'loaded' && !dmGroupChatList?.length ? (
-				<UserEmptyMessage
-					onPress={() => {
-						navigateToAddFriendScreen();
-					}}
-				/>
-			) : loadingStatus === 'loading' && !dmGroupChatList?.length ? (
-				<SkeletonMessageItem numberSkeleton={10} />
-			) : (
-				<FlashList
-					data={dmGroupChatList}
-					contentContainerStyle={{
-						paddingBottom: size.s_100
-					}}
-					showsVerticalScrollIndicator={false}
-					keyExtractor={(dm) => dm + 'DM_MSG_ITEM'}
-					estimatedItemSize={size.s_60}
-					renderItem={({ item }) => <DmListItem id={item} navigation={navigation} key={item} onLongPress={handleLongPress} />}
-				/>
-			)}
-
-			<Pressable style={styles.addMessage} onPress={() => navigateToNewMessageScreen()}>
-				<Icons.MessagePlusIcon width={size.s_22} height={size.s_22} />
-			</Pressable>
-
-			<MezonBottomSheet ref={bottomSheetDMMessageRef} snapPoints={['40%', '60%']}>
-				<MessageMenu messageInfo={directMessageSelected} />
-			</MezonBottomSheet>
-		</View>
-	);
+	return <MessagesScreenRender chatList={JSON.stringify(dmGroupChatList)} />;
 };
 
 export default MessagesScreen;
