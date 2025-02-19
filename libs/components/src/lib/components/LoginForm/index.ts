ea7f62b9b69c18e38723be89a41cbde888e97e3f
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2025-02-19 16:44:26 +0700

    remove login form

--- libs/components/src/lib/components/LoginForm/index.ts
+++ libs/components/src/lib/components/LoginForm/index.ts
@@ -1,5 +1,4 @@
 export { default as FormLogin } from './FormLogin';
 export { default as GoogleButtonLogin } from './GoogleButton';
-export { default as LoginForm } from './LoginForm';
 export { QRSection } from './QR';
 export * from './Title';
