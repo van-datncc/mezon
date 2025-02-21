88a9869725ee4d67cdf4a94bd6c058b22acb75df
hoang.tranlehuy <hoang.tranlehuy@ncc.asia>
2025-02-21 17:49:22 +0700

    ignore redirect and remove modal and login button

--- apps/mobile/src/app/screens/auth/NewLogin/index.tsx
+++ apps/mobile/src/app/screens/auth/NewLogin/index.tsx
@@ -1,8 +1,9 @@
 import { useAuth } from '@mezon/core';
 import { useTheme } from '@mezon/mobile-ui';
+import { appActions, useAppDispatch } from '@mezon/store-mobile';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useMemo, useState } from 'react';
-import { Modal, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect, useMemo, useState } from 'react';
+import { Platform, View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import WebView from 'react-native-webview';
 import StatusBarHeight from '../../../components/StatusBarHeight/StatusBarHeight';
@@ -26,63 +27,57 @@ const NewLoginScreen = () => {
 		return randomState;
 	}, []);
 	const [authUri, setAuthUri] = useState(null);
-	const [modalVisible, setModalVisible] = useState(false);
 	const { loginByEmail } = useAuth();
+	const dispatch = useAppDispatch();
 
-	const handleLogin = async () => {
+	useEffect(() => {
 		const codeVerifier = generateCodeVerifier();
-		const codeChallenge = await generateCodeChallenge(codeVerifier);
+		const codeChallenge = generateCodeChallenge(codeVerifier);
 		AsyncStorage.setItem('code_verifier', codeVerifier);
 
 		const authUrl = `${OAUTH2_AUTHORIZE_URL}?client_id=${CLIENT_ID}&prompt=consent&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&redirect_uri=${REDIRECT_URI}&code_challenge=${codeChallenge}&code_challenge_method=${CODE_CHALLENGE_METHOD}&state=${STATE}`;
 		setAuthUri(authUrl);
-		if (authUrl) {
-			setModalVisible(true);
-		}
-	};
+	}, []);
 
-	const handleNavigationStateChange = (newNavState) => {
+	const handleShouldNavigationStateChange = (newNavState) => {
 		if (newNavState?.url?.includes('code') && newNavState?.url?.includes('state')) {
-			setModalVisible(false);
-			setAuthUri(null);
 			const code = new URLSearchParams(newNavState?.url?.split('?')[1]).get('code');
 			const state = new URLSearchParams(newNavState?.url?.split('?')[1]).get('state');
-			AsyncStorage.getItem('oauth_state').then(async (storedState) => {
-				if (state === storedState) {
-					await loginByEmail(code);
-				} else {
-					// todo: handle error or ignore
-				}
-			});
-		} else if (newNavState.url.includes('error')) {
-			setModalVisible(false);
-			setAuthUri(null);
-			Toast.show({
-				type: 'error',
-				text1: 'Login Failed',
-				text2: 'Invalid email or password'
-			});
+			if (code) {
+				AsyncStorage.getItem('oauth_state').then(async (storedState) => {
+					if (state === storedState) {
+						dispatch(appActions.setLoadingMainMobile(true));
+						try {
+							const res = await loginByEmail(code);
+							if (res === 'Invalid session') {
+								if (Platform.OS === 'android') {
+									Toast.show({
+										type: 'error',
+										text1: 'Login Failed',
+										text2: 'Invalid email or password'
+									});
+								}
+							}
+						} catch (error) {
+							dispatch(appActions.setLoadingMainMobile(false));
+						}
+					} else {
+						// todo: handle error or ignore
+					}
+				});
+				return false;
+			}
 		}
+		return true;
 	};
 
 	return (
 		<View style={styles.supperContainer}>
 			<StatusBarHeight />
-			<View style={styles.headerContainer}>
-				<Text style={styles.headerTitle}>WELCOME BACK</Text>
-				<Text style={styles.headerContent}>So glad to meet you again!</Text>
-			</View>
-			<TouchableOpacity onPress={handleLogin} style={styles.button}>
-				<Text style={styles.buttonText}>Login</Text>
-			</TouchableOpacity>
 			{authUri && (
-				<Modal visible={modalVisible} onRequestClose={() => setAuthUri(null)} transparent={true} style={styles.modal}>
-					<View style={styles.modalView}>
-						<View style={styles.webView}>
-							<WebView source={{ uri: authUri }} onNavigationStateChange={handleNavigationStateChange} />
-						</View>
-					</View>
-				</Modal>
+				<View style={styles.webView}>
+					<WebView source={{ uri: authUri }} onShouldStartLoadWithRequest={handleShouldNavigationStateChange} startInLoadingState={true} />
+				</View>
 			)}
 		</View>
 	);
