5322d1fec2c8ab27fe186a9bf3fb63fc48653163
trinhtruongthiphuong <trinh.truongthiphuong@ncc.asia>
2025-03-04 10:23:48 +0700

    fix wrong highest role color for member on mobile

--- apps/mobile/src/app/screens/home/homedrawer/components/InfoUserMessage/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/InfoUserMessage/index.tsx
@@ -1,8 +1,9 @@
-import { Text, useColorsRoleById } from '@mezon/mobile-ui';
-import { DEFAULT_MESSAGE_CREATOR_NAME_DISPLAY_COLOR, convertTimeString } from '@mezon/utils';
-import { ChannelStreamMode } from 'mezon-js';
-import React from 'react';
+import { Text } from '@mezon/mobile-ui';
+import { RolesClanEntity, selectRolesClanEntities } from '@mezon/store-mobile';
+import { DEFAULT_ROLE_COLOR, convertTimeString } from '@mezon/utils';
+import React, { useMemo } from 'react';
 import { TouchableOpacity, View } from 'react-native';
+import { useSelector } from 'react-redux';
 import { styles } from './styles';
 
 interface IProps {
@@ -14,16 +15,42 @@ interface IProps {
 	mode: number;
 }
 export const InfoUserMessage = ({ createTime, isShow, onPress, senderDisplayName, messageSenderId, mode }: IProps) => {
-	const userRolesClan = useColorsRoleById(messageSenderId);
-	const colorSenderName =
-		mode === ChannelStreamMode.STREAM_MODE_CHANNEL || mode === ChannelStreamMode.STREAM_MODE_THREAD
-			? userRolesClan.highestPermissionRoleColor
-			: DEFAULT_MESSAGE_CREATOR_NAME_DISPLAY_COLOR;
+	const rolesClanEntity = useSelector(selectRolesClanEntities);
+	const userRolesClan = useMemo(() => {
+		const activeRole: Array<RolesClanEntity> = [];
+		let userRoleLength = 0;
+		let highestPermissionRole = null;
+		let maxLevelPermission = 0;
 
+		for (const key in rolesClanEntity) {
+			const role = rolesClanEntity[key];
+			const checkHasRole = role.role_user_list?.role_users?.some((listUser) => listUser.id === messageSenderId);
+
+			if (checkHasRole) {
+				activeRole.push(role);
+				userRoleLength++;
+
+				if (role.max_level_permission !== undefined && role.max_level_permission > maxLevelPermission) {
+					maxLevelPermission = role.max_level_permission;
+					highestPermissionRole = role;
+				}
+			}
+		}
+
+		return {
+			usersRole: activeRole,
+			length: userRoleLength,
+			highestPermissionRoleColor: highestPermissionRole?.color || activeRole[0]?.color || DEFAULT_ROLE_COLOR
+		};
+	}, [messageSenderId, rolesClanEntity]);
 	if (isShow) {
 		return (
 			<TouchableOpacity activeOpacity={0.8} onPress={onPress} style={styles.messageBoxTop}>
-				<Text style={{ ...styles.usernameMessageBox, color: colorSenderName }} numberOfLines={1} ellipsizeMode="tail">
+				<Text
+					style={{ ...styles.usernameMessageBox, color: userRolesClan.highestPermissionRoleColor }}
+					numberOfLines={1}
+					ellipsizeMode="tail"
+				>
 					{senderDisplayName}
 				</Text>
 				<Text style={styles.dateMessageBox}>{createTime ? convertTimeString(createTime) : ''}</Text>
