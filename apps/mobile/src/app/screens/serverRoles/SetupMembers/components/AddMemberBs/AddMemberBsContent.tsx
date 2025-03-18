00f62831b279fcd26c79c08f1156ff8144fc4b93
hoang.tranlehuy <177521363+hoangtranlehuy@users.noreply.github.com>
2025-03-18 16:27:51 +0700

    fix setting edit permission to role mobile

--- apps/mobile/src/app/screens/serverRoles/SetupMembers/components/AddMemberBs/AddMemberBsContent.tsx
+++ apps/mobile/src/app/screens/serverRoles/SetupMembers/components/AddMemberBs/AddMemberBsContent.tsx
@@ -53,7 +53,7 @@ export const AddMemberBsContent = memo((props: IAddMemberBsContentProps) => {
 	const handleAddMemberToRole = useCallback(async () => {
 		const response = await updateRole(role?.clan_id, role?.id, role?.title, role?.color || '', selectedMemberIdList, [], [], []);
 		onClose && onClose();
-		if (response) {
+		if (response === true) {
 			Toast.show({
 				type: 'success',
 				props: {
--- apps/mobile/src/app/screens/serverRoles/SetupMembers/components/MemberItem/index.tsx
+++ apps/mobile/src/app/screens/serverRoles/SetupMembers/components/MemberItem/index.tsx
@@ -43,7 +43,7 @@ export const MemberItem = memo((props: IMemberItemProps) => {
 	const onDeleteMember = useCallback(async () => {
 		const response = await updateRole(role?.clan_id, role?.id, role?.title, role?.color || '', [], [], [member?.id], []);
 
-		if (response) {
+		if (response === true) {
 			Toast.show({
 				type: 'success',
 				props: {
