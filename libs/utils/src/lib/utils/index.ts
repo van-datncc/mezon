b039e661fea04252dfaf4a0eefbd240ce44831f2
Nhan Nguyen <nguyentran@ncc.asia>
2025-03-21 14:30:24 +0700

    fix link

--- libs/utils/src/lib/utils/index.ts
+++ libs/utils/src/lib/utils/index.ts
@@ -855,10 +855,7 @@ type ImgproxyOptions = {
 export const createImgproxyUrl = (sourceImageUrl: string, options: ImgproxyOptions = { width: 100, height: 100, resizeType: 'fit' }) => {
 	//const extension = sourceImageUrl.split('.').pop()?.toLowerCase();
 	if (!sourceImageUrl) return '';
-	if (
-		!sourceImageUrl.startsWith(`https://${process.env.NX_BASE_IMG_URL}`) ||
-		sourceImageUrl.startsWith(`https://${process.env.NX_BASE_IMG_URL}/stickers`)
-	) {
+	if (!sourceImageUrl.startsWith(`${process.env.NX_BASE_IMG_URL}`) || sourceImageUrl.startsWith(`${process.env.NX_BASE_IMG_URL}/stickers`)) {
 		return sourceImageUrl;
 	}
 	const { width, height, resizeType } = options;
