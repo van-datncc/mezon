aca95276d0fe764eb67ec74ff700402089d5646c
nam trinh <nam.trinhhoai@ncc.asia>
2025-03-24 10:18:29 +0700

    modify z-index

--- libs/components/src/lib/components/ScreenSelectionModal/ScreenSelectionModal.tsx
+++ libs/components/src/lib/components/ScreenSelectionModal/ScreenSelectionModal.tsx
@@ -29,7 +29,7 @@ const ScreenSelectionModal = memo(({ onClose }: ScreenSelectionModalProps) => {
 		<div
 			ref={modalRef}
 			tabIndex={-1}
-			className="outline-none justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-99 outline-none focus:outline-none bg-black bg-opacity-80 dark:text-white text-black hide-scrollbar overflow-hidden contain-layout contain-paint"
+			className="outline-none justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-80 dark:text-white text-black hide-scrollbar overflow-hidden contain-layout contain-paint"
 		>
 			<div className="relative w-full sm:h-auto rounded-lg max-w-[600px] contain-layout contain-paint">
 				<div className="rounded-lg text-sm overflow-hidden">
