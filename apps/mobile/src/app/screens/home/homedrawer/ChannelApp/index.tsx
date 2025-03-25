478256a25d885a8f1cd56e63503bc51bf9208811
hoang.tranlehuy <177521363+hoangtranlehuy@users.noreply.github.com>
2025-03-25 15:41:05 +0700

    update new channel app mobile

--- apps/mobile/src/app/screens/home/homedrawer/ChannelApp/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/ChannelApp/index.tsx
@@ -1,21 +1,34 @@
 import { useClans } from '@mezon/core';
-import { ThemeModeBase, useTheme } from '@mezon/mobile-ui';
-import { getAuthState } from '@mezon/store-mobile';
-import { sleep } from '@mezon/utils';
-import { memo, useState } from 'react';
-import { StatusBar, View } from 'react-native';
+import { size, ThemeModeBase, useTheme } from '@mezon/mobile-ui';
+import { getAuthState, selectAllAccount, selectAllChannelMembers, selectAppChannelById, useAppSelector } from '@mezon/store-mobile';
+import { MiniAppEventType, sleep } from '@mezon/utils';
+import { useNavigation } from '@react-navigation/native';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
 import { Chase } from 'react-native-animated-spinkit';
 import WebView from 'react-native-webview';
 import { useSelector } from 'react-redux';
+import MezonIconCDN from '../../../../componentUI/MezonIconCDN';
+import { IconCDN } from '../../../../constants/icon_cdn';
 import { style } from './styles';
 
-const ChannelAppScreen = memo(({ channelId }: { channelId: string }) => {
+const ChannelAppScreen = ({ route }) => {
 	const { themeValue, themeBasic } = useTheme();
 	const styles = style(themeValue);
 	const authState = useSelector(getAuthState);
 	const session = JSON.stringify(authState.session);
+	const { channelId } = route?.params || {};
 	const [loading, setLoading] = useState(true);
 	const { currentClanId } = useClans();
+	const channelApp = useAppSelector((state) => selectAppChannelById(state, channelId as string));
+	const userProfile = useSelector(selectAllAccount);
+	const userChannels = useAppSelector((state) => selectAllChannelMembers(state, channelApp?.channel_id));
+	const webviewRef = useRef<WebView>(null);
+	const [channelTitle, setChannelTitle] = useState<string>('');
+	const navigation = useNavigation();
+	const miniAppDataHash = useMemo(() => {
+		return `userChannels=${JSON.stringify(userChannels)}`;
+	}, [userChannels]);
 
 	const uri = `${process.env.NX_CHAT_APP_REDIRECT_URI}/chat/clans/${currentClanId}/channels/${channelId}`;
 
@@ -41,6 +54,67 @@ const ChannelAppScreen = memo(({ channelId }: { channelId: string }) => {
 	true;
   `;
 
+	const injectSelector = `#mainChat > div`;
+
+	const injectedDataJS = `
+   (function() {
+      document.addEventListener('message', function(event) {
+	  		window.ReactNativeWebView.postMessage(event.data);
+          window.ReactNativeWebView.postMessage('Pong');
+      });
+    })();
+	true;
+	(function() {
+      var style = document.createElement('style');
+      style.innerHTML = \`
+        .h-heightTopBar {
+          display: none !important;
+        }
+      \`;
+      document.head.appendChild(style);
+    })();
+	true;
+	(function() {
+	setTimeout(() => {
+		window.ReactNativeWebView.postMessage("huh");
+      let element = document.querySelector("#mainChat > div > div > div.flex-shrink.flex.flex-col.dark\\:bg-bgPrimary.bg-bgLightPrimary.h-auto.relative.w-full > div.flex.gap-2.px-3.pt-2.dark\\:text-channelTextLabel.text-colorTextLightMode > div:nth-child(1)");
+      if (element) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({
+          id: element.id,
+          innerText: element.innerText,
+          classList: Array.from(element.classList)
+        }));
+      } else {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ error: "Không tìm thấy #mainChat" }));
+      }
+    }, 5000);
+	})();
+	true;
+  `;
+
+	const handlePing = useCallback(() => {
+		const message = JSON.stringify({ eventType: MiniAppEventType.CURRENT_USER_INFO, eventData: userProfile });
+		webviewRef?.current?.postMessage(message);
+	}, [userProfile]);
+
+	const handleReload = () => {
+		webviewRef?.current?.reload();
+	};
+
+	const handleMessage = useCallback((event) => {
+		if (event?.nativeEvent?.title) {
+			setChannelTitle(event?.nativeEvent?.title);
+		}
+	}, []);
+
+	const handleClose = () => {
+		navigation.goBack();
+	};
+
+	const handleGoback = () => {
+		webviewRef?.current?.goBack();
+	};
+
 	return (
 		<View style={styles.container}>
 			{loading && (
@@ -60,22 +134,40 @@ const ChannelAppScreen = memo(({ channelId }: { channelId: string }) => {
 				</View>
 			)}
 			<StatusBar barStyle={themeBasic === ThemeModeBase.LIGHT ? 'dark-content' : 'light-content'} backgroundColor={themeValue.charcoal} />
+			<View style={styles.topBar}>
+				<TouchableOpacity onPress={handleClose} style={styles.backButton}>
+					<MezonIconCDN icon={IconCDN.closeLargeIcon} height={size.s_20} width={size.s_20} />
+				</TouchableOpacity>
+				<Text style={styles.title}>{channelTitle}</Text>
+				<View style={styles.row}>
+					<TouchableOpacity onPress={handleGoback} style={styles.backButton}>
+						<MezonIconCDN icon={IconCDN.chevronSmallLeftIcon} height={size.s_20} width={size.s_20} />
+					</TouchableOpacity>
+					<TouchableOpacity onPress={handleReload} style={styles.backButton}>
+						<MezonIconCDN icon={IconCDN.reloadIcon} height={size.s_20} width={size.s_20} />
+					</TouchableOpacity>
+				</View>
+			</View>
 			<WebView
+				ref={webviewRef}
 				source={{
 					uri: uri
 				}}
 				originWhitelist={['*']}
 				style={styles.container}
 				injectedJavaScriptBeforeContentLoaded={injectedJS}
+				injectedJavaScript={injectedDataJS}
 				javaScriptEnabled={true}
 				nestedScrollEnabled={true}
 				onLoadEnd={async () => {
 					await sleep(500);
 					setLoading(false);
+					handlePing();
 				}}
+				onMessage={handleMessage}
 			/>
 		</View>
 	);
-});
+};
 
 export default ChannelAppScreen;
