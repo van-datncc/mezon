03becd47b8428e5744c4dbcfce34a0b17dac650f
sadohpv <anh.trantruong@ncc.asia>
2025-03-26 17:27:49 +0700

    fix UI message

--- libs/components/src/lib/components/MessageWithUser/index.tsx
+++ libs/components/src/lib/components/MessageWithUser/index.tsx
@@ -25,7 +25,7 @@ import ModalUserProfile from '../ModalUserProfile';
 import TokenTransactionMessage from '../TokenTransactionMsg/TokenTransactionMsg';
 import MessageAttachment from './MessageAttachment';
 import MessageAvatar from './MessageAvatar';
-import MessageContent from './MessageContent';
+import MessageContent, { TopicViewButton } from './MessageContent';
 import MessageHead from './MessageHead';
 import MessageInput from './MessageInput';
 import MessageReaction from './MessageReaction/MessageReaction';
@@ -281,6 +281,7 @@ function MessageWithUser({
 								onContextMenu={onContextMenu}
 							/>
 						)}
+						{!isTopic && message?.code === TypeMessage.Topic && <TopicViewButton message={message} />}
 
 						{Array.isArray(message?.content?.embed) && (
 							<div className="w-full">
