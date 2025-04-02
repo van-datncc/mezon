ed199059d2df8c43421581e9b2d30228746c350f
sadohpv <anh.trantruong@ncc.asia>
2025-04-02 14:54:48 +0700

    fix embed image size

--- libs/components/src/lib/components/EmbedMessage/components/EmbedImage.tsx
+++ libs/components/src/lib/components/EmbedMessage/components/EmbedImage.tsx
@@ -3,16 +3,24 @@ import { ImagePreview } from '../../ImagePreview';
 
 interface EmbedImageProps {
 	url: string;
+	width?: number;
+	height?: number;
 }
 
-export function EmbedImage({ url }: EmbedImageProps) {
+export function EmbedImage({ url, width, height }: EmbedImageProps) {
 	const [showPreview, closePreview] = useModal(() => {
 		return <ImagePreview imageUrl={url} onClose={closePreview} />;
 	});
 
 	return (
 		<div className="mt-2 rounded overflow-hidden">
-			<img src={url} alt="Embed image" className="w-full h-auto max-w-full cursor-pointer" onClick={showPreview} />
+			<img
+				src={url}
+				style={{ width: width || 'auto', height: height || 'auto' }}
+				alt=""
+				className="w-full h-auto max-w-full cursor-pointer"
+				onClick={showPreview}
+			/>
 		</div>
 	);
 }
