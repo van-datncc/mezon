2f8bb6845abd8dbc1c0c9109ec501e73eef911ed
sadohpv <anh.trantruong@ncc.asia>
2025-04-09 11:23:13 +0700

    fix reponsive UI

--- libs/components/src/lib/components/MemberList/index.tsx
+++ libs/components/src/lib/components/MemberList/index.tsx
@@ -1,4 +1,4 @@
-import { selectCloseMenu, selectCurrentClanId } from '@mezon/store';
+import { selectCurrentClanId } from '@mezon/store';
 import { memo } from 'react';
 import { useSelector } from 'react-redux';
 import ListMember from './listMember';
@@ -6,13 +6,10 @@ import ListMember from './listMember';
 export type MemberListProps = { className?: string };
 
 function MemberList() {
-	const closeMenu = useSelector(selectCloseMenu);
 	const currentClanId = useSelector(selectCurrentClanId);
 
 	return (
-		<div
-			className={`contain-strict self-stretch h-full flex-col justify-start items-start flex gap-[24px] w-full ${closeMenu ? 'pt-20' : 'pt-0'}`}
-		>
+		<div className={`contain-strict self-stretch h-full flex-col justify-start items-start flex gap-[24px] w-full`}>
 			<div className="w-full">
 				<div className="flex flex-col gap-4 pr-[2px]">
 					<ListMember key={currentClanId} />
