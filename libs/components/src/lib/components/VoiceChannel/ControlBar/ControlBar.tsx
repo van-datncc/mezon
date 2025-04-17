45869fc800b2dd20eafcd7709f6fb5d94d4d3df7
huy.lexuan <lexuanhuy4497@gmail.com>
2025-04-17 10:31:23 +0700

    fix/meet-camera

--- libs/components/src/lib/components/VoiceChannel/ControlBar/ControlBar.tsx
+++ libs/components/src/lib/components/VoiceChannel/ControlBar/ControlBar.tsx
@@ -21,7 +21,6 @@ import { useMediaPermissions } from '@mezon/utils';
 
 import isElectron from 'is-electron';
 import { LocalTrackPublication, Track } from 'livekit-client';
-import Tooltip from 'rc-tooltip';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useModal } from 'react-modal-hook';
 import { useSelector } from 'react-redux';
@@ -96,10 +95,13 @@ export function ControlBar({
 		[dispatch]
 	);
 
-	const cameraOnChange = useCallback(
-		(enabled: boolean, isUserInitiated: boolean) => (isUserInitiated ? dispatch(voiceActions.setShowCamera(enabled)) : null),
-		[dispatch]
-	);
+	const cameraOnChange = useCallback((enabled: boolean, isUserInitiated: boolean) => {
+		console.log(enabled, 'enabled');
+
+		console.log(isUserInitiated, 'cameraOnChange');
+
+		return isUserInitiated ? dispatch(voiceActions.setShowCamera(enabled)) : null;
+	}, []);
 
 	useEffect(() => {
 		if (isShowSelectScreenModal) {
@@ -243,29 +245,14 @@ export function ControlBar({
 			<div className="flex justify-center gap-3">
 				{visibleControls.microphone && hasMicrophoneAccess && (
 					<div className="relative rounded-full">
-						<Tooltip
-							showArrow={{ className: '!bottom-1' }}
+						<TrackToggle
 							key={+showMicrophone}
-							placement="top"
-							overlay={
-								<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">
-									{showMicrophone ? 'Turn Off Microphone' : 'Turn On Microphone'}
-								</span>
-							}
-							overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-							overlayClassName="whitespace-nowrap z-50 !p-0 !pt-4"
-							getTooltipContainer={() => document.getElementById('livekitRoom') || document.body}
-							destroyTooltipOnHide
-						>
-							<TrackToggle
-								key={+showMicrophone}
-								initialState={showMicrophone}
-								className="w-14 h-14 !rounded-full flex justify-center items-center"
-								source={Track.Source.Microphone}
-								onChange={microphoneOnChange}
-								onDeviceError={(error) => onDeviceError?.({ source: Track.Source.Microphone, error })}
-							/>
-						</Tooltip>
+							initialState={showMicrophone}
+							className="w-14 h-14 !rounded-full flex justify-center items-center"
+							source={Track.Source.Microphone}
+							onChange={microphoneOnChange}
+							onDeviceError={(error) => onDeviceError?.({ source: Track.Source.Microphone, error })}
+						/>
 						<MediaDeviceMenu
 							kind="audioinput"
 							onActiveDeviceChange={(_kind, deviceId) => saveAudioInputDeviceId(deviceId ?? 'default')}
@@ -274,27 +261,14 @@ export function ControlBar({
 				)}
 				{visibleControls.camera && hasCameraAccess && (
 					<div className="relative rounded-full">
-						<Tooltip
-							showArrow={{ className: '!bottom-1' }}
+						<TrackToggle
 							key={+showCamera}
-							placement="top"
-							overlay={
-								<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">{showCamera ? 'Turn Off Camera' : 'Turn On Camera'}</span>
-							}
-							overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-							overlayClassName="whitespace-nowrap z-50 !p-0 !pt-4"
-							getTooltipContainer={() => document.getElementById('livekitRoom') || document.body}
-							destroyTooltipOnHide
-						>
-							<TrackToggle
-								key={+showCamera}
-								initialState={showCamera}
-								className="w-14 h-14 !rounded-full flex justify-center items-center"
-								source={Track.Source.Camera}
-								onChange={cameraOnChange}
-								onDeviceError={(error) => onDeviceError?.({ source: Track.Source.Camera, error })}
-							/>
-						</Tooltip>
+							initialState={showCamera}
+							className="w-14 h-14 !rounded-full flex justify-center items-center"
+							source={Track.Source.Camera}
+							onChange={cameraOnChange}
+							onDeviceError={(error) => onDeviceError?.({ source: Track.Source.Camera, error })}
+						/>
 						<MediaDeviceMenu
 							kind="videoinput"
 							onActiveDeviceChange={(_kind, deviceId) => saveVideoInputDeviceId(deviceId ?? 'default')}
@@ -304,121 +278,57 @@ export function ControlBar({
 						)}
 					</div>
 				)}
-				{visibleControls.screenShare && browserSupportsScreenSharing && (
-					<Tooltip
-						showArrow={{ className: '!bottom-1' }}
-						key={+showScreen}
-						placement="top"
-						overlay={
-							<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">{showScreen ? 'Stop screen share' : 'Share Your Screen'}</span>
-						}
-						overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-						overlayClassName="whitespace-nowrap z-50 !p-0 !pt-4"
-						getTooltipContainer={() => document.getElementById('livekitRoom') || document.body}
-						destroyTooltipOnHide
-					>
-						{!isDesktop ? (
-							<TrackToggle
-								key={+showScreen}
-								initialState={showScreen}
-								className="w-14 h-14 !rounded-full flex justify-center items-center"
-								source={Track.Source.ScreenShare}
-								captureOptions={{ audio: true, selfBrowserSurface: 'include' }}
-								onChange={onScreenShare}
-								onDeviceError={(error) => onDeviceError?.({ source: Track.Source.ScreenShare, error })}
-							/>
-						) : (
-							<ScreenShareToggleButton
-								onClick={handleOpenScreenSelection}
-								className="w-14 h-14 !rounded-full flex justify-center items-center"
-							/>
-						)}
-					</Tooltip>
-				)}
+				{visibleControls.screenShare &&
+					browserSupportsScreenSharing &&
+					(!isDesktop ? (
+						<TrackToggle
+							key={+showScreen}
+							initialState={showScreen}
+							className="w-14 h-14 !rounded-full flex justify-center items-center"
+							source={Track.Source.ScreenShare}
+							captureOptions={{ audio: true, selfBrowserSurface: 'include' }}
+							onChange={onScreenShare}
+							onDeviceError={(error) => onDeviceError?.({ source: Track.Source.ScreenShare, error })}
+						/>
+					) : (
+						<ScreenShareToggleButton
+							onClick={handleOpenScreenSelection}
+							className="w-14 h-14 !rounded-full flex justify-center items-center"
+						/>
+					))}
 				{visibleControls.leave && (
-					<Tooltip
-						showArrow={{ className: '!bottom-1' }}
-						placement="top"
-						overlay={<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">Disconnect</span>}
-						overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-						overlayClassName="whitespace-nowrap z-50 !p-0 !pt-4"
-						getTooltipContainer={() => document.getElementById('livekitRoom') || document.body}
+					<div
+						onClick={onLeaveRoom}
+						className="w-14 h-14 bg-[#da373c] hover:bg-[#a12829] cursor-pointer rounded-full flex justify-center items-center"
 					>
-						<div
-							onClick={onLeaveRoom}
-							className="w-14 h-14 bg-[#da373c] hover:bg-[#a12829] cursor-pointer rounded-full flex justify-center items-center"
-						>
-							<Icons.EndCall className="w-6 h-6" />
-						</div>
-					</Tooltip>
+						<Icons.EndCall className="w-6 h-6" />
+					</div>
 				)}
 			</div>
 			<div className="flex justify-end gap-4">
 				{!isExternalCalling && (
 					<div onClick={togglePopout}>
 						{isOpenPopOut ? (
-							<Tooltip
-								showArrow={{ className: '!bottom-1' }}
-								placement="top"
-								overlay={<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">Return To App</span>}
-								overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-								overlayClassName="whitespace-nowrap z-50 !p-0 !pt-4"
-								getTooltipContainer={() => document.getElementById(livekitRoomId) || document.body}
-							>
-								<span>
-									<Icons.VoicePopOutIcon className="cursor-pointer hover:text-white text-[#B5BAC1] rotate-180" />
-								</span>
-							</Tooltip>
+							<span>
+								<Icons.VoicePopOutIcon className="cursor-pointer hover:text-white text-[#B5BAC1] rotate-180" />
+							</span>
 						) : (
-							<Tooltip
-								showArrow={{ className: '!bottom-1' }}
-								placement="top"
-								overlay={<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">Pop Out</span>}
-								overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-								overlayClassName="whitespace-nowrap z-50 !p-0 !pt-4"
-								getTooltipContainer={() => document.getElementById(livekitRoomId) || document.body}
-							>
-								<span>
-									<Icons.VoicePopOutIcon className="cursor-pointer hover:text-white text-[#B5BAC1] " />
-								</span>
-							</Tooltip>
+							<span>
+								<Icons.VoicePopOutIcon className="cursor-pointer hover:text-white text-[#B5BAC1] " />
+							</span>
 						)}
 					</div>
 				)}
 
 				<div onClick={onFullScreen}>
 					{isFullScreen ? (
-						<Tooltip
-							showArrow={{ className: '!bottom-1' }}
-							placement="topRight"
-							align={{
-								offset: [11, -4]
-							}}
-							overlay={<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">Exit Full Screen</span>}
-							overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-							overlayClassName="whitespace-nowrap z-50 !p-0"
-							getTooltipContainer={() => document.getElementById('livekitRoom') || document.body}
-						>
-							<span>
-								<Icons.ExitFullScreen className="cursor-pointer hover:text-white text-[#B5BAC1]" />
-							</span>
-						</Tooltip>
+						<span>
+							<Icons.ExitFullScreen className="cursor-pointer hover:text-white text-[#B5BAC1]" />
+						</span>
 					) : (
-						<Tooltip
-							showArrow={{ className: '!bottom-1' }}
-							placement="topRight"
-							align={{
-								offset: [11, -4]
-							}}
-							overlay={<span className="bg-[#2B2B2B] rounded p-[6px] text-[14px]">Full Screen</span>}
-							overlayInnerStyle={{ background: 'none', boxShadow: 'none' }}
-							overlayClassName="whitespace-nowrap !p-0"
-							key={Number(isFullScreen)}
-						>
-							<span>
-								<Icons.FullScreen className="cursor-pointer hover:text-white text-[#B5BAC1]" />
-							</span>
-						</Tooltip>
+						<span>
+							<Icons.FullScreen className="cursor-pointer hover:text-white text-[#B5BAC1]" />
+						</span>
 					)}
 				</div>
 			</div>
