8c5265c4330e4c9c7892c775cc8713fbfd3d025b
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2025-04-22 13:14:47 +0700

    fix logic message user typing mobie

--- apps/mobile/src/app/screens/home/homedrawer/components/MessageUserTyping/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/MessageUserTyping/index.tsx
@@ -1,9 +1,10 @@
-import { useChatTypings } from '@mezon/core';
 import { Text, ThemeModeBase, useTheme } from '@mezon/mobile-ui';
+import { selectAllAccount, selectTypingUsersById } from '@mezon/store-mobile';
 import LottieView from 'lottie-react-native';
 import { ChannelStreamMode } from 'mezon-js';
 import React, { useMemo } from 'react';
 import { View } from 'react-native';
+import { useSelector } from 'react-redux';
 import { TYPING_DARK_MODE, TYPING_LIGHT_MODE } from '../../../../../../assets/lottie';
 import { style } from './styles';
 
@@ -17,7 +18,8 @@ interface IProps {
 export const MessageUserTyping = React.memo(({ channelId, isDM, mode, isPublic }: IProps) => {
 	const { themeValue, themeBasic } = useTheme();
 	const styles = style(themeValue);
-	const { typingUsers } = useChatTypings({ channelId, mode, isPublic, isDM });
+	const userProfile = useSelector(selectAllAccount);
+	const typingUsers = useSelector((state) => selectTypingUsersById(state, channelId, userProfile?.user?.id as string));
 	const typingLabel = useMemo(() => {
 		if (typingUsers?.length === 1) {
 			return `${typingUsers[0].typingName} is typing...`;
