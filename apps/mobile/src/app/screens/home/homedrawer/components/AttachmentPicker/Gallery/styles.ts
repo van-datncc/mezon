b6f7ee5dde986b799cab7dff606fc98381014af8
lich.duongthanh <lich.duongthanh@ncc.asia>
2025-04-22 15:08:45 +0700

    feat: add style for loading effect

--- apps/mobile/src/app/screens/home/homedrawer/components/AttachmentPicker/Gallery/styles.ts
+++ apps/mobile/src/app/screens/home/homedrawer/components/AttachmentPicker/Gallery/styles.ts
@@ -59,5 +59,15 @@ export const style = (colors: Attributes) =>
 		},
 		disable: {
 			opacity: 0.5
+		},
+		loadingContainer: {
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: 0,
+			justifyContent: 'center',
+			alignItems: 'center',
+			backgroundColor: colors.selectedOverlay
 		}
 	});
