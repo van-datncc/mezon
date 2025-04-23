f8af84ad1b4ed8885ea2f7b052cd15916aa64f82
sadohpv <anh.trantruong@ncc.asia>
2025-04-23 11:18:09 +0700

    fix prioty name typing

--- libs/core/src/lib/chat/hooks/useChatSending.ts
+++ libs/core/src/lib/chat/hooks/useChatSending.ts
@@ -41,12 +41,13 @@ export function useChatSending({ mode, channelOrDirect, fromTopic = false }: Use
 			: userProfile?.user?.avatar_url;
 
 	const priorityDisplayName = userProfile?.user?.display_name ? userProfile?.user?.display_name : userProfile?.user?.username;
-	const priorityNameToShow =
-		mode === ChannelStreamMode.STREAM_MODE_THREAD || mode === ChannelStreamMode.STREAM_MODE_CHANNEL
+	const priorityNameToShow = useMemo(() => {
+		return mode === ChannelStreamMode.STREAM_MODE_THREAD || mode === ChannelStreamMode.STREAM_MODE_CHANNEL
 			? profileInTheClan?.clan_nick
 				? profileInTheClan?.clan_nick
 				: priorityDisplayName
 			: priorityDisplayName;
+	}, [profileInTheClan]);
 
 	const currentUserId = userProfile?.user?.id || '';
 	const anonymousMode = useSelector(selectAnonymousMode);
