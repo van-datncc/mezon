dbc094407e70738a7fb4ae5d820277b6fed18637
sadohpv <anh.trantruong@ncc.asia>
2025-04-23 10:15:52 +0700

    fix audio share option

--- libs/components/src/lib/components/ScreenSelectionModal/ScreenListItems.tsx
+++ libs/components/src/lib/components/ScreenSelectionModal/ScreenListItems.tsx
@@ -4,6 +4,7 @@ import ScreenItems from './ScreenItem';
 type ScreenListItemsProp = {
 	source: string;
 	onClose: () => void;
+	audio: boolean;
 };
 type ScreenItems = {
 	id: string;
@@ -11,7 +12,7 @@ type ScreenItems = {
 	thumbnail: string;
 };
 
-const ScreenListItems = memo(({ source, onClose }: ScreenListItemsProp) => {
+const ScreenListItems = memo(({ source, onClose, audio }: ScreenListItemsProp) => {
 	const [screens, setScreens] = useState<ScreenItems[]>([]);
 
 	const getListScreen = useCallback(async () => {
@@ -26,7 +27,7 @@ const ScreenListItems = memo(({ source, onClose }: ScreenListItemsProp) => {
 	return (
 		<>
 			{screens.map((screen) => (
-				<ScreenItems key={screen.id} onClose={onClose} id={screen.id} name={screen.name} thumbnail={screen.thumbnail} />
+				<ScreenItems key={screen.id} onClose={onClose} id={screen.id} name={screen.name} thumbnail={screen.thumbnail} audio={audio} />
 			))}
 		</>
 	);
