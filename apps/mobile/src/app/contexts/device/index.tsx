ac2b267cfd0d68b3c7ea0340d7667ade3bfb9cc8
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2025-04-24 15:03:49 +0700

    update: file DeviceProvider mobile

--- apps/mobile/src/app/contexts/device/index.tsx
+++ apps/mobile/src/app/contexts/device/index.tsx
@@ -6,7 +6,7 @@ export const DeviceContext = createContext<DeviceContextType | null>(null);
 
 export const DeviceProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	const [isTabletLandscape, setIsTabletLandscape] = useState<boolean>(false);
-	
+
 	const checkTablet = async (): Promise<boolean> => {
 		try {
 			return Platform.OS === 'ios' ? await NativeModules.DeviceUtilsIOS.isTablet() : await NativeModules.DeviceUtils.isTablet();
@@ -14,8 +14,8 @@ export const DeviceProvider: React.FC<{ children: React.ReactNode }> = ({ childr
 			console.error('Error checking if device is a tablet:', error);
 			return false;
 		}
-	
-	
+	};
+
 	const checkOrientation = useCallback(async () => {
 		const { width, height } = Dimensions.get('window');
 		const isLandscape = width > height;
