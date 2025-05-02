4b3977199c442674cd1f8fca459ae6ebc681bdc0
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-05-02 17:33:57 +0700

    Update ProtectedRoutes to include redirectUrl handling for navigation

--- apps/admin/src/app/routes/ProtectedRoutes.tsx
+++ apps/admin/src/app/routes/ProtectedRoutes.tsx
@@ -1,4 +1,4 @@
-import { selectIsLogin } from '@mezon/store';
+import { selectAllAuth, selectIsLogin } from '@mezon/store';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useLoaderData } from 'react-router-dom';
 import { IAuthLoaderData } from '../loader/authLoader';
@@ -6,11 +6,13 @@ import { IAuthLoaderData } from '../loader/authLoader';
 const ProtectedRoutes = () => {
 	const { isLogin: isLoginLoader, redirect } = useLoaderData() as IAuthLoaderData;
 	const isLoginStore = useSelector(selectIsLogin);
+	const { redirectUrl } = useSelector(selectAllAuth);
 	const isLogin = isLoginLoader && isLoginStore;
 
 	if (!isLogin) {
-		return <Navigate to={redirect || ''} replace />;
+		return <Navigate to={redirectUrl || redirect || '/login'} replace />;
 	}
+
 	return <Outlet />;
 };
 
