32839e81890cd0ea2b4e5480225da265f1b0ad47
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-05-05 16:08:57 +0700

    fix(auth): update redirect handling in ProtectedRoutes for better navigation flow

--- apps/chat/src/app/routes/ProtectedRoutes.tsx
+++ apps/chat/src/app/routes/ProtectedRoutes.tsx
@@ -1,22 +1,22 @@
 import { authActions, selectIsLogin, stickerSettingActions, useAppDispatch } from '@mezon/store';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, Outlet, useLoaderData } from 'react-router-dom';
+import { Navigate, Outlet, useLoaderData, useLocation } from 'react-router-dom';
 import { IAuthLoaderData } from '../loaders/authLoader';
 
 const ProtectedRoutes = () => {
 	const { isLogin: isLoginLoader, redirect } = useLoaderData() as IAuthLoaderData;
 	const isLoginStore = useSelector(selectIsLogin);
 	const isLogin = isLoginLoader && isLoginStore;
+	const location = useLocation();
 	const dispatch = useAppDispatch();
 	useEffect(() => {
 		dispatch(stickerSettingActions.fetchStickerByUserId({}));
 	}, [dispatch]);
 	if (!isLogin) {
-		dispatch(authActions.setLogout());
+		dispatch(authActions.setRedirectUrl(location.pathname));
 		return <Navigate to={redirect || '/desktop/login'} replace />;
 	}
-
 	return <Outlet />;
 };
 
