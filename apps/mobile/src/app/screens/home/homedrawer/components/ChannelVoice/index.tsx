6dd959a7067d212fd7bc2aadc8fa80e0f456d24a
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2025-05-06 14:06:04 +0700

    fix: cannot switch camera front/back video call mezon meet on IOS

--- apps/mobile/src/app/screens/home/homedrawer/components/ChannelVoice/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ChannelVoice/index.tsx
@@ -1,6 +1,7 @@
 import { AudioSession, LiveKitRoom, TrackReference, useConnectionState, useLocalParticipant } from '@livekit/react-native';
 import { size, useTheme } from '@mezon/mobile-ui';
 import { ChannelsEntity, selectChannelById2, selectIsPiPMode, useAppDispatch, useAppSelector, voiceActions } from '@mezon/store-mobile';
+import { Room, Track, createLocalVideoTrack } from 'livekit-client';
 import React, { memo, useEffect, useState } from 'react';
 import { AppState, Dimensions, NativeModules, Platform, Text, TouchableOpacity, View } from 'react-native';
 import InCallManager from 'react-native-incall-manager';
@@ -73,17 +74,38 @@ type headerProps = {
 const HeaderRoomView = memo(({ channel, onPressMinimizeRoom, onToggleSpeaker, isSpeakerOn }: headerProps) => {
 	const { themeValue } = useTheme();
 	const styles = style(themeValue);
-	const { cameraTrack, isCameraEnabled } = useLocalParticipant();
+	const { cameraTrack, isCameraEnabled, localParticipant } = useLocalParticipant();
 
-	const handleSwitchCamera = () => {
-		if (cameraTrack && cameraTrack.track) {
-			if (typeof cameraTrack?.track?.mediaStreamTrack?._switchCamera === 'function') {
-				try {
-					cameraTrack?.track?.mediaStreamTrack?._switchCamera();
-				} catch (error) {
-					console.error(error);
+	const handleSwitchCamera = async () => {
+		try {
+			if (cameraTrack && cameraTrack.track) {
+				if (Platform.OS === 'ios') {
+					const videoPublication = localParticipant.getTrackPublication(Track.Source.Camera);
+					const videoTrack = videoPublication?.track;
+					const facingModeCurrent = videoPublication.track?.mediaStreamTrack?.getSettings?.()?.facingMode;
+					if (videoTrack) {
+						await localParticipant.unpublishTrack(videoTrack);
+					}
+					const newFacingMode = facingModeCurrent === 'user' ? 'environment' : 'user';
+					const devices = await Room.getLocalDevices('videoinput');
+					const targetCamera = devices.find((d) => d?.facing === (newFacingMode === 'user' ? 'front' : 'environment'));
+					const newTrack = await createLocalVideoTrack({
+						deviceId: targetCamera.deviceId,
+						facingMode: newFacingMode
+					});
+					await localParticipant.publishTrack(newTrack);
+				} else {
+					if (typeof cameraTrack?.track?.mediaStreamTrack?._switchCamera === 'function') {
+						try {
+							cameraTrack?.track?.mediaStreamTrack?._switchCamera();
+						} catch (error) {
+							console.error(error);
+						}
+					}
 				}
 			}
+		} catch (error) {
+			console.error(error);
 		}
 	};
 
@@ -101,7 +123,7 @@ const HeaderRoomView = memo(({ channel, onPressMinimizeRoom, onToggleSpeaker, is
 			<View style={{ flexDirection: 'row', alignItems: 'center', gap: size.s_10 }}>
 				{isCameraEnabled && (
 					<TouchableOpacity onPress={() => handleSwitchCamera()} style={[styles.buttonCircle]}>
-						<MezonIconCDN icon={IconCDN.cameraFront} height={size.s_17} width={size.s_20} color={themeValue.white} />
+						<MezonIconCDN icon={IconCDN.cameraFront} height={size.s_24} width={size.s_24} color={themeValue.white} />
 					</TouchableOpacity>
 				)}
 				<TouchableOpacity onPress={() => onToggleSpeaker()} style={[styles.buttonCircle, isSpeakerOn && styles.buttonCircleActive]}>
