2e16823d0f1ac72f393b8c671f9f21ed0a1f6ed4
lich.duongthanh <lich.duongthanh@ncc.asia>
2025-05-14 09:27:43 +0700

    fix: cannot show preview youtube short

--- apps/mobile/src/app/screens/home/homedrawer/components/RenderTextMarkdown/index.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/RenderTextMarkdown/index.tsx
@@ -225,7 +225,7 @@ function parseMarkdownLink(text: string) {
 }
 
 export function extractYoutubeVideoId(url: string) {
-	const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+	const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/;
 	const match = url.match(regExp);
 	return match && match[2].length === 11 ? match[2] : null;
 }
