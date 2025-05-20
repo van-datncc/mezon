0277a46a34a9e4daf7548de44039bd70ec22f7dd
huy.lexuan <lexuanhuy4497@gmail.com>
2025-05-20 11:06:17 +0700

    fix/external-meet

--- libs/transport/src/lib/utils/index.ts
+++ libs/transport/src/lib/utils/index.ts
@@ -2,3 +2,10 @@ export function createApiGWBasePath(host: string, port?: string, useSSL = false)
 	const scheme = useSSL ? 'https://' : 'http://';
 	return port ? `${scheme}${host}:${port}` : `${scheme}${host}`;
 }
+
+export function generateBasePath(): string {
+	const host = process.env.NX_CHAT_APP_API_GW_HOST as string;
+	const port = process.env.NX_CHAT_APP_API_GW_PORT as string;
+	const useSSL = process.env.NX_CHAT_APP_API_SECURE === 'true';
+	return createApiGWBasePath(host, port, useSSL);
+}
