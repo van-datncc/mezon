61e222bf3662e6d8a99b07b013af4eed2ae311eb
Nhan Nguyen <nguyentran@ncc.asia>
2025-05-22 15:52:31 +0700

    fix env

--- apps/discover/src/app/context/DiscoverContext.tsx
+++ apps/discover/src/app/context/DiscoverContext.tsx
@@ -52,7 +52,10 @@ export const DiscoverProvider: React.FC<{ children: React.ReactNode }> = ({ chil
 				item_per_page: PAGINATION.ITEMS_PER_PAGE
 			};
 
-			const response = await mezon.listClanDiscover(`${process.env.NX_CHAT_APP_API_GW_HOST}:${process.env.NX_CHAT_APP_API_GW_PORT}`, request);
+			const response = await mezon.listClanDiscover(
+				`https://${process.env.NX_CHAT_APP_API_GW_HOST}:${process.env.NX_CHAT_APP_API_GW_PORT}`,
+				request
+			);
 			if (!response) {
 				throw new Error('No response from API');
 			}
