6fd3bb554837020d7c260e9e2578aae8363ea6e7
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2025-06-03 16:28:02 +0700

    fix break ui tablet in small device

--- apps/mobile/android/app/src/main/java/com/mobile/DeviceUtilsModule.kt
+++ apps/mobile/android/app/src/main/java/com/mobile/DeviceUtilsModule.kt
@@ -1,10 +1,15 @@
 package com.mezon.mobile
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.content.res.Configuration
+import android.content.pm.ActivityInfo
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import android.content.Context;
+import android.util.DisplayMetrics;
 
 class DeviceUtilsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -12,11 +17,22 @@ class DeviceUtilsModule(reactContext: ReactApplicationContext) : ReactContextBas
         return "DeviceUtils"
     }
 
+    fun checkIsTablet(context: Context): Boolean {
+        val metrics = context.resources.displayMetrics
+        val widthInches = metrics.widthPixels / metrics.xdpi
+        val heightInches = metrics.heightPixels / metrics.ydpi
+        val screenSize =
+            Math.sqrt(
+                (widthInches.toDouble() * widthInches.toDouble()) +
+                    (heightInches.toDouble() * heightInches.toDouble())
+            )
+        return screenSize >= 7.0
+    }
+
     @ReactMethod
     fun isTablet(promise: Promise) {
         try {
-            val isTablet = (reactApplicationContext.resources.configuration.screenLayout
-                    and Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE
+            val isTablet = checkIsTablet(reactApplicationContext)
             promise.resolve(isTablet)
         } catch (e: Exception) {
             promise.reject("ERROR", e)
