8ac62bbc5eaeae5db0581517cc1fc31be76f20f2
sadohpv <anh.trantruong@ncc.asia>
2025-06-09 09:43:40 +0700

    revert

--- libs/components/src/lib/components/FooterProfile/index.tsx
+++ libs/components/src/lib/components/FooterProfile/index.tsx
@@ -149,7 +149,7 @@ function FooterProfile({ name, status, avatar, userId, isDM }: FooterProfileProp
 		[createDirectMessageWithUser, sendInviteMessage]
 	);
 
-	const handleSaveSendToken = async (id: string, username?: string, avatar?: string, display_name?: string) => {
+	const handleSaveSendToken = async (id?: string, username?: string, avatar?: string, display_name?: string) => {
 		const userId = selectedUserId !== '' ? selectedUserId : id;
 		if (userId === '') {
 			setUserSearchError('Please select a user');
@@ -177,7 +177,9 @@ function FooterProfile({ name, status, avatar, userId, isDM }: FooterProfileProp
 		try {
 			await dispatch(giveCoffeeActions.sendToken(tokenEvent)).unwrap();
 			dispatch(giveCoffeeActions.setSendTokenEvent({ tokenEvent: tokenEvent, status: TOKEN_SUCCESS_STATUS }));
-			await sendNotificationMessage(userId, token, note ?? '', username, avatar, display_name);
+			if (id) {
+				await sendNotificationMessage(id, token, note ?? '', username, avatar, display_name);
+			}
 		} catch (err) {
 			dispatch(giveCoffeeActions.setSendTokenEvent({ tokenEvent: tokenEvent, status: TOKEN_FAILED_STATUS }));
 		}
