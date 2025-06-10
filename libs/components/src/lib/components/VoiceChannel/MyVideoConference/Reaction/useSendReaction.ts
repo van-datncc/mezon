c97864007c87ea7fae5a11fe061021cb554f4b1a
huy.lexuan <lexuanhuy4497@gmail.com>
2025-06-10 14:31:47 +0700

    update/reaction-meet

--- libs/components/src/lib/components/VoiceChannel/MyVideoConference/Reaction/useSendReaction.ts
+++ libs/components/src/lib/components/VoiceChannel/MyVideoConference/Reaction/useSendReaction.ts
@@ -1,6 +1,6 @@
 import { useMezon } from '@mezon/transport';
 import { useCallback } from 'react';
-import { ReactionType, UseSendReactionParams } from './types';
+import { UseSendReactionParams } from './types';
 
 export const useSendReaction = ({ currentChannel }: UseSendReactionParams) => {
 	const { socketRef } = useMezon();
@@ -8,30 +8,7 @@ export const useSendReaction = ({ currentChannel }: UseSendReactionParams) => {
 	const sendEmojiReaction = useCallback(
 		(emoji: string, emojiId: string) => {
 			if (!socketRef.current || !currentChannel?.channel_id) return;
-
-			socketRef.current.writeChatMessage(
-				currentChannel.clan_id || '',
-				currentChannel.channel_id,
-				2,
-				!currentChannel.channel_private,
-				{
-					t: emoji + ' ',
-					ej: [
-						{
-							emojiid: emojiId,
-							s: 0,
-							e: emoji.length
-						}
-					],
-					vr: ReactionType.VIDEO
-				},
-				[],
-				[],
-				[],
-				undefined,
-				undefined,
-				undefined
-			);
+			socketRef.current.writeVoiceReaction([emojiId], currentChannel.channel_id);
 		},
 		[socketRef, currentChannel]
 	);
