98a98fdd2364cf0f0fd9fee9ce40e7b431a65b6c
sadohpv <anh.trantruong@ncc.asia>
2025-06-13 10:02:05 +0700

    fix voice disconnected

--- libs/core/src/lib/chat/contexts/ChatContext.tsx
+++ libs/core/src/lib/chat/contexts/ChatContext.tsx
@@ -227,7 +227,7 @@ const ChatContextProvider: React.FC<ChatContextProviderProps> = ({ children }) =
 
 	const onvoiceleaved = useCallback(
 		(voice: VoiceLeavedEvent) => {
-			dispatch(voiceActions.remove(voice.voice_user_id + voice.voice_channel_id));
+			dispatch(voiceActions.remove(voice.id));
 		},
 		[dispatch]
 	);
