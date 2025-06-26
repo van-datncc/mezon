820ef2dc5954e2acac3769e8070b9b5035b8e86a
NguyenNCC <nguyen.nguyenphuoc@ncc.asia>
2025-06-26 12:06:26 +0700

    update: check network state and wait refresh-session

--- apps/mobile/src/app/components/NetworkInfo/index.tsx
+++ apps/mobile/src/app/components/NetworkInfo/index.tsx
@@ -1,31 +1,96 @@
 import { Colors, size } from '@mezon/mobile-ui';
 import { appActions, selectHasInternetMobile } from '@mezon/store-mobile';
 import NetInfo from '@react-native-community/netinfo';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text } from 'react-native';
-import Animated from 'react-native-reanimated';
+import React, { useEffect, useRef } from 'react';
+import { AppState, StyleSheet, Text, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 const NetInfoComp = () => {
 	const hasInternet = useSelector(selectHasInternetMobile);
-
 	const dispatch = useDispatch();
+	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+	const fetchWithTimeout = async (url, timeout = 3000) => {
+		const controller = new AbortController();
+		const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+		try {
+			const response = await fetch(url, {
+				cache: 'no-cache',
+				signal: controller.signal
+			});
+			clearTimeout(timeoutId);
+			return response;
+		} catch (error) {
+			console.error('log  => error', error);
+			clearTimeout(timeoutId);
+			if (error.name === 'AbortError') {
+				throw new Error('Request timed out');
+			}
+			throw error;
+		}
+	};
+	const checkConnectionQuality = async () => {
+		try {
+			const startTime = Date.now();
+			const response = await fetchWithTimeout(`${process.env.NX_CHAT_APP_REDIRECT_URI}/favicon.ico`, 3000);
+
+			if (!response.ok) {
+				dispatch(appActions.setHasInternetMobile(false));
+				return false;
+			}
+
+			// Calculate response time
+			const endTime = Date.now();
+			const responseTime = endTime - startTime;
+
+			// If response time is too high (e.g., > 3 seconds), consider it a poor connection
+			if (responseTime > 3000) {
+				dispatch(appActions.setHasInternetMobile(false));
+				return false;
+			}
+			timeoutRef?.current && clearInterval(timeoutRef.current);
+			dispatch(appActions.setHasInternetMobile(true));
+			return true;
+		} catch (error) {
+			dispatch(appActions.setHasInternetMobile(false));
+			console.error('log  => error checkConnectionQuality', error);
+			return false;
+		}
+	};
+
+	const checkInitConnection = async () => {
+		const isCheckConnect = await checkConnectionQuality();
+		if (!isCheckConnect) {
+			timeoutRef.current = setInterval(async () => {
+				await checkConnectionQuality();
+			}, 5000);
+		}
+	};
+
+	const handleAppStateChangeListener = async (nextAppState: string) => {
+		if (nextAppState === 'active') {
+			const state = await NetInfo.fetch();
+			dispatch(appActions.setHasInternetMobile(state.isConnected));
+			await checkInitConnection();
+		}
+	};
+
 	useEffect(() => {
-		const netInfoSubscription = NetInfo.addEventListener((state) => {
+		checkInitConnection();
+		AppState.addEventListener('change', handleAppStateChangeListener);
+		NetInfo.addEventListener((state) => {
 			dispatch(appActions.setHasInternetMobile(state.isConnected));
 		});
-		return () => {
-			netInfoSubscription();
-		};
-	}, [dispatch]);
+	}, []);
 
 	return !hasInternet ? (
-		<Animated.View style={styles.container}>
+		<View style={styles.container}>
 			<Text style={styles.text1}>No internet connection</Text>
-			<Text numberOfLines={1} style={styles.text2}>
-				Please check your connection and try again
+			<Text numberOfLines={2} style={styles.text2}>
+				Please check your connection or restart the app and try again
 			</Text>
-		</Animated.View>
+		</View>
 	) : null;
 };
 
@@ -34,11 +99,11 @@ const styles = StyleSheet.create({
 		padding: 20,
 		paddingVertical: 15,
 		position: 'absolute',
-		zIndex: 999999999999,
+		zIndex: 10,
 		top: 60,
 		marginHorizontal: 10,
 		alignSelf: 'center',
-		backgroundColor: 'white',
+		backgroundColor: 'rgba(255, 255, 255, 0.93)',
 		borderRadius: 10,
 		elevation: 5, // Android shadow
 		shadowColor: Colors.black, // iOS shadow
