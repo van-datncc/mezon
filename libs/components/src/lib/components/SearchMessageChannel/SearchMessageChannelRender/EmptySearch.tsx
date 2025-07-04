5b513c64280158859f439931475fabc8c0f21979
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-07-04 09:50:18 +0700

    add 2

--- libs/components/src/lib/components/SearchMessageChannel/SearchMessageChannelRender/EmptySearch.tsx
+++ libs/components/src/lib/components/SearchMessageChannel/SearchMessageChannelRender/EmptySearch.tsx
@@ -3,10 +3,10 @@ import { Image } from '@mezon/ui';
 
 const EmptySearch = () => {
 	return (
-		<div className="flex flex-col flex-1 h-full p-4 bg-bgLightSecondary dark:bg-bgSecondary overflow-y-auto">
+		<div className="flex flex-col flex-1 h-full p-4 bg-outside-footer overflow-y-auto">
 			<div className="m-auto">
 				<Image className="w-[160px] h-[160px] mx-auto pointer-events-none" src={`assets/images/empty-search.svg`} />
-				<div className="text-base font-medium w-[280px] mt-10 text-center text-textLightTheme dark:text-textPrimary">
+				<div className="text-base font-medium w-[280px] mt-10 text-center text-theme-primary">
 					We searched far and wide. Unfortunately, no results were found.
 				</div>
 			</div>
