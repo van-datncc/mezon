000bd534b7dc87c6b2dc5402418f9852bad9ad84
van-datncc <dat.dangvan@ncc.asia>
2025-07-08 16:27:22 +0700

    update color theme dark and button friend

--- libs/ui/src/lib/Button/index.tsx
+++ libs/ui/src/lib/Button/index.tsx
@@ -14,7 +14,7 @@ export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement
 const Button: React.FC<ButtonProps> = ({ variant, size, disabled, children, className, ...rest }) => {
 	return (
 		<button
-			className={` p-2 font-[500] capitalize disabled:opacity-50 disabled:cursor-not-allowed text-theme-primary-hover ease-linear transition-all duration-150  ${className}`}
+			className={`font-[500] capitalize disabled:opacity-50 disabled:cursor-not-allowed ease-linear transition-all duration-150  ${className}`}
 			{...rest}
 			disabled={disabled}
 		>
