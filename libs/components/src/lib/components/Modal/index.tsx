e9d722a0cf580b46b748716be3be8f602ff2eb0a
sadohpv <anh.trantruong@ncc.asia>
2025-07-11 15:33:15 +0700

    fix click outside modal

--- libs/components/src/lib/components/Modal/index.tsx
+++ libs/components/src/lib/components/Modal/index.tsx
@@ -14,9 +14,9 @@ const Modal = ({ onClose, className, children }: ModalProps) => {
 		<div
 			ref={modalRef}
 			className={`flex items-center justify-center fixed top-0 bottom-0 left-0 right-1 z-[100] bg-modal-overlay base-theme-color text-theme-primary `}
-			onClick={onClose}
+			onMouseDown={onClose}
 		>
-			<div onClick={(e) => e.stopPropagation()} className="contents">
+			<div onMouseDown={(e) => e.stopPropagation()} onClick={(e) => e.stopPropagation()} className="contents">
 				{children}
 			</div>
 		</div>
