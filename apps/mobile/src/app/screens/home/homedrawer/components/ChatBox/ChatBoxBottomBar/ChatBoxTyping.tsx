cf57441f15a0c95d15662dbd10ab1470f45906e4
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2025-07-15 18:27:25 +0700

    fix topic discussion cache and keyboard mobile

--- apps/mobile/src/app/screens/home/homedrawer/components/ChatBox/ChatBoxBottomBar/ChatBoxTyping.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ChatBox/ChatBoxBottomBar/ChatBoxTyping.tsx
@@ -18,64 +18,67 @@ interface IChatInputProps {
 	channelId: string;
 	isPublic: boolean;
 	anonymousMode: boolean;
+	topicChannelId?: string;
 }
 
-export const ChatBoxTyping = memo(({ textChange, mode = 2, channelId = '', isPublic = false, anonymousMode = false }: IChatInputProps) => {
-	const dispatch = useAppDispatch();
-	const currentClanId = useSelector(selectCurrentClanId);
-	const userProfile = useSelector(selectAllAccount);
-	const userClanProfile = useAppSelector((state) => selectMemberClanByUserId2(state, userProfile?.user?.id));
+export const ChatBoxTyping = memo(
+	({ textChange, mode = 2, channelId = '', isPublic = false, anonymousMode = false, topicChannelId = '' }: IChatInputProps) => {
+		const dispatch = useAppDispatch();
+		const currentClanId = useSelector(selectCurrentClanId);
+		const userProfile = useSelector(selectAllAccount);
+		const userClanProfile = useAppSelector((state) => selectMemberClanByUserId2(state, userProfile?.user?.id));
 
-	const handleTyping = async () => {
-		if (anonymousMode) return;
-		dispatch(
-			messagesActions.sendTypingUser({
-				clanId: currentClanId || '',
-				channelId,
-				mode,
-				isPublic,
-				username: userClanProfile?.clan_nick || userProfile?.user?.display_name || userProfile?.user?.username
-			})
-		);
-	};
-
-	const handleDirectMessageTyping = async () => {
-		await Promise.all([
+		const handleTyping = async () => {
+			if (anonymousMode || !!topicChannelId) return;
 			dispatch(
 				messagesActions.sendTypingUser({
-					clanId: '0',
-					channelId: channelId,
-					mode: mode,
-					isPublic: false,
-					username: userProfile?.user?.display_name || userProfile?.user?.username
+					clanId: currentClanId || '',
+					channelId,
+					mode,
+					isPublic,
+					username: userClanProfile?.clan_nick || userProfile?.user?.display_name || userProfile?.user?.username
 				})
-			)
-		]);
-	};
+			);
+		};
 
-	const handleTypingDebounced = useThrottledCallback(handleTyping, 1000);
-	const handleDirectMessageTypingDebounced = useThrottledCallback(handleDirectMessageTyping, 1000);
+		const handleDirectMessageTyping = async () => {
+			await Promise.all([
+				dispatch(
+					messagesActions.sendTypingUser({
+						clanId: '0',
+						channelId: channelId,
+						mode: mode,
+						isPublic: false,
+						username: userProfile?.user?.display_name || userProfile?.user?.username
+					})
+				)
+			]);
+		};
 
-	const handleTypingMessage = async () => {
-		switch (mode) {
-			case ChannelStreamMode.STREAM_MODE_CHANNEL:
-			case ChannelStreamMode.STREAM_MODE_THREAD:
-				await handleTypingDebounced();
-				break;
-			case ChannelStreamMode.STREAM_MODE_DM:
-			case ChannelStreamMode.STREAM_MODE_GROUP:
-				await handleDirectMessageTypingDebounced();
-				break;
-			default:
-				break;
-		}
-	};
+		const handleTypingDebounced = useThrottledCallback(handleTyping, 1000);
+		const handleDirectMessageTypingDebounced = useThrottledCallback(handleDirectMessageTyping, 1000);
 
-	useEffect(() => {
-		if (textChange) {
-			handleTypingMessage();
-		}
-	}, [textChange]);
+		const handleTypingMessage = async () => {
+			switch (mode) {
+				case ChannelStreamMode.STREAM_MODE_CHANNEL:
+				case ChannelStreamMode.STREAM_MODE_THREAD:
+					await handleTypingDebounced();
+					break;
+				case ChannelStreamMode.STREAM_MODE_DM:
+				case ChannelStreamMode.STREAM_MODE_GROUP:
+					await handleDirectMessageTypingDebounced();
+					break;
+				default:
+					break;
+			}
+		};
 
-	return <View />;
-});
+		useEffect(() => {
+			if (textChange) {
+				handleTypingMessage();
+			}
+		}, [textChange]);
+
+		return <View />;
+	}
+);
