0a52fa45348f0319f08b7fb29145dcc79b4cbea7
ho.nguyenphi <nguyenphiho147@gmail.com>
2025-08-10 12:51:26 +0700

    Mobile App: custom view map location

--- apps/mobile/src/app/screens/home/homedrawer/RenderMessageBlock.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/RenderMessageBlock.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import RenderMessageMapView from '../../../components/RenderMessageMapView';
 import RenderMessageInvite from './components/RenderMessageInvite';
+
 function RenderMessageBlock({
 	isInviteLink,
 	isGoogleMapsLink,
-	contentMessage
+	contentMessage,
+	avatarUrl,
+	isSelf,
+	senderName
 }: {
 	isGoogleMapsLink: boolean;
 	isInviteLink: boolean;
 	contentMessage: string;
+	avatarUrl?: string;
+	isSelf?: boolean;
+	senderName?: string;
 }) {
 	if (isInviteLink) return <RenderMessageInvite content={contentMessage} />;
-	if (isGoogleMapsLink) return <RenderMessageMapView content={contentMessage} />;
+	if (isGoogleMapsLink)
+		return <RenderMessageMapView content={contentMessage} avatarUrl={avatarUrl} isSelf={isSelf} senderName={senderName} />;
 	return null;
 }
 
