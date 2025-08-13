6e7824d5bcdf8198ee2272bd89faf6d3e87046ac
lich.duongthanh <lich.duongthanh@ncc.asia>
2025-08-13 18:05:23 +0700

    fix: issue comment from PR

--- apps/mobile/src/app/components/ClanGroup/index.tsx
+++ apps/mobile/src/app/components/ClanGroup/index.tsx
@@ -1,12 +1,13 @@
 import { Colors, size, useTheme } from '@mezon/mobile-ui';
 import { ClanGroup as ClanGroupType, clansActions, useAppDispatch } from '@mezon/store-mobile';
 import { createImgproxyUrl } from '@mezon/utils';
-import { CloseSmallIcon, FolderIcon } from 'libs/mobile-components/src/lib/icons2';
 import React, { memo, useMemo } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { ScaleDecorator } from 'react-native-draggable-flatlist';
-import FastImage from 'react-native-fast-image';
+import MezonIconCDN from '../../componentUI/MezonIconCDN';
+import { IconCDN } from '../../constants/icon_cdn';
 import { ClanIcon } from '../../screens/home/homedrawer/components/ClanIcon';
+import ImageNative from '../ImageNative';
 import { style } from './styles';
 
 interface ClanGroupProps {
@@ -21,15 +22,22 @@ export const ClanGroup = memo(({ group, onClanPress, clans, drag, isActive }: Cl
 	const { themeValue } = useTheme();
 	const styles = style(themeValue, Colors);
 	const dispatch = useAppDispatch();
-	const groupClans = group?.clanIds?.map((clanId) => clans?.find((clan) => clan?.clan_id === clanId));
+	const groupClans = useMemo(() => {
+		return group?.clanIds?.map((clanId) => clans?.find((clan) => clan?.clan_id === clanId));
+	}, [clans, group?.clanIds]);
 
 	const totalBadgeCount = useMemo(() => {
 		if (!group?.clanIds) return 0;
 
-		return group.clanIds.reduce((total, clanId) => {
-			const clan = clans?.find((c) => c?.clan_id === clanId);
-			return total + (clan?.badge_count || 0);
-		}, 0);
+		try {
+			return group.clanIds.reduce((total, clanId) => {
+				const clan = clans?.find((c) => c?.clan_id === clanId);
+				return total + (clan?.badge_count || 0);
+			}, 0);
+		} catch (error) {
+			console.error('Error in totalBadgeCount: ', error);
+			return 0;
+		}
 	}, [group?.clanIds, clans]);
 
 	const handleRemoveClanFromGroup = (clanId: string) => {
@@ -47,11 +55,13 @@ export const ClanGroup = memo(({ group, onClanPress, clans, drag, isActive }: Cl
 		return (
 			<View>
 				{clan?.logo ? (
-					<FastImage
-						source={{ uri: createImgproxyUrl(clan?.logo, { width: 100, height: 100, resizeType: 'fit' }) }}
-						style={{ width: '100%', height: '100%', borderRadius: 10 }}
-						resizeMode="cover"
-					/>
+					<View style={{ overflow: 'hidden', borderRadius: 10 }}>
+						<ImageNative
+							url={createImgproxyUrl(clan?.logo, { width: 100, height: 100, resizeType: 'fit' })}
+							style={{ width: '100%', height: '100%' }}
+							resizeMode="cover"
+						/>
+					</View>
 				) : (
 					<View style={styles.clanIcon}>
 						<Text style={styles.clanIconText}>{clan?.clan_name?.charAt(0)?.toUpperCase()}</Text>
@@ -65,14 +75,14 @@ export const ClanGroup = memo(({ group, onClanPress, clans, drag, isActive }: Cl
 		return (
 			<View style={styles.expandedGroup}>
 				<TouchableOpacity style={styles.groupHeader} onPress={handleToggleGroup}>
-					<FolderIcon width={size.s_24} height={size.s_24} color={themeValue.text} />
+					<MezonIconCDN icon={IconCDN.forderIcon} width={size.s_24} height={size.s_24} color={themeValue.text} />
 				</TouchableOpacity>
 
 				{groupClans?.map((clan) => (
 					<View key={`${clan?.clan_id}-expanded-container`} style={styles.clanContainer}>
 						<ClanIcon key={`${clan?.clan_id}-expanded`} data={clan} onPress={onClanPress} drag={undefined} isActive={false} />
 						<TouchableOpacity style={styles.removeButton} onPress={() => handleRemoveClanFromGroup(clan?.clan_id)}>
-							<CloseSmallIcon width={size.s_16} height={size.s_16} color={Colors.red} />
+							<MezonIconCDN icon={IconCDN.closeIcon} width={size.s_16} height={size.s_16} color={themeValue.textStrong} />
 						</TouchableOpacity>
 					</View>
 				))}
