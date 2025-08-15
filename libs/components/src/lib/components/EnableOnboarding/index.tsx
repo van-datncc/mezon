b62d73ef9a841d2ad92f3a201df92e6527d62f90
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-08-15 13:35:27 +0700

    update : UI COMMUNIY/ONBOADING

--- libs/components/src/lib/components/EnableOnboarding/index.tsx
+++ libs/components/src/lib/components/EnableOnboarding/index.tsx
@@ -1,7 +1,13 @@
 const EnableOnboarding = ({ onEnable }: { onEnable: () => void }) => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-[400px] bg-[#5865F2] rounded-lg p-8">
-			<img src={'assets/images/Onboarding_mezon_v2.png'} alt="Mezon Community" className="max-w-[400px] mb-8" />
+			<div className="mb-8 p-4 bg-white/10 backdrop-blur-sm rounded-2xl shadow-lg">
+				<img
+					src={"assets/images/onboarding.png"}
+					alt="Mezon Community"
+					className="max-w-[350px] w-full h-auto rounded-lg shadow-md"
+				/>
+			</div>
 			<h2 className="text-3xl font-bold text-white mb-4">Are you enable Onboarding?</h2>
 			<p className="text-white text-lg mb-6 text-center max-w-xl">
 				Convert to a Onboarding to access additional administrative tools that help you moderate and grow your Clan..
