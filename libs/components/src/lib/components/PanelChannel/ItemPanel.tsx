d53edc955e4174d12ac4a020e73196203ddfd0c2
thin.tranhoang <thin.tranhoang@ncc.asia>
2025-08-20 11:44:21 +0700

    uupdate: all btn danger

--- libs/components/src/lib/components/PanelChannel/ItemPanel.tsx
+++ libs/components/src/lib/components/PanelChannel/ItemPanel.tsx
@@ -35,12 +35,12 @@ const ItemPanel = ({
 		<button
 			disabled={disabled}
 			onClick={onClick}
-			className={`flex flex-col justify-center w-full rounded-sm bg-item-hover pr-2 ${danger ? 'hover:bg-colorDanger' : ''}`}
+			className={`flex flex-col justify-center w-full rounded-sm bg-item-hover pr-2 ${danger ? 'hover:bg-[#f67e882a]' : ''}`}
 		>
 			<div className={'flex flex-row items-center justify-between w-full'}>
 				<li
 					className={`text-[14px] font-medium w-full py-[6px] px-[8px]  text-left cursor-pointer list-none m-0 truncate
-						${danger ? ' text-colorDanger hover:text-white' : info ? 'text-blue-500 dark:text-blue-400' : 'text-theme-primary text-theme-primary-hover'}`}
+						${danger ? ' text-colorDanger ' : info ? 'text-blue-500 dark:text-blue-400' : 'text-theme-primary text-theme-primary-hover'}`}
 				>
 					{children}
 				</li>
